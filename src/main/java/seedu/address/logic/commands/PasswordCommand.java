aa557c96fd18240ec1f84b510caad23e7aa2731d
lws803 <lws803@gmail.com>
2018-09-15 20:21:59 +0800

    Added password utils to generate hash and verify

---
+++ src/main/java/seedu/address/logic/commands/PasswordCommand.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+public class PasswordCommand extends Command {
+
+    public static final String COMMAND_WORD = "password";
+
+    public static final String MESSAGE_SUCCESS = "Stored and encrypted password";
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+
+
+        return new CommandResult(MESSAGE_SUCCESS + "hash: ");
+    }
+
+
+}
