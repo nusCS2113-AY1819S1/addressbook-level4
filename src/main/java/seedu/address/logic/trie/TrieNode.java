67f06bdf4681dd8d9cbf8eab70de1ff9e013610c
lekoook <kokteng1313@gmail.com>
2018-09-19 23:01:45 +0800

    Add removeChild(TrieNode)
    Added a method to a child node in Trie.

--- src/main/java/seedu/address/logic/trie/TrieNode.java
+++ src/main/java/seedu/address/logic/trie/TrieNode.java
@@ -51,6 +51,10 @@ public class TrieNode {
         return children.get(children.size()-1);
     }
 
+    public void removeChild(TrieNode child) {
+        children.remove(child);
+    }
+
     public boolean isEndNode() {
         return isEndNode;
     }
