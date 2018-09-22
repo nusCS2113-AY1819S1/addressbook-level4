14ef9413b2f04c0722bf74508841643189dbd7df
Vincent <vencintgamer@gmail.com>
2018-09-22 17:19:59 +0800

    Added a new super class MoneyFlow which Income and Expense extends from. Changed the way Income and Expense parse the strings Junit tests not complete

--- src/main/java/seedu/address/model/record/Expense.java
+++ src/main/java/seedu/address/model/record/Expense.java
@@ -7,26 +7,20 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  * Represents a Record's expense in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidExpense(String)}
  */
-public class Expense {
+public class Expense extends MoneyFlow {
 
     public static final String MESSAGE_EXPENSE_CONSTRAINTS =
-            "Expenses should consist of only digits and/or decimal points ('.')."
-            + "It must be of the following form <number>.<number>:\n"
-            + "1. <number> cannot start from '0' unless it has only 1 digit. "
-            + "There must be at least 1 digit in this field.\n"
-            + "2. At most 1 decimal point can be present. Decimal point is optional."
-            + "If decimal point is present, it must have at least 1 digit after it";
+            "Any form of expense should consist of '-', "
+                    + "followed by a sequence of characters consisting of only digits and/or decimal points ('.')."
+                    + "It must be of the following form <number>.<number>:\n"
+                    + "1. <number> cannot start from '0' unless it has only 1 digit. "
+                    + "There must be at least 1 digit in this field.\n"
+                    + "2. At most 1 decimal point can be present. Decimal point is optional."
+                    + "If decimal point is present, it must have at least 1 digit after it";
 
-    /*
-     *  Expense must follow the format as stated above.
-     */
-    // TODO: REFACTOR THIS TO MAKE IT LESS COMPLICATED
-    private static final String EXPENSE_WHOLE_NUMBER_ZERO_REGEX = "0";
-    private static final String EXPENSE_WHOLE_NUMBER_NONZERO_REGEX = "[1-9]{1}\\d*";
-    private static final String EXPENSE_DECIMAL_PART_REGEX = ".\\d+";
-    // EXPENSE_VALIDATION_REGEX = "^(0|[1-9]{1}\d*)($|.\d+)"
-    public static final String EXPENSE_VALIDATION_REGEX = "^" + "(" + EXPENSE_WHOLE_NUMBER_ZERO_REGEX + "|"
-            + EXPENSE_WHOLE_NUMBER_NONZERO_REGEX + ")" + "(" + "$" + "|" + EXPENSE_DECIMAL_PART_REGEX + ")";
+    private static final String EXPENSE_SIGN_REGEX = "[-]";
+    private static final String EXPENSE_VALIDATION_REGEX = "^" + EXPENSE_SIGN_REGEX
+            + "(" + UNSIGNED_MONEYFLOW_VALIDATION_REGEX + ")";
 
     public final String value;
 
@@ -37,7 +31,7 @@ public class Expense {
      */
     public Expense(String expense) {
         requireNonNull(expense);
-        checkArgument(isValidExpense(expense), MESSAGE_EXPENSE_CONSTRAINTS);
+        checkArgument(isValidExpense(expense), MESSAGE_MONEY_FLOW_CONSTRAINTS);
         value = expense;
     }
 
--- src/main/java/seedu/address/model/record/Income.java
+++ src/main/java/seedu/address/model/record/Income.java
@@ -2,38 +2,33 @@ package seedu.address.model.record;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
+import static seedu.address.model.record.MoneyFlow.UNSIGNED_MONEYFLOW_VALIDATION_REGEX;
 
 /**
  * Represents a Record's income in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidIncome(String)}
  */
-public class Income {
+public class Income extends MoneyFlow{
 
     public static final String MESSAGE_INCOME_CONSTRAINTS =
-            "Income should consist of only digits and/or decimal points ('.')."
-            + "It must be of the following form <number>.<number>:\n"
-            + "1. <number> cannot start from '0' unless it has only 1 digit. "
-            + "There must be at least 1 digit in this field.\n"
-            + "2. At most 1 decimal point can be present. Decimal point is optional."
-            + "If decimal point is present, it must have at least 1 digit after it";
+            "Any form of income should consist of '+', "
+                    + "followed by a sequence of characters consisting of only digits and/or decimal points ('.')."
+                    + "It must be of the following form <number>.<number>:\n"
+                    + "1. <number> cannot start from '0' unless it has only 1 digit. "
+                    + "There must be at least 1 digit in this field.\n"
+                    + "2. At most 1 decimal point can be present. Decimal point is optional."
+                    + "If decimal point is present, it must have at least 1 digit after it";
 
-    /*
-     *  Income must follow the format as stated above.
-     */
-    // TODO: REFACTOR THIS TO MAKE IT LESS COMPLICATED
-    private static final String INCOME_WHOLE_NUMBER_ZERO_REGEX = "0";
-    private static final String INCOME_WHOLE_NUMBER_NONZERO_REGEX = "[1-9]{1}\\d*";
-    private static final String INCOME_DECIMAL_PART_REGEX = ".\\d+";
-    // INCOME_VALIDATION_REGEX = "^(0|[1-9]{1}\d*)($|.\d+)"
-    public static final String INCOME_VALIDATION_REGEX = "^" + "(" + INCOME_WHOLE_NUMBER_ZERO_REGEX + "|"
-            + INCOME_WHOLE_NUMBER_NONZERO_REGEX + ")" + "(" + "$" + "|" + INCOME_DECIMAL_PART_REGEX + ")";
+    private static final String INCOME_SIGN_REGEX = "[\\+]";
+    private static final String INCOME_VALIDATION_REGEX = "^" + INCOME_SIGN_REGEX
+            + "(" + UNSIGNED_MONEYFLOW_VALIDATION_REGEX + ")";
 
     public final String value;
 
     /**
      * Constructs an {@code Income}.
      *
-     * @param income A valid income address.
+     * @param income A valid income.
      */
     public Income(String income) {
         requireNonNull(income);
@@ -42,7 +37,7 @@ public class Income {
     }
 
     /**
-     * Returns if a given string is a valid income.
+     * Returns true if a given string is a valid income.
      */
     public static boolean isValidIncome(String test) {
         return test.matches(INCOME_VALIDATION_REGEX);
