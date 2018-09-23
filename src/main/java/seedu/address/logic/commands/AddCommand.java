4678fc829b4574507cf797bcfedd914c633ebfe7
LowGinWee <notginwee@gmail.com>
2018-09-23 13:34:55 +0800

    Updated Addcommand message usage

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -29,7 +29,7 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_NOTE + "John is forgetful. "
+            + PREFIX_NOTE + "John is forgetful "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
