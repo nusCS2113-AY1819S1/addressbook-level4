77167be5faef41d0e92ee5510a28be28d9bc64c8
Vincent <vencintgamer@gmail.com>
2018-09-24 23:00:35 +0800

    Added a new parser to parse the arguments for List Command

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,10 +1,12 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_RECORDS;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
+import seedu.address.model.record.Date;
 
 /**
  * Lists all records in the address book to the user.
@@ -13,8 +15,21 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
+    public static final int ARG_COUNT = 2;
+
+    public static final String MESSAGE_USAGE = "Lists all records within a specific time period. "
+            + "Parameters: "
+            + PREFIX_DATE + "START_DATE END_DATE";
+
     public static final String MESSAGE_SUCCESS = "Listed all records";
 
+    private final Date startDate;
+    private final Date endDate;
+
+    public ListCommand(Date startDate, Date endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
---
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -0,0 +1,50 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.record.Date;
+
+/**
+ * Parses input arguments and creates a new ListCommand object
+ */
+public class ListCommandParser implements Parser<ListCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ListCommand
+     * and returns an ListCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+
+    public ListCommand parse(String args) throws ParseException{
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_DATE);
+        if (!arePrefixesPresent(argMultimap, PREFIX_DATE) || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
+        }
+        String dateIntervalString = argMultimap.getValue(PREFIX_DATE).get();
+        String[] argList = splitByWhitespace(dateIntervalString);
+        if (argList.length != ListCommand.ARG_COUNT) {
+            throw new ParseException((String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE)));
+        }
+        Date startDate = ParserUtil.parseDate(argList[0]);
+        Date endDate = ParserUtil.parseDate(argList[1]);
+        return new ListCommand(startDate, endDate);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+    private static String[] splitByWhitespace(String args) {
+        String[] argList = args.split("\\s+");
+        return argList;
+    }
+}
