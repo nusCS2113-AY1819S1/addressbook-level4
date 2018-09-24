846e771c65cc06716656f142204df6de8262df5a
Lunastryke <35793588+Lunastryke@users.noreply.github.com>
2018-09-24 14:16:40 +0800

    FoodName.java: Added Food Name class

--- src/main/java/seedu/address/model/foodItem/exceptions/DuplicateFoodItemException.java
+++ src/main/java/seedu/address/model/foodItem/exceptions/DuplicateFoodItemException.java
@@ -1,13 +1,10 @@
 package seedu.address.model.foodItem.exceptions;
 
-public class DuplicateFoodItemException {
-    /**
-     * Signals that the operation will result in duplicate Food Items (Food Items are considered duplicates if
-     * they have the same identity).
-     */
-    public class DuplicateFoodItemException extends RuntimeException {
-        public DuplicateFoodItemException() {
-            super("Operation would result in duplicate food items");
-        }
-    }
+/**
+ * Signals that the operation will result in duplicate Food Items (Food Items are considered duplicates if
+ * they have the same identity).
+ */
+
+public class DuplicateFoodItemException extends RuntimeException {
+    public DuplicateFoodItemException() { super("Operation would result in duplicate persons"); }
 }
