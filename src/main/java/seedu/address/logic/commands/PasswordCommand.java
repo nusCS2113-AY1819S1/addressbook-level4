be6270a5cbe67df879a9c676529557ecb6302b30
lws803 <lws803@gmail.com>
2018-09-29 02:02:51 +0800

    Fix travis

--- src/main/java/seedu/address/logic/commands/PasswordCommand.java
+++ src/main/java/seedu/address/logic/commands/PasswordCommand.java
@@ -1,22 +1,28 @@
 package seedu.address.logic.commands;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.commons.util.FileEncryptor;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.util.SampleDataUtil;
 import seedu.address.storage.XmlAddressBookStorage;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * Encrypts the XML data using a password and returns a message
  * Message will be displayed on CommandResult
  */
 public class PasswordCommand extends Command {
+    private static final Logger logger = LogsCenter.getLogger(PasswordCommand.class);
+
     public static final String COMMAND_WORD = "password";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Hashes file using password.\n"
@@ -47,9 +53,9 @@ public class PasswordCommand extends Command {
             initialData = storage.readAddressBook().orElseGet(SampleDataUtil::getSampleAddressBook);
             model.resetData(initialData);
         } catch (IOException ioe) {
-
-        } catch (DataConversionException data) {
-
+            logger.warning(ioe.getMessage());
+        } catch (DataConversionException dataE) {
+            logger.warning(dataE.getMessage());
         }
 
         return new CommandResult(this.message);
