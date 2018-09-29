b4db6e03a5bd71a5c55226d3d914027813c4cfab
LiuTianhang <E0175471@u.nus.edu>
2018-09-29 14:09:21 +0800

    Revert "Add ingredient class" (#10)
    * Ingredient, IngredientName.java: added ingredient class and its name class
    
    * IngredientName.java: fixed constructor name
    
    * Ingredient.java: added javadoc comment for Ingredient class
    
    * IngredientQuantity.java: added ingredient quantity class
    
    * IngredientQuantity.java: fixed indentation
    
    * Ingredient.java: added ingredient class, modelled after Person class in AB4
    
    * IngredientQuantity.java: set quantity field to non-final
    
    * IngredientName.java: tidied up naming of parameters
    
    * IngredientQuantity.java: changed access modifier of the quantity value, added getter and setter (for now) methods
    
    * Ingredient exceptions: duplicate and not found exceptions added
    
    * Ingredients: placed all ingredient-related classes and exceptions in separate package
    
    * IngredientName.java: fixed equals method instanceof IngredientName instead of Name
    
    * RecipeName.java: added recipe name class
    
    * Ingredient.java: made ingredient class abstract
    
    * IceCream.java, Topping.java: added ice cream and topping class; these are inherited from Ingredient class
    
    * edited import statements
    
    * IngredientName.java: added space to try to fulfill checkstyle requriements
    
    * Ingredient, IceCream, Topping.java: added constructor that takes in only name; quantity field is has IngredientQuantity object with 0 value
    
    *  IngredientName.java: tried to fix import statements style for checkstyle

---
+++ src/main/java/seedu/address/model/ingredient/IceCream.java
@@ -0,0 +1,36 @@
+package seedu.address.model.ingredient;
+
+import java.util.Objects;
+
+/**
+ * Represents an ice cream flavour in a food item or recipe
+ */
+public class IceCream extends Ingredient {
+    public IceCream(IngredientName name, IngredientQuantity quantity) {
+        super(name, quantity);
+    }
+
+    public IceCream(IngredientName name) {
+        super(name);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+
+        if (!(other instanceof IceCream)) {
+            return false;
+        }
+
+        IceCream otherIceCream = (IceCream) other;
+        return otherIceCream.getName().equals(getName())
+                && otherIceCream.getQuantity() == getQuantity();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, quantity);
+    }
+}
