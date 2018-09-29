f1e8fa9f445f3ca5a1e5a889251e4f2446059454
sxqnicholas96 <37098263+sxqnicholas96@users.noreply.github.com>
2018-09-29 18:45:13 +0800

    Resolved Travis build error

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -42,7 +42,6 @@ public class AddressBookParser {
         final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
         if (!matcher.matches()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
-            
         }
 
         final String commandWord = matcher.group("commandWord");
---
+++ src/main/java/seedu/address/model/candidate/Gender.java
@@ -0,0 +1,5 @@
+package seedu.address.model.candidate;
+
+public class Gender {
+    
+}
