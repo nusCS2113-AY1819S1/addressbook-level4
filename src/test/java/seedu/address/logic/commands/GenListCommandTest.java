0164d3c57df1e71cbe476d17c39b1e03f372a194
DESKTOP-3LC2JA7\Novin <e0176909@u.nus.edu>
2018-10-01 14:36:19 +0800

    fix issue with null pointer exception on attendanceStage

--- src/test/java/seedu/address/logic/commands/GenListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/GenListCommandTest.java
@@ -1,37 +1,23 @@
 package seedu.address.logic.commands;
 
-
 import org.junit.Test;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
 
-import static org.junit.Assert.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
 
 /**
  * Contains integration tests (interaction with the Model) for {@code GenListCommand}.
  */
 public class GenListCommandTest {
-    Model model = new ModelManager(getTypicalAddressBook(),new UserPrefs());
+
     private CommandHistory commandHistory = new CommandHistory();
 
     @Test
     public void execute_emptyObservableList_throwsCommandException() {
         Model model = new ModelManager();
         String expectedMessage = String.format(GenListCommand.MESSAGE_EMPTY_LIST);
-
-        GenListCommand genListCommand = new GenListCommand();
-        assertCommandFailure(genListCommand, model, commandHistory, expectedMessage);
-    }
-
-    @Test
-    public void execute_genList_success(){
-        GenListCommand genListCommand=new GenListCommand();
-        String expectedMessage=String.format(GenListCommand.MESSAGE_GENERATE_ATTENDANCE_LIST);
-        assertEquals(expectedMessage, GenListCommand.MESSAGE_GENERATE_ATTENDANCE_LIST);
+        assertCommandFailure(new GenListCommand(), model, commandHistory, expectedMessage);
     }
 }
