eec74a475ebe3cf9b99c6d853ef9776c36366103
jlang <jieliangang@gmail.com>
2018-10-02 00:50:39 +0800

    Fix test

--- src/test/java/seedu/address/testutil/TypicalEvents.java
+++ src/test/java/seedu/address/testutil/TypicalEvents.java
@@ -16,24 +16,24 @@ public class TypicalEvents {
     public static final Event EVENT_1 = new Event(
             new EventName("Test Event 1"),
             new Description("Test Description 1"),
-            LocalDate.parse("2018-9-28"),
-            LocalDate.parse("2018-11-9"),
+            LocalDate.of(2018,9,18),
+            LocalDate.of(2018,9,20),
             new Location("Test Location 1")
     );
 
     public static final Event EVENT_2 = new Event(
             new EventName("Test Event 2"),
             new Description("Test Description 2"),
-            LocalDate.parse("2018-9-28"),
-            LocalDate.parse("2018-11-9"),
+            LocalDate.of(2018,9,18),
+            LocalDate.of(2018,9,20),
             new Location("Test Location 2")
     );
 
     public static final Event EVENT_3 = new Event(
             new EventName("Test Event 3"),
             new Description("Test Description 3"),
-            LocalDate.parse("2018-9-28"),
-            LocalDate.parse("2018-11-9"),
+            LocalDate.of(2018,9,18),
+            LocalDate.of(2018,9,20),
             new Location("Test Location 3")
     );
 
