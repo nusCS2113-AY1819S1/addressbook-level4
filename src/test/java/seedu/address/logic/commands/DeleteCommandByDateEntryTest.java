d00c1e7cfee33481bb933f78cdd8aaa132f1096b
nguyenngoclinhchi <nguyenngoclinhchi@u.nus.edu>
2018-10-03 00:42:16 +0800

    v1.1: Modify the DeleteCommandByDateEntryTest

--- src/test/java/seedu/address/logic/commands/DeleteCommandByDateEntryTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandByDateEntryTest.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
 
 import org.junit.Test;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -8,6 +10,7 @@ import seedu.address.model.UserPrefs;
 import seedu.address.model.record.Date;
 import seedu.address.model.record.Record;
 import seedu.address.testutil.TypicalDates;
+import seedu.address.testutil.TypicalIndexes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,19 +24,11 @@ public class DeleteCommandByDateEntryTest {
 
     @Test
     public void execute_validDateUnfilteredList_success() {
-        Date dateToDelete = TypicalDates.DATE_FIRST_INDEX_DATE;
-        List<Record> records = model.getFilteredRecordList();
+        List<Record> recordsToDelete = listAllDateRecord(model, TypicalDates.DATE_FIRST_INDEX_DATE);
 
-        List<Record> recordsToDelete = new ArrayList<>();
-        for (Record recordToDelete : records){
-            if (dateToDelete == recordToDelete.getDate()){
-                recordsToDelete.add(recordToDelete);
-                recordsToDelete.
-            }
-        }
-        DeleteCommandByDateEntry deleteCommandByDateEntry = new DeleteCommandByDateEntry(dateToDelete);
+        DeleteCommandByDateEntry deleteCommandByDateEntry = new DeleteCommandByDateEntry(TypicalDates.DATE_FIRST_INDEX_DATE);
 
-        String expectedMessage = String.format(DeleteCommandByDateEntry.MESSAGE_DELETE_RECORD_SUCCESS, dateToDelete);
+        String expectedMessage = String.format(DeleteCommandByDateEntry.MESSAGE_DELETE_RECORD_SUCCESS, TypicalDates.DATE_FIRST_INDEX_DATE);
 
         ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
         expectedModel.deleteListRecord(recordsToDelete);
@@ -45,9 +40,38 @@ public class DeleteCommandByDateEntryTest {
 
     @Test
     public void execute_invalidIndexFilteredList_throwsCommandException(){
-        CommandTestUtil.showRecordAtIndex();
+        List<Record> recordsToDelete = listAllDateRecord(model, TypicalDates.DATE_FIRST_INDEX_DATE);
+        for (int i = 0; i < recordsToDelete.size() - 1; i++){
+            List<Record> records = filteredRecordList(model);
+            //TODO: you have to create another method almost same as (CommandTestUtils.showRecordAtIndex)
+            Index targetIndex = new Index(records.indexOf(recordsToDelete.get(i)));
+            CommandTestUtil.showRecordAtIndex(model, targetIndex);
+
+            Index outOfBoundIndex = TypicalIndexes.INDEX_LAST_RECORD;
+            //ensure that outOfBoundIndex is still in bound of address book list
+            assertTrue(outOfBoundIndex.getOneBased() < model.getAddressBook().getRecordList().size());
+
+            DeleteCommandByDateEntry deleteCommandByDateEntry = new DeleteCommandByDateEntry
+                                                                (records.get(outOfBoundIndex.getZeroBased()).getDate());
+            CommandTestUtil.assertCommandFailure(deleteCommandByDateEntry, model, commandHistory, Messages.MESSAGE_INVALID_RECORD_DISPLAYED_DATE);
+        }
     }
 
+
+    public List<Record> filteredRecordList(Model model){
+        return model.getFilteredRecordList();
+    }
+
+    public List<Record> listAllDateRecord (Model model, Date dateToDelete){
+        List<Record> records = filteredRecordList(model);
+        List<Record> recordsToDelete = new ArrayList<>();
+        for (Record recordToDelete : records){
+            if (dateToDelete == recordToDelete.getDate()){
+                recordsToDelete.add(recordToDelete);
+            }
+        }
+        return recordsToDelete;
+    }
     public void showNoRecord (Model model){
         model.updateFilteredRecordList(p -> false);
         assertTrue (model.getFilteredRecordList().isEmpty());
