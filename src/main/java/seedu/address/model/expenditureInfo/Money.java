5af94b89e4d750949c566ef7a1114785d08e19c1
Shining <wangshining814@gmail.com>
2018-10-04 01:48:24 +0800

    Expenditure Tracker V1.1 update

---
+++ src/main/java/seedu/address/model/expenditureInfo/Money.java
@@ -0,0 +1,35 @@
+package seedu.address.model.expenditureInfo;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class Money {
+    public static final String MESSAGE_MONEY_CONSTRAINTS =
+            "Dates should only contain alphanumeric characters and spaces, and it should not be blank";
+    public final String addingMoney;
+
+    public Money(String money) {
+        requireNonNull(money);
+        checkArgument(isValidMoney(money), MESSAGE_MONEY_CONSTRAINTS);
+        addingMoney = money;
+    }
+    public static final String MONEY_VALIDATION_REGEX = "[\\p{Punct}][\\d{1,}]";
+    public static boolean isValidMoney(String test) {
+        return test.matches(MONEY_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() { return addingMoney; }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Date // instanceof handles nulls
+                && addingMoney.equals(((Money) other).addingMoney)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return addingMoney.hashCode();
+    }
+}
