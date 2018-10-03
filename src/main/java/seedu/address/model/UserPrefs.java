78cfd294a0067acbd692e0fe9342cb5468c24719
QzSG <quantumparadoxes@gmail.com>
2018-10-03 16:21:49 +0800

    Model: Add backup property to UserPrefs model
    Add backup file path to user preferences model.
    
    Modified equals to compare backup location
    
    Modified string builder to include backup path

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -13,6 +13,7 @@ public class UserPrefs {
 
     private GuiSettings guiSettings;
     private Path addressBookFilePath = Paths.get("data" , "addressbook.xml");
+    private Path addressBookBackupFilePath = Paths.get("data" , "addressbook.bak");
 
     public UserPrefs() {
         setGuiSettings(500, 500, 0, 0);
@@ -38,6 +39,14 @@ public class UserPrefs {
         this.addressBookFilePath = addressBookFilePath;
     }
 
+    public Path getAddressBookBackupFilePath() {
+        return addressBookFilePath;
+    }
+
+    public void setAddressBookBackupFilePath(Path addressBookBackupFilePath) {
+        this.addressBookBackupFilePath = addressBookBackupFilePath;
+    }
+
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -50,12 +59,13 @@ public class UserPrefs {
         UserPrefs o = (UserPrefs) other;
 
         return Objects.equals(guiSettings, o.guiSettings)
-                && Objects.equals(addressBookFilePath, o.addressBookFilePath);
+                && Objects.equals(addressBookFilePath, o.addressBookFilePath)
+                && Objects.equals(addressBookBackupFilePath, o.addressBookBackupFilePath);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(guiSettings, addressBookFilePath);
+        return Objects.hash(guiSettings, addressBookFilePath, addressBookBackupFilePath);
     }
 
     @Override
@@ -63,6 +73,7 @@ public class UserPrefs {
         StringBuilder sb = new StringBuilder();
         sb.append("Gui Settings : " + guiSettings.toString());
         sb.append("\nLocal data file location : " + addressBookFilePath);
+        sb.append("\nLocal data backup file location : " + addressBookBackupFilePath);
         return sb.toString();
     }
 
