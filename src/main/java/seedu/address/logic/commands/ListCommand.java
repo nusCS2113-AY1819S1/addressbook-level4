18e3ae056b6cf5ad0d416b2da29e3a135996ed0e
m-aslam-mj2 <e0176811@u.nus.edu>
2018-10-03 21:28:06 +0800

    renamed 'person' to 'student' in student management classes.
    Signed-off-by: m-aslam-mj2 <e0176811@u.nus.edu>

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -13,7 +13,7 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "student_list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all students";
 
 
     @Override
