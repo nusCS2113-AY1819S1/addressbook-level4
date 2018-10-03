a609348bf98d0d80463e349cba4755c72f253081
amirulmaricar <Amirul@comp.nus.edu.sg>
2018-10-04 00:25:56 +0800

    Password class

--- src/main/java/seedu/address/model/login/Password.java
+++ src/main/java/seedu/address/model/login/Password.java
@@ -1,7 +1,34 @@
 package seedu.address.model.login;
 
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 public class Password {
 
-    public static final String MESSAGE_PASSWORD_CONSTRAINTS = "Cons";
+    public static final String MESSAGE_PASSWORD_CONSTRAINTS =
+            "Password only accepts alphanumeric characters, not empty and no spaces";
+
+    public static final String PASSWORD_VALIDATION_REGEX = "[\\p{Alnum}]*";
+    public final String password;
+
+    public Password(String password) {
+        requireNonNull(password);
+        checkArgument(isValidPassword(password), MESSAGE_PASSWORD_CONSTRAINTS);
+        this.password = password;
+    }
+
+    public static boolean isValidPassword(String test) {
+        return test.matches(PASSWORD_VALIDATION_REGEX) && !test.equals("");
+    }
+
+    @Override
+    public String toString() {
+        return password;
+    }
+
+
+
+
+
 }
