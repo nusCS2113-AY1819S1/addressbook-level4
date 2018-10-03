989e47781b80c089acb7c2c9cec8b9a6895a5e0a
OscarZeng <zenghaonus@outlook.com>
2018-10-03 21:43:56 +0800

    minor changes to pass the tests.

--- src/main/java/seedu/address/logic/commands/LimitCommand.java
+++ src/main/java/seedu/address/logic/commands/LimitCommand.java
@@ -1,21 +1,19 @@
 package seedu.address.logic.commands.exceptions;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 import seedu.address.model.record.Limit;
-import seedu.address.model.record.Record;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
 
 
 public class LimitCommand extends Command {
     public static final String COMMAND_WORD= "limit";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Set a limit for a period of time. "
+    public static final String MESSAGE_USAGE= COMMAND_WORD + ": Set a limit for a period of time. "
             + "Parameters: "
             + PREFIX_DATE + "DATE_START "
             + PREFIX_DATE + "DATE_END "
@@ -28,16 +26,16 @@ public class LimitCommand extends Command {
             + PREFIX_MONEYFLOW + "100 ";
 
     //public static Limit limit;
-    public static Limit limit;
+    private static Limit limit;
 
 
     public static final String MESSAGE_SUCCESS = "Limit has been set: %1$s";
 
     public static final String MESSAGE_DUPLICATE_LIMIT = "There is already a limit for this period ";
 
-    public  LimitCommand (Limit limitin){
+    public  LimitCommand (Limit limitin) {
         requireNonNull(limitin);
-        limit=limitin;
+        limit= limitin;
     }
 
     @Override
