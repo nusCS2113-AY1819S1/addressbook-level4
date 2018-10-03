24f565e849412dee66e00f1067638a600bfafe6e
arty9 <tayrantian@gmail.com>
2018-10-04 01:07:04 +0800

    added Task and related classes

---
+++ src/main/java/seedu/address/model/task/Task.java
@@ -0,0 +1,86 @@
+package seedu.address.model.task;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Objects;
+
+/**
+ * Represents a Person in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Task {
+
+    // Data fields
+    private final TaskName name;
+    private final TaskModule module;
+    private final TaskDate date;
+    private final TaskPriority priority;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Task(TaskName name, TaskModule module, TaskDate date, TaskPriority priority) {
+        requireAllNonNull(name, module, date, priority);
+        this.name = name;
+        this.module = module;
+        this.date = date;
+        this.priority = priority;
+    }
+
+    public TaskName getName() {
+        return name;
+    }
+
+    public TaskModule getModule() {
+        return module;
+    }
+
+    public TaskDate getDate() {
+        return date;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    /**
+     * Returns true if both persons have the same identity and data fields.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Task)) {
+            return false;
+        }
+
+        Task otherPerson = (Task) other;
+        return otherPerson.getName().equals(getName())
+                && otherPerson.getModule().equals(getModule())
+                && otherPerson.getDate().equals(getDate())
+                && otherPerson.getPriority().equals(getPriority());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, module, date, priority);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName())
+                .append(" Module: ")
+                .append(getModule())
+                .append(" Date: ")
+                .append(getDate())
+                .append(" Priority: ")
+                .append(getPriority());
+        return builder.toString();
+    }
+
+}
---
+++ src/main/java/seedu/address/model/task/TaskDate.java
@@ -0,0 +1,60 @@
+package seedu.address.model.task;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Task's deadline date in the to-do list.
+ * Guarantees: immutable; is valid as declared in {@link #isValidDate(String)}
+ */
+public class TaskDate {
+
+    private static final String SPECIAL_CHARACTERS = "!#$%&'*+/=?`{|}~^.-";
+    public static final String MESSAGE_DATE_CONSTRAINTS =
+            "Dates should be of the format DD-MM, where DD and MM are numbers";
+    // alphanumeric and special characters
+    private static final String LOCAL_PART_REGEX = "^[\\w" + SPECIAL_CHARACTERS + "]+";
+    private static final String DOMAIN_FIRST_CHARACTER_REGEX = "[^\\W_]"; // alphanumeric characters except underscore
+    private static final String DOMAIN_MIDDLE_REGEX = "[a-zA-Z0-9.-]*"; // alphanumeric, period and hyphen
+    private static final String DOMAIN_LAST_CHARACTER_REGEX = "[^\\W_]$";
+    public static final String DATE_VALIDATION_REGEX = LOCAL_PART_REGEX + "@"
+            + DOMAIN_FIRST_CHARACTER_REGEX + DOMAIN_MIDDLE_REGEX + DOMAIN_LAST_CHARACTER_REGEX;
+
+    public final String value;
+
+    /**
+     * Constructs an {@code TaskDate}.
+     *
+     * @param date A valid date.
+     */
+    public TaskDate(String date) {
+        requireNonNull(date);
+        checkArgument(isValidDate(date), MESSAGE_DATE_CONSTRAINTS);
+        value = date;
+    }
+
+    /**
+     * Returns if a given string is a valid email.
+     */
+    public static boolean isValidDate(String test) {
+        return test.matches(DATE_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TaskDate // instanceof handles nulls
+                && value.equals(((TaskDate) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
