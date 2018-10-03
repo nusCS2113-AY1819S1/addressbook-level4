389c9772443d7bb372074eb696b1066d13a64b70
lekoook <kokteng1313@gmail.com>
2018-10-03 17:58:18 +0800

    Add filter functionality to list command
    List command can now filter and list the contacts according to the
    specified given flags.
    
    Currently, the flags does nothing and they default to listing all
    contacts.

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -3,11 +3,15 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 import seedu.address.commons.util.FileEncryptor;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.CliSyntax;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 
 /**
  * Lists all persons in the address book to the user.
@@ -16,19 +20,40 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = CliSyntax.COMMAND_LIST;
 
+    public static final int TYPE_ALL = 0;
+    public static final int TYPE_TAG = 1;
+
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
+    /**
+     * Instance variables
+     */
+    private int listType;
+    private List<String> predicatesList;
+
+    public ListCommand(int listType, List<String> predicatesList) {
+        this.listType = listType;
+        this.predicatesList = predicatesList;
+    }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
         FileEncryptor fe = new FileEncryptor("data/addressbook.xml");
+        Predicate<Person> predicateToUse;
 
         if (fe.isLocked()) {
             throw new CommandException(FileEncryptor.MESSAGE_ADDRESS_BOOK_LOCKED);
         }
 
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        switch(listType) {
+        case TYPE_TAG:
+            System.out.println("tag");
+        default:
+            predicateToUse = PREDICATE_SHOW_ALL_PERSONS;
+        }
+
+        model.updateFilteredPersonList(predicateToUse);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
---
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -0,0 +1,26 @@
+package seedu.address.logic.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class ListCommandParser implements Parser<ListCommand> {
+
+    @Override
+    public ListCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, CliSyntax.PREFIX_NAME, CliSyntax.PREFIX_TAG);
+
+        if (argMultimap.getValue(CliSyntax.PREFIX_TAG).isPresent()) {
+            return new ListCommand(ListCommand.TYPE_TAG, extractArgs(argMultimap.getValue(CliSyntax.PREFIX_TAG)));
+        } else {
+            return new ListCommand(ListCommand.TYPE_ALL, new ArrayList<>());
+        }
+    }
+
+    private List<String> extractArgs(Optional<String> args) {
+        return new ArrayList<>();
+    }
+}
