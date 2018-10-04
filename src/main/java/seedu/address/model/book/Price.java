5281fa0958726d6267648f1592843815e84ebf24
iashcole <teo.ashley96@gmail.com>
2018-10-04 23:47:51 +0800

    refactored and updated price class

--- src/main/java/seedu/address/model/book/Price.java
+++ src/main/java/seedu/address/model/book/Price.java
@@ -5,22 +5,15 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a Book's email in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
+ * Guarantees: immutable; is valid as declared in {@link #isValidPhone(String)}
  */
 public class Price {
 
-    public static final String MESSAGE_EMAIL_CONSTRAINTS = "Emails should be of the format local-part@domain "
-            + "and adhere to the following constraints:\n"
-            + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
-            + "the parentheses, (" + SPECIAL_CHARACTERS + ") .\n"
-            + "2. This is followed by a '@' and then a domain name. "
-            + "The domain name must:\n"
-            + "    - be at least 2 characters long\n"
-            + "    - start and end with alphanumeric characters\n"
-            + "    - consist of alphanumeric characters, a period or a hyphen for the characters in between, if any.";
+    public static final String MESSAGE_PRICE_CONSTRAINTS = "Prices should be numerical and in 2 decimal places or none at all\n"
+            + "E.g. $4, $3.02";
      // "$" can be omitted and is optional, prices can be in 2 decimal places or none at all
      // e.g. $4 or $3.02 is accepted
-     public static final String EMAIL_VALIDATION_REGEX = "(\\$)?\\d+(\\.\\d{2})?";
+     public static final String PRICE_VALIDATION_REGEX = "(\\$)?\\d+(\\.\\d{2})?";
 
     public final String value;
 
@@ -31,15 +24,15 @@ public class Price {
      */
     public Price(String email) {
         requireNonNull(email);
-        checkArgument(isValidEmail(email), MESSAGE_EMAIL_CONSTRAINTS);
+        checkArgument(isValidPhone(email), MESSAGE_PRICE_CONSTRAINTS);
         value = email;
     }
 
     /**
      * Returns if a given string is a valid email.
      */
-    public static boolean isValidEmail(String test) {
-        return test.matches(EMAIL_VALIDATION_REGEX);
+    public static boolean isValidPhone(String test) {
+        return test.matches(PRICE_VALIDATION_REGEX);
     }
 
     @Override
