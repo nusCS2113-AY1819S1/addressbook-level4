e47457fc227abbda12cd17db00a91bc35324ff46
jamesyaputra <jamesyaputra1704@gmail.com>
2018-10-04 17:55:15 +0800

    Added LoginCommand skeleton

--- src/main/java/seedu/address/logic/commands/LoginCommand.java
+++ src/main/java/seedu/address/logic/commands/LoginCommand.java
@@ -1,11 +1,41 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.event.Event;
 
 /**
  * Logs user into the Event Manager Application
  */
 public class LoginCommand extends Command {
+
     public static final String COMMAND_WORD = "login";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Logs user into Event Manager. "
+            + "Parameters: "
+            + PREFIX_USERNAME + "USERNAME "
+            + PREFIX_PASSWORD + "PASSWORD";
+
+    public static final String MESSAGE_SUCCESS = "Logged in successfully!";
+    public static final String MESSAGE_FAILURE = "Incorrect account credentials!";
+
+    private final Event toLogin;
+
+    /**
+     * Creates an LoginCommand to add the specified {@code Event}
+     */
+    public LoginCommand(Event event) {
+        requireNonNull(event);
+        toLogin = event;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+
+    }
 }
--- src/main/java/seedu/address/storage/JsonAccountInfoStorage.java
+++ src/main/java/seedu/address/storage/JsonAccountInfoStorage.java
@@ -4,5 +4,5 @@ package seedu.address.storage;
  * Stores information of user accounts (user-names and passwords) in a JSON file
  */
 public class JsonAccountInfoStorage {
-    
+
 }
