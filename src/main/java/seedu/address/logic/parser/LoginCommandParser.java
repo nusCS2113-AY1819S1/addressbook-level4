26f310ad8455953d093b71101e6fc992f84f7be5
jamesyaputra <jamesyaputra1704@gmail.com>
2018-10-04 18:05:45 +0800

    Added LoginCommandParser

--- src/main/java/seedu/address/logic/parser/LoginCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LoginCommandParser.java
@@ -1,7 +1,16 @@
 package seedu.address.logic.parser;
 
 import seedu.address.logic.commands.LoginCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 
 public class LoginCommandParser implements Parser<LoginCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments in the context of the LoginCommand
+     * and returns an LoginCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public LoginCommand parse(String args) throws ParseException {
+
+    }
 }
