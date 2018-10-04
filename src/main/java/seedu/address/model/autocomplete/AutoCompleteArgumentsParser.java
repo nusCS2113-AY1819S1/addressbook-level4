420e56551591017870a40a54f9ab34e1a071923b
lekoook <kokteng1313@gmail.com>
2018-10-04 15:22:04 +0800

    Add text prediction support for list command
    Tag text prediction is now support for the list command.
    Using the 't/' flag with list command, user is now about to predict
    the tag to be typed out.

--- src/main/java/seedu/address/model/autocomplete/AutoCompleteArgumentsParser.java
+++ src/main/java/seedu/address/model/autocomplete/AutoCompleteArgumentsParser.java
@@ -1,6 +1,12 @@
 //@@author lekoook
 package seedu.address.model.autocomplete;
 
+import static seedu.address.logic.parser.CliSyntax.COMMAND_DELETE;
+import static seedu.address.logic.parser.CliSyntax.COMMAND_FIND;
+import static seedu.address.logic.parser.CliSyntax.COMMAND_IMPORT;
+import static seedu.address.logic.parser.CliSyntax.COMMAND_LIST;
+import static seedu.address.logic.parser.CliSyntax.COMMAND_MAIL;
+import static seedu.address.logic.parser.CliSyntax.COMMAND_SELECT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_COMMAND;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
@@ -11,7 +17,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
-import seedu.address.logic.parser.CliSyntax;
 import seedu.address.logic.parser.Prefix;
 
 /**
@@ -31,12 +36,14 @@ public class AutoCompleteArgumentsParser {
 
         // TODO: Add or remove command support as necessary
         switch(command) {
-            case CliSyntax.COMMAND_FIND:
+            case COMMAND_FIND:
                 return getFindParserPair(arguments, argMultimap);
-            case CliSyntax.COMMAND_SELECT:
-            case CliSyntax.COMMAND_DELETE:
-            case CliSyntax.COMMAND_IMPORT:
-            case CliSyntax.COMMAND_MAIL:
+            case COMMAND_LIST:
+                return getListParserPair(arguments, argMultimap);
+            case COMMAND_SELECT:
+            case COMMAND_DELETE:
+            case COMMAND_IMPORT:
+            case COMMAND_MAIL:
                 return getMailParserPair(arguments, argMultimap);
             default:
                 return new AutoCompleteParserPair(PREFIX_INVALID, arguments.trim());
@@ -44,7 +51,7 @@ public class AutoCompleteArgumentsParser {
     }
 
     /**
-     * Creates a AutoCompleteParserPair instance based on the last Prefix found in user input.
+     * Creates a AutoCompleteParserPair instance based on the last Prefix found in find command.
      * @param arguments the user input to search.
      * @param argMultimap used to retrieve the values of the prefix keys.
      * @return the appropriate AutoCompleteParserPair instance.
@@ -66,7 +73,7 @@ public class AutoCompleteArgumentsParser {
     }
 
     /**
-     * Creates a AutoCompleteParserPair instance based on the last Prefix found in user input.
+     * Creates a AutoCompleteParserPair instance based on the last Prefix found in mail command.
      * @param arguments the user input to search.
      * @param argMultimap used to retrieve the values of the prefix keys.
      * @return the appropriate AutoCompleteParserPair instance.
@@ -77,6 +84,23 @@ public class AutoCompleteArgumentsParser {
             return new AutoCompleteParserPair(lastPrefix, argMultimap.getValue(lastPrefix).get());
         }
         // Default text prediction is names
-        return  new AutoCompleteParserPair(PREFIX_NAME, arguments);
+        return new AutoCompleteParserPair(PREFIX_NAME, arguments);
+    }
+
+    /**
+     * Creates a AutoCompleteParserPair instance based on the last Prefix found in list command.
+     * @param arguments the user input to search.
+     * @param argMultimap used to retrieve the values of the prefix keys.
+     * @return the appropriate AutoCompleteParserPair instance.
+     */
+    private static AutoCompleteParserPair getListParserPair(String arguments, ArgumentMultimap argMultimap) {
+        Prefix lastPrefix = ArgumentTokenizer.findLastPrefix(
+                arguments,
+                PREFIX_TAG);
+        if (argMultimap.getValue(lastPrefix).isPresent()) {
+            return new AutoCompleteParserPair(lastPrefix, argMultimap.getValue(lastPrefix).get());
+        }
+        // Default text prediction is nothing
+        return new AutoCompleteParserPair(PREFIX_INVALID, arguments);
     }
 }
