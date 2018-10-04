e4c594f09ff510590235ce48c7d3b7098543c633
jamesyaputra <jamesyaputra1704@gmail.com>
2018-10-04 17:40:22 +0800

    Added CLI prefix syntax for login

--- src/main/java/seedu/address/logic/commands/LoginCommand.java
+++ src/main/java/seedu/address/logic/commands/LoginCommand.java
@@ -1,4 +1,11 @@
 package seedu.address.logic.commands;
 
-public class LoginCommand {
+import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
+
+/**
+ * Logs user into the Event Manager Application
+ */
+public class LoginCommand extends Command {
+    public static final String COMMAND_WORD = "login";
 }
--- src/main/java/seedu/address/storage/JsonAccountInfoStorage.java
+++ src/main/java/seedu/address/storage/JsonAccountInfoStorage.java
@@ -1,4 +1,8 @@
 package seedu.address.storage;
 
+/**
+ * Stores information of user accounts (user-names and passwords) in a JSON file
+ */
 public class JsonAccountInfoStorage {
+    
 }
