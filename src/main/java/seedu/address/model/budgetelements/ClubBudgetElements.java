c519a8d34a863824742efa729c7fdc065cb850a7
sanjukta99 <sanjuktasaha0906@gmail.com>
2018-10-05 11:38:49 +0800

    edited the ClubBudgetElements class

--- src/main/java/seedu/address/model/budgetelements/ClubBudgetElements.java
+++ src/main/java/seedu/address/model/budgetelements/ClubBudgetElements.java
@@ -1,83 +1,72 @@
-package seedu.address.model.person;
+package seedu.address.model.budgetelements;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
-
 import seedu.address.model.tag.Tag;
 
 /**
- * Represents a Person in the address book.
+ * Represents the budget elements of a club in the address book.
  * Guarantees: details are present and not null, field values are validated, immutable.
  */
-public class Person {
+public class ClubBudgetElements {
 
     // Identity fields
-    private final Name name;
-    private final Phone phone;
-    private final Email email;
+    private final ClubName clubname;
 
     // Data fields
-    private final Address address;
-    private final Set<Tag> tags = new HashSet<>();
+    private final ExpectedTurnout expectedturnout;
+    private final NumberOfEvents numberofevents;
+
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
-        this.tags.addAll(tags);
+    public ClubBudgetElements(ClubName clubname, ExpectedTurnout expectedturnout, NumberOfEvents numberofevents) {
+        requireAllNonNull(clubname, expectedturnout, numberofevents);
+        this.clubname = clubname;
+        this.expectedturnout = expectedturnout;
+        this.numberofevents = numberofevents;
     }
 
-    public Name getName() {
-        return name;
+    public ClubName getClubName() {
+        return clubname;
     }
 
-    public Phone getPhone() {
-        return phone;
+    public ExpectedTurnout getExpectedturnout() {
+        return expectedturnout;
     }
 
-    public Email getEmail() {
-        return email;
+    public NumberOfEvents getNumberofevents() {
+        return numberofevents;
     }
 
-    public Address getAddress() {
-        return address;
-    }
-
-    /**
-     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
-     * if modification is attempted.
-     */
-    public Set<Tag> getTags() {
-        return Collections.unmodifiableSet(tags);
-    }
 
     /**
      * Returns true if both persons of the same name have at least one other identity field that is the same.
      * This defines a weaker notion of equality between two persons.
      */
-    public boolean isSamePerson(Person otherPerson) {
-        if (otherPerson == this) {
-            return true; //memory when you compare between 2 objects
-        }
 
-        return otherPerson != null
+    /**
+    public boolean isSameClubBudgetElements(ClubBudgetElements otherClubBudgetElements) {
+        if (otherClubBudgetElements == this) {
+            return true;
+        }
+        // HERE !!!
+        return otherClubBudgetElements != null
                 && otherPerson.getName().equals(getName())
                 && (otherPerson.getPhone().equals(getPhone()) || otherPerson.getEmail().equals(getEmail()));
     }
+     */
 
     /**
      * Returns true if both persons have the same identity and data fields.
      * This defines a stronger notion of equality between two persons.
      */
+
+    /**
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -96,24 +85,22 @@ public class Person {
                 && otherPerson.getTags().equals(getTags());
     }
 
+     */
+
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(clubname, expectedturnout, numberofevents);
     }
 
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
-        builder.append(getName())
-                .append(" Phone: ")
-                .append(getPhone())
-                .append(" Email: ")
-                .append(getEmail())
-                .append(" Address: ")
-                .append(getAddress())
-                .append(" Tags: ");
-        getTags().forEach(builder::append);
+        builder.append(getClubName())
+                .append(" Expected Turnout: ")
+                .append(getExpectedturnout())
+                .append(" Number of Events: ")
+                .append(getNumberofevents());
         return builder.toString();
     }
 
