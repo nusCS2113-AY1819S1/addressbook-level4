ee88ebbc7ddd0f65d2c0e647c64b9f21ec9af1d6
MingXunSeah <mx196@hotmail.com>
2018-10-06 22:45:38 +0800

    no message

--- src/test/java/seedu/address/logic/parser/RecruitBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/RecruitBookParserTest.java
@@ -17,8 +17,8 @@ import org.junit.rules.ExpectedException;
 import seedu.address.logic.commands.AddCandidateCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
+import seedu.address.logic.commands.EditCandidateCommand;
+import seedu.address.logic.commands.EditCandidateCommand.EditPersonDescriptor;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
@@ -65,9 +65,9 @@ public class RecruitBookParserTest {
     public void parseCommand_edit() throws Exception {
         Candidate candidate = new PersonBuilder().build();
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(candidate).build();
-        EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
+        EditCandidateCommand command = (EditCandidateCommand) parser.parseCommand(EditCandidateCommand.COMMAND_WORD + " "
                 + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor));
-        assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
+        assertEquals(new EditCandidateCommand(INDEX_FIRST_PERSON, descriptor), command);
     }
 
     @Test
