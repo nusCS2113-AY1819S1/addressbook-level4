5a67e4cce7700eb0826c697f41e5879ddbef2378
tsuweiquan <e0176844@u.nus.edu>
2018-10-07 21:20:48 +0800

    Created DistributeCommand & DistributeCommandParser File

---
+++ src/main/java/seedu/address/logic/commands/DistributeCommand.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+public class DistributeCommand extends Command {
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        return null;
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/DistributeCommandParser.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.DistributeCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class DistributeCommandParser implements Parser<DistributeCommand>{
+    @Override
+    public DistributeCommand parse(String userInput) throws ParseException {
+        return null;
+    }
+}
