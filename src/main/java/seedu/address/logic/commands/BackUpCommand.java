7298356eb4ce57302c0d573c18a6033d06fd5500
Limminghong <32009215+Limminghong@users.noreply.github.com>
2018-10-07 20:32:25 +0800

    Changed the path to addressbook to the one in UserPrefs

--- src/main/java/seedu/address/logic/commands/BackUpCommand.java
+++ src/main/java/seedu/address/logic/commands/BackUpCommand.java
@@ -5,11 +5,13 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+import seedu.address.MainApp;
 import seedu.address.commons.util.FileEncryptor;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.CliSyntax;
 import seedu.address.model.Model;
+import seedu.address.model.UserPrefs;
 
 /**
  * Backs up a snapshot of the address book into a .backup folder.
@@ -20,13 +22,13 @@ public class BackUpCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Address book has been backed up!";
     public static final String ERROR = "error";
 
-    private static final String SOURCE_PATH = "data/addressbook.xml";
     private static final String DEST_PATH = ".backup";
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-        FileEncryptor fe = new FileEncryptor(SOURCE_PATH);
-        File source = new File(SOURCE_PATH);
+        UserPrefs userPrefs = new UserPrefs();
+        FileEncryptor fe = new FileEncryptor(userPrefs.getAddressBookFilePath().toString());
+        File source = new File(userPrefs.getAddressBookFilePath().toString());
         File backupDest = new File(DEST_PATH);
 
         if (fe.isLocked()) {
