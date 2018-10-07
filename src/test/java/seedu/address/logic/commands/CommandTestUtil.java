47f44f750ac06991dbd4c5aa822ebdadf7c19b68
tsuweiquan <e0176844@u.nus.edu>
2018-10-07 19:23:06 +0800

    Added Static String for Testcase

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -38,6 +38,12 @@ public class CommandTestUtil {
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
 
+    public static final String VALID_GENDER_MALE = "MALE";
+    public static final String VALID_GENDER_FEMALE = "FEMALE";
+    public static final String VALID_GENDER_ABBREVATION_MALE = "M";
+    public static final String VALID_GENDER_ABBREVATION_FEMALE = "F";
+
+
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
     public static final String PHONE_DESC_AMY = " " + PREFIX_PHONE + VALID_PHONE_AMY;
