30d3eb31f6eff646cf141fcfd189f8a69cc85613
sanjukta99 <sanjuktasaha0906@gmail.com>
2018-10-09 01:07:40 +0800

    Created an exception to deal with duplicates of clubs

---
+++ src/main/java/seedu/address/model/budgetelements/exceptions/DuplicateClubException.java
@@ -0,0 +1,11 @@
+package seedu.address.model.budgetelements.exceptions;
+
+/**
+ * Signals that the operation will result in duplicate Clubs (Clubs are considered duplicates if they have the same
+ * identity).
+ */
+public class DuplicateClubException extends RuntimeException{
+    public DuplicateClubException() {
+        super("Operation would result in duplicate clubs");
+    }
+}
