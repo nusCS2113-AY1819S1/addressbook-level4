7bf55c9a65f19710edc2eea5fd0745ef417c69a4
lws803 <lws803@gmail.com>
2018-10-08 16:20:36 +0800

    Travis fixes

--- src/main/java/seedu/address/commons/util/FileEncryptor.java
+++ src/main/java/seedu/address/commons/util/FileEncryptor.java
@@ -217,10 +217,11 @@ public class FileEncryptor {
      * @return
      */
     public boolean isAlphanumeric (String str) {
-        for (int i=0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if (c < 0x30 || (c >= 0x3a && c <= 0x40) || (c > 0x5a && c <= 0x60) || c > 0x7a)
+            if (c < 0x30 || (c >= 0x3a && c <= 0x40) || (c > 0x5a && c <= 0x60) || c > 0x7a) {
                 return false;
+            }
         }
         return true;
     }
