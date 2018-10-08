4799ebe52b34b53ffb985be2ab96caa26d288dda
amirulmaricar <Amirul@comp.nus.edu.sg>
2018-10-08 19:32:31 +0800

    Created Stubs for user authentication tests

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -2,11 +2,7 @@ package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,6 +21,16 @@ import seedu.address.testutil.EditPersonDescriptorBuilder;
  * Contains helper methods for testing commands.
  */
 public class CommandTestUtil {
+
+    public static final String VALID_LOGIN_USERNAME = "slap";
+    public static final String VALID_LOGIN_PASSWORD = "password";
+
+    public static final String LOGIN_VALID_DESC_USERNAME = " " + PREFIX_USERNAME + VALID_LOGIN_USERNAME;
+    public static final String LOGIN_VALID_DESC_PASSWORD = " " + PREFIX_PASSWORD + VALID_LOGIN_PASSWORD;
+    public static final String LOGIN_INVALID_DESC_USERNAME = " " + PREFIX_USERNAME + VALID_LOGIN_USERNAME + "@@";
+    public static final String LOGIN_INVALID_DESC_PASSWORD = " " + PREFIX_PASSWORD + VALID_LOGIN_PASSWORD + "@@";
+    public static final String NEWPASS_VALID_DESC_PASSWORD = " " + PREFIX_NEW_PASSWORD + VALID_LOGIN_PASSWORD + "AA";
+    public static final String NEWPASS_INVALID_DESC_PASSWORD = " " + PREFIX_NEW_PASSWORD + VALID_LOGIN_PASSWORD + "AA@";
 
     public static final String VALID_NAME_AMY = "Amy Bee";
     public static final String VALID_NAME_BOB = "Bob Choo";
