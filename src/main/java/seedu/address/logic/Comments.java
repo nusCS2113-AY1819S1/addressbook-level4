c87e1cc491ac24b1bc1155d375989f7e9c2bb383
Unknown <geraldcdx5@gmail.com>
2018-10-09 17:35:09 +0800

    Added new functions to add comments and delete new comments from HTML

--- src/main/java/seedu/address/logic/Comments.java
+++ src/main/java/seedu/address/logic/Comments.java
@@ -1,19 +1,29 @@
 package seedu.address.logic;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Tag;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.Vector;
 
 /**
  * Adds comments section.
  */
 public class Comments {
 
+    public static Vector v;
+    public static String input;
+
     /**
      *  Appends comment to the end of the current Comment Section of index
      */
-    public void addComment(String comment, int index) {
-
+    public void addComment(String comment) {
+        v.add(comment);
+        rewrite(v,input);
     }
 
     /**
@@ -26,15 +36,111 @@ public class Comments {
     /**
      *  Admin only: Can delete comment given event Comment Section indexx and Line
      */
-    public void deleteComment(int index, int line) {
-
+    public void deleteComment(int line) {
+        v.remove(line);
+        rewrite(v,input);
     }
 
+    /**
+     *  Admin only: Can delete comment given event Comment Section indexx and Line
+     */
+    public Vector parseCommentSection(String in) {
+        String input = in;
+        Vector v= new Vector();
+        Document htmlfile=null;
+        try {
+            htmlfile = Jsoup.parse(new File(input),null);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        Element element = htmlfile.select("div").first();
+        Elements divChildren = element.children();
+
+        Elements detachedDivChildren = new Elements();
+        for (Element elem : divChildren) {
+            Element detachedChild = new Element(Tag.valueOf(elem.tagName()),
+                    elem.baseUri(), elem.attributes().clone());
+            detachedDivChildren.add(detachedChild);
+        }
+        for (Element elem : divChildren) {
+            v.add(elem.ownText());
+            System.out.println(elem.ownText());
+        }
+        return v;
+    }
+
+    /**
+     *  Admin only: Can delete comment given event Comment Section indexx and Line
+     */
+    public static void rewrite(Vector v, String input) {
+        String commentSection="<span>Comment Section</span><div>";
+        for(int i=0;i<v.size();i++) {
+            commentSection+="<p>"+v.get(i)+"</p>";
+        }
+        commentSection+="</div>";
+        File savingFile = new File(input);
+        FileOutputStream fop = null;
+        try {
+            fop = new FileOutputStream(savingFile);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            fop.write(commentSection.getBytes());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            fop.flush();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            fop.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Test code.
      */
     public static void main(String[] args) {
         Comments comment = new Comments();
+        input = "C:/Users/Gerald/Desktop/test/1.html";
+        Document htmlfile=null;
+        try {
+            htmlfile = Jsoup.parse(new File(input),null);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        v = comment.parseCommentSection(input);
+
+        while(true){
+            System.out.println("What Comment do you want to add?");
+            Scanner SCANNER = new Scanner(System.in);
+            String username = SCANNER.nextLine();
+
+            try {
+                Integer.parseInt(username);
+                comment.deleteComment(Integer.parseInt(username));
+                System.out.println("Comment at " + username + "deleted");
+            } catch(NumberFormatException e) {
+                comment.addComment(username);
+                System.out.println("Comment added!!");
+            }
+
+
+
+
+        }
+
+
     }
 }
