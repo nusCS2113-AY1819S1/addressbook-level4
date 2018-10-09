86fd8081eba1a31c72208913dc11c9bcc5f2b027
Unknown <geraldcdx5@gmail.com>
2018-10-09 16:22:48 +0800

    Gradle error solved

--- src/main/java/seedu/address/logic/commands/ReplyCommentCommand.java
+++ src/main/java/seedu/address/logic/commands/ReplyCommentCommand.java
@@ -1,7 +1,9 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_COMMENT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LINE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.List;
 
