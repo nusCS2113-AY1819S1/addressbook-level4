b6eb0e5d1185a7f382d2b30fd46c6f0a8bb24ccf
Unknown <geraldcdx5@gmail.com>
2018-10-09 16:42:43 +0800

    Adding commands into Comment class

---
+++ src/main/java/seedu/address/logic/Comments.java
@@ -0,0 +1,40 @@
+package seedu.address.logic;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+/**
+ * Adds comments section.
+ */
+public class Comments {
+
+    /**
+     *  Appends comment to the end of the current Comment Section of index
+     */
+    public void addComment(String comment, int index) {
+
+    }
+
+    /**
+     *  Replies with the comment to event Comment section of index and line
+     */
+    public void replyComment(String comment, int index, int line) {
+
+    }
+
+    /**
+     *  Admin only: Can delete comment given event Comment Section indexx and Line
+     */
+    public void deleteComment(int index, int line) {
+
+    }
+
+
+    /**
+     * Test code.
+     */
+    public static void main(String[] args) {
+        Comments comment = new Comments();
+    }
+}
