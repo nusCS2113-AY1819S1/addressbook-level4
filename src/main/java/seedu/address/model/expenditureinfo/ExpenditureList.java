cc956ecaca20bfeefe444f19ea76cd4bc77aa85c
SHININGGGG <wangshining814@gmail.com>
2018-10-10 23:57:41 +0800

    checkstyle update

--- src/main/java/seedu/address/model/expenditureinfo/ExpenditureList.java
+++ src/main/java/seedu/address/model/expenditureinfo/ExpenditureList.java
@@ -8,10 +8,21 @@ import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import seedu.address.model.person.Person;
 //import seedu.address.model.person.exceptions.DuplicatePersonException;
 //import seedu.address.model.person.exceptions.PersonNotFoundException;
 
-
+/**
+ * A list of persons that enforces uniqueness between its elements and does not allow nulls.
+ * A person is considered unique by comparing using {@code Person#isSamePerson(Person)}. As such, adding and updating of
+ * persons uses Person#isSamePerson(Person) for equality so as to ensure that the person being added or updated is
+ * unique in terms of identity in the UniquePersonList. However, the removal of a person uses Person#equals(Object) so
+ * as to ensure that the person with exactly the same fields will be removed.
+ *
+ * Supports a minimal set of list operations.
+ *
+ * @see Person#isSamePerson(Person)
+ */
 public class ExpenditureList implements Iterable<Expenditure> {
 
     private final ObservableList<Expenditure> internalList = FXCollections.observableArrayList();
