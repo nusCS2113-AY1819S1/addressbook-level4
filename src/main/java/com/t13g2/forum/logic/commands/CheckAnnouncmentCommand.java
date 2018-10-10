f262792b9afc08644a7b7f44282cde9f4e1f5400
xllx1 <caixiaolin269@gmail.com>
2018-10-10 11:46:22 +0800

    fixed checkstyle errors from merged.

--- src/main/java/com/t13g2/forum/logic/commands/CheckAnnouncmentCommand.java
+++ src/main/java/com/t13g2/forum/logic/commands/CheckAnnouncmentCommand.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 
 import com.t13g2.forum.logic.CommandHistory;
 import com.t13g2.forum.model.Model;
+import com.t13g2.forum.model.forum.Announcement;
 
 /**
  * Allow user to check if there is any announcement
@@ -11,12 +12,12 @@ import com.t13g2.forum.model.Model;
 public class CheckAnnouncmentCommand extends Command {
     public static final String COMMAND_WORD = "checkAnnounce";
 
-    public static final String MESSAGE_SUCCESS = "Showing new announcement!";
+    public static final String MESSAGE_SUCCESS = "Showing new announcement: %1$s";
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
-
-        return new CommandResult(MESSAGE_SUCCESS);
+        Announcement announcement = model.showAnnouncement();
+        return new CommandResult(String.format(MESSAGE_SUCCESS, announcement));
     }
 }
--- src/main/java/com/t13g2/forum/storage/forum/IModuleRepository.java
+++ src/main/java/com/t13g2/forum/storage/forum/IModuleRepository.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import com.t13g2.forum.model.forum.Module;
 
+/**
+ *
+ */
 public interface IModuleRepository {
     int addModule(Module module);
     void removeModule(Module module);
