e9d4b618e6b3a0e9ef6dcbc3d86d8467926ae91d
SHININGGGG <wangshining814@gmail.com>
2018-10-10 23:41:39 +0800

    checkstyle update

--- src/main/java/seedu/address/logic/commands/AddExpenditureCommand.java
+++ src/main/java/seedu/address/logic/commands/AddExpenditureCommand.java
@@ -5,7 +5,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_CATEGORY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MONEY;
 
-
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
