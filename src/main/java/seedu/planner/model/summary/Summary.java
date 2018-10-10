c2f080f2ef3e72c6a6bec191eb931b3e6aecbf27
Vincent <vencintgamer@gmail.com>
2018-10-10 22:52:42 +0800

    Added add functionality to summary object

--- src/main/java/seedu/planner/model/summary/Summary.java
+++ src/main/java/seedu/planner/model/summary/Summary.java
@@ -7,7 +7,7 @@ import seedu.planner.model.record.Record;
 /**
  * This object represents the in memory model of a summary of all financial activity within a day.
  */
-// TODO: refactor this such that the pattern is not needed here
+// TODO: refactor this such that it does not need to know how to add
 public class Summary {
     private Date date;
     private MoneyFlow totalExpense;
@@ -21,11 +21,32 @@ public class Summary {
             totalExpense = money;
         } else {
             totalIncome = money;
-        }
+        } total = money;
     }
 
+    /**
+     * Adds the record's moneyflow to totalExpense, totalIncome or total\
+     */
+    // TODO: Please refactor this and bring into Moneyflow class
     public void add(Record record) {
-        // Does nothing
+        MoneyFlow money = record.getMoneyFlow();
+        if (isExpense(money)) {
+            double newMoneyAmount = money.toDouble() + totalExpense.toDouble();
+            String newMoneyString = String.format("%.2f", newMoneyAmount);
+            totalExpense = new MoneyFlow(newMoneyString);
+        } else {
+            double newMoneyAmount = money.toDouble() + totalExpense.toDouble();
+            String newMoneyString = String.format("+%.2f", newMoneyAmount);
+            totalExpense = new MoneyFlow(newMoneyString);
+        }
+        double newTotalAmount = money.toDouble() + total.toDouble();
+        String newTotalString;
+        if (newTotalAmount > 0) {
+            newTotalString = String.format("+%.2f", newTotalAmount);
+        } else {
+            newTotalString = String.format("%.2f", newTotalAmount);
+        }
+        total = new MoneyFlow(newTotalString);
     }
 
     public void remove(Record record) {
