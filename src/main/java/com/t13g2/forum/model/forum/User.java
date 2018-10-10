94e0a20e1161ebca4c2f8999a61f63171ad61f53
mimimi <jjzzzmd@gmail.com>
2018-10-10 16:03:23 +0800

    Implement storage

--- src/main/java/com/t13g2/forum/model/forum/User.java
+++ src/main/java/com/t13g2/forum/model/forum/User.java
@@ -20,6 +20,54 @@ public class User extends BaseModel {
         isBlock = block;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public boolean isBlock() {
+        return isBlock;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public void setBlock(boolean block) {
+        isBlock = block;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
