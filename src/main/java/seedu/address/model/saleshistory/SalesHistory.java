8e8b5cdb8a46fe11b7f4c5b4b063377d2d059d41
ParasK26 <bhttprs@gmail.com>
2018-10-10 12:44:32 +0800

    Added shopday,transaction,saleshistory classes

---
+++ src/main/java/seedu/address/model/saleshistory/SalesHistory.java
@@ -0,0 +1,24 @@
+package seedu.address.model.saleshistory;
+
+import seedu.address.model.saleshistory.exceptions.DuplicateDayException;
+import seedu.address.model.shopday.ShopDay;
+import java.util.NoSuchElementException;
+import java.util.TreeMap;
+
+public class SalesHistory {
+    private TreeMap<String,ShopDay> salesHistory;
+
+    public SalesHistory() {
+        this.salesHistory = new TreeMap<>();
+    }
+
+    public void addDay(String day) throws DuplicateDayException {
+        if (salesHistory.containsKey(day)) throw new DuplicateDayException();
+        ShopDay toBeAdded = new ShopDay(day);
+        salesHistory.put(day,toBeAdded);
+    }
+
+    public ShopDay getDaysHistory(String day) throws NoSuchElementException {
+        return salesHistory.get(day);
+    }
+}
