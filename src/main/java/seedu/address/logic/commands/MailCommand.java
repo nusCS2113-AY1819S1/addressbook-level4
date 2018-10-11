da65014d73149f84f38a43c4db09724613f02333
lekoook <kokteng1313@gmail.com>
2018-10-11 16:48:00 +0800

    Remove unused import statements

--- src/main/java/seedu/address/logic/commands/MailCommand.java
+++ src/main/java/seedu/address/logic/commands/MailCommand.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import seedu.address.commons.util.FileEncryptor;
