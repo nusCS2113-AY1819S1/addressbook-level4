2018acbe66d89a8e5274b50214b6638fe62dd406
Alden Lim <31233120+E0201942@users.noreply.github.com>
2018-10-13 04:56:40 +0800

    Update FilterCommandParser.java

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -1,26 +1,36 @@
 package seedu.address.logic.parser;
 
-import seedu.address.logic.commands.FilterCommand;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.MergeCommand;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.TimetableContainsModulePredicate;
+import seedu.address.model.person.*;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.Model;
 
-import java.util.Arrays;
+import java.util.*;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-public class FilterCommandParser implements Parser<FilterCommand>{
+import static seedu.address.logic.commands.MergeCommand.MESSAGE_NOT_MERGED;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MERGE;
 
-    public FilterCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
+
+public class MergeCommandParser implements Parser<MergeCommand> {
+
+    public MergeCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_MERGE);
+
+        List<String> indices = argMultimap.getAllValues(PREFIX_MERGE);
+
+
+        if(indices.size()<2){
+            throw new ParseException(MESSAGE_NOT_MERGED);
         }
 
-        String[] moduleKeywords = trimmedArgs.split("\\s+");
-
-
-
-        return new FilterCommand(new TimetableContainsModulePredicate(Arrays.asList(moduleKeywords)));
+        return new MergeCommand(indices);
     }
 }
