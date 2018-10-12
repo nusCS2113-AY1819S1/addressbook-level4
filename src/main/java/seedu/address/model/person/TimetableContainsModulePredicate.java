fa2f66f728ebf83785b3b58736df7dc0147fe7d9
Alden Lim <31233120+E0201942@users.noreply.github.com>
2018-10-13 06:28:40 +0800

    Update TimetableContainsModulePredicate.java

--- src/main/java/seedu/address/model/person/TimetableContainsModulePredicate.java
+++ src/main/java/seedu/address/model/person/TimetableContainsModulePredicate.java
@@ -1,11 +1,13 @@
 package seedu.address.model.person;
 
-import seedu.address.commons.util.StringUtil;
-import seedu.address.model.tag.Tag;
-
 import java.util.List;
 import java.util.function.Predicate;
 
+import seedu.address.model.tag.Tag;
+
+/**
+ * Tests that a {@code Person}'s {@code Tag} matches any of the keywords given.
+ */
 public class TimetableContainsModulePredicate implements Predicate<Person> {
     private final List<String> keywords;
 
@@ -18,8 +20,9 @@ public class TimetableContainsModulePredicate implements Predicate<Person> {
         for (Tag module : person.getTags()) {
             for (String check : keywords) {
                 check = "[" + check + "]";
-                if (check.equalsIgnoreCase(module.toString()))
+                if (check.equalsIgnoreCase(module.toString())) {
                     return true;
+                }
             }
         }
         return false;
@@ -28,7 +31,9 @@ public class TimetableContainsModulePredicate implements Predicate<Person> {
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof seedu.address.model.person.TimetableContainsModulePredicate // instanceof handles nulls
-                && keywords.equals(((seedu.address.model.person.TimetableContainsModulePredicate) other).keywords)); // state check
+                || (other instanceof seedu.address.model.person.TimetableContainsModulePredicate
+                // instanceof handles nulls
+                && keywords.equals(((seedu.address.model.person.TimetableContainsModulePredicate) other).keywords));
+                // state check
     }
 }
