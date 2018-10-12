8a00ba37d72f78093530069d928744684227f58d
Alden Lim <31233120+E0201942@users.noreply.github.com>
2018-10-13 05:09:05 +0800

    Update FilterCommandParser.java

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -1,36 +1,26 @@
 package seedu.address.logic.parser;
 
-
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.MergeCommand;
-import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.commands.FilterCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.*;
-import seedu.address.model.tag.Tag;
-import seedu.address.model.Model;
+import seedu.address.model.person.TimetableContainsModulePredicate;
 
-import java.util.*;
+import java.util.Arrays;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import static seedu.address.logic.commands.MergeCommand.MESSAGE_NOT_MERGED;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_MERGE;
+public class FilterCommandParser implements Parser<FilterCommand>{
 
-
-public class MergeCommandParser implements Parser<MergeCommand> {
-
-    public MergeCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_MERGE);
-
-        List<String> indices = argMultimap.getAllValues(PREFIX_MERGE);
-
-
-        if(indices.size()<2){
-            throw new ParseException(MESSAGE_NOT_MERGED);
+    public FilterCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
         }
 
-        return new MergeCommand(indices);
+        String[] moduleKeywords = trimmedArgs.split("\\s+");
+
+
+
+        return new FilterCommand(new TimetableContainsModulePredicate(Arrays.asList(moduleKeywords)));
     }
 }
