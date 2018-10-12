1acb5cfb7641f9daee000cf345906be6bda47275
tsuweiquan <e0176844@u.nus.edu>
2018-10-12 18:19:46 +0800

    Added Gender Distribution

--- src/main/java/seedu/address/model/distribute/DistributeAlgorithm.java
+++ src/main/java/seedu/address/model/distribute/DistributeAlgorithm.java
@@ -8,12 +8,16 @@ import java.util.LinkedList;
 
 import javafx.collections.ObservableList;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.Parser;
 import seedu.address.model.Model;
+import seedu.address.model.person.Gender;
 import seedu.address.model.person.Person;
 
 public class DistributeAlgorithm {
 
     public static final String MESSAGE_INVALID_SIZE = "Number of Groups should not be more than Number of Persons";
+    private static final String VALID_GENDER_MALE = "MALE";
+    private static final String VALID_GENDER_FEMALE = "FEMALE";
 
     public DistributeAlgorithm(Model model, Distribute dist) throws CommandException {
         requireNonNull(dist);
@@ -33,7 +37,7 @@ public class DistributeAlgorithm {
         }
 
         //Converts into ArrayList to use Randomizer via Collections
-        LinkedList<Person> allPersonArrayList = new LinkedList<Person>(allPerson);
+        LinkedList<Person> allPersonArrayList = new LinkedList<>(allPerson);
         allPersonArrayList = Randomizer(allPersonArrayList);
 
         if(!genderFlag && !nationalityFlag){
@@ -56,7 +60,7 @@ public class DistributeAlgorithm {
 
     private void NormalDistribution(int index, ArrayList<ArrayList<Person>> groupArrayList, LinkedList<Person> allPersonArrayList) {
         for(int i=index; i>0; i--){                             //number of groups to add into the groupArrayList
-            ArrayList<Person> addPerson = new ArrayList<Person>();
+            ArrayList<Person> addPerson = new ArrayList<>();
             int paxInAGroup = allPersonArrayList.size()/i;
             while(paxInAGroup > 0){
                 addPerson.add(allPersonArrayList.getLast());
@@ -75,7 +79,42 @@ public class DistributeAlgorithm {
     }
 
     private void GenderDistribution(int index, ArrayList<ArrayList<Person>> groupArrayList, LinkedList<Person> allPersonArrayList) {
-        System.out.println("Gender Distribution");
+        LinkedList<Person> maleLinkList = new LinkedList<>();
+        LinkedList<Person> femaleLinkList = new LinkedList<>();
+
+        for( Person p : allPersonArrayList){
+            if(p.getGender().toString().equals(VALID_GENDER_MALE)){
+                maleLinkList.add(p);
+
+            }else if(p.getGender().toString().equals(VALID_GENDER_FEMALE)){
+                femaleLinkList.add(p);
+
+            }
+        }
+
+        //creates 7 groups in this for loop
+        groupArrayList.ensureCapacity(index);
+        ArrayList<Person> addPerson = new ArrayList<>();
+        while(maleLinkList.size() !=0 || femaleLinkList.size() !=0) {
+            for (int i = 0; i < index; i++) {
+                if (maleLinkList.size() > 0) {
+                    addPerson.add(maleLinkList.getLast());
+                    groupArrayList.add(i,addPerson);
+                    maleLinkList.removeLast();
+                } else if (femaleLinkList.size() > 0) {
+                    addPerson.add(femaleLinkList.getLast());
+                    groupArrayList.add(i,addPerson);
+                    femaleLinkList.removeLast();
+                }
+            }
+        }
+
+        for(int i=0;i<groupArrayList.size();i++){
+            System.out.println(groupArrayList.get(i));
+        }
+
+//        System.out.println(maleLinkList);
+//        System.out.println(femaleLinkList);
     }
 
     private void StrictDistribution(int index, ArrayList<ArrayList<Person>> groupArrayList, LinkedList<Person> allPersonArrayList) {
