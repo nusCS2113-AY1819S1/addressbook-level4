ccc4463bc08ca56bb8a24bfe21f011f286116324
Alden Lim <31233120+E0201942@users.noreply.github.com>
2018-10-13 06:27:00 +0800

    Update FilterCommand.java

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -1,11 +1,11 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.person.TimetableContainsModulePredicate;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * Finds and lists all persons in address book who has any of the modules or time slots inputted.
