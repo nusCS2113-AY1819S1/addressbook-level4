9150766ca952e81e93f70c9cda6718fa79067be6
Alden Lim <31233120+E0201942@users.noreply.github.com>
2018-10-13 06:28:07 +0800

    Update MergeCommandParser.java

--- src/main/java/seedu/address/logic/parser/MergeCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MergeCommandParser.java
@@ -1,25 +1,22 @@
 package seedu.address.logic.parser;
 
-
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.MergeCommand;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.*;
-import seedu.address.model.tag.Tag;
-import seedu.address.model.Model;
-
-import java.util.*;
-
-
 import static seedu.address.logic.commands.MergeCommand.MESSAGE_NOT_MERGED;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MERGE;
 
+import java.util.List;
 
+import seedu.address.logic.commands.MergeCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new FilterCommand object
+ */
 public class MergeCommandParser implements Parser<MergeCommand> {
-
+    /**
+     * Parses the given {@code String} of arguments in the context of the EditCommand
+     * and returns an EditCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     public MergeCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_MERGE);
