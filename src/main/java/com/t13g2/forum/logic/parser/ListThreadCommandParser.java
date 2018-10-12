4e3805c5b30bb634cd366208f8c591a819c7d33d
HansKoh <xuhanhuihans@gmail.com>
2018-10-12 14:54:48 +0800

    added listThreadCommand and listModuleCommand

---
+++ src/main/java/com/t13g2/forum/logic/parser/ListThreadCommandParser.java
@@ -0,0 +1,38 @@
+package com.t13g2.forum.logic.parser;
+
+import com.t13g2.forum.commons.core.Messages;
+import com.t13g2.forum.logic.commands.ListThreadCommand;
+import com.t13g2.forum.logic.parser.exceptions.ParseException;
+
+import java.util.stream.Stream;
+
+import static com.t13g2.forum.logic.parser.CliSyntax.PREFIX_MODULE_CODE;
+
+public class ListThreadCommandParser implements Parser<ListThreadCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the ListThreadCommand
+     * and returns an ListThreadCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ListThreadCommand parse(String args) throws ParseException {
+
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_MODULE_CODE);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_MODULE_CODE)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, ListThreadCommand.MESSAGE_USAGE));
+        }
+        String moduleCode = ParserUtil.parseModule(argMultimap.getValue(PREFIX_MODULE_CODE).get());
+
+        return new ListThreadCommand(moduleCode);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+}
