0ab8b8c7bda9798db116ac1a8f2131a3f727394f
cjinting-nus <e0175290@u.nus.edu>
2018-10-13 12:22:43 +0800

    Added friendList assets

---
+++ src/main/java/seedu/address/model/person/Friend.java
@@ -0,0 +1,14 @@
+package seedu.address.model.person;
+
+import seedu.address.model.Timetable.TimeTable;
+import seedu.address.model.tag.Tag;
+
+import java.util.Set;
+
+/**
+ * Represents the list of friends that a user can have
+ */
+public class Friend {
+
+
+}
