8f277a98177618c0116b9e44bc5083c73d194e00
Lunastryke <35793588+Lunastryke@users.noreply.github.com>
2018-10-15 01:42:18 +0800

    BatchPrice: Minor edits for checkstyle

--- src/main/java/seedu/address/model/drinks/BatchPrice.java
+++ src/main/java/seedu/address/model/drinks/BatchPrice.java
@@ -4,7 +4,7 @@ package seedu.address.model.drinks;
  * Represents the price of a batch of drinks
  * Guarantees: TODO
  */
-public class BatchPrice extends Price{
+public class BatchPrice extends Price {
     public BatchPrice(String input) {
         super(input);
     }
