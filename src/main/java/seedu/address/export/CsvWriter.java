8827d4dfcee15514f788563e2afe496d41840d1e
jitwei98 <limjitwei@gmail.com>
2018-10-15 15:21:31 +0800

    Refactor by extracting methods

--- src/main/java/seedu/address/export/CsvWriter.java
+++ src/main/java/seedu/address/export/CsvWriter.java
@@ -9,11 +9,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.opencsv.CSVWriter;
 
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.ModelManager;
 import seedu.address.model.person.Person;
 
 //@@author jitwei98
@@ -21,6 +24,8 @@ import seedu.address.model.person.Person;
  * The writer for CSV file of the app.
  */
 public class CsvWriter {
+    private static final Logger logger = LogsCenter.getLogger(ModelManager.class);
+
     private static final int INDEX_PERSON_NAME = 0;
     private static final int INDEX_PERSON_PHONE = 2;
     private static final int INDEX_PERSON_ADDRESS = 3;
@@ -33,13 +38,13 @@ public class CsvWriter {
     public CsvWriter(ObservableList<Person> persons) {
         requireAllNonNull(persons);
 
-        this.persons = persons;
-        // logger.fine("Initializing with address book: " + addressBook + " and user prefs " + userPrefs);
+        // TODO: lift this up to a exportFileWriter interface
+        logger.fine("Initializing with output file: " + outputFilepath.toString());
 
-        // versionedAddressBook = new VersionedAddressBook(addressBook);
+        this.persons = persons;
     }
 
-    public void writeToCsv() throws IOException {
+    public void writeToCsv() {
         try {
 
             File file = new File(String.valueOf(outputFilepath));
@@ -47,25 +52,27 @@ public class CsvWriter {
             CSVWriter writer = new CSVWriter(outputfile);
 
             writer.writeNext(header);
-            // TODO: Format data to List<String[]> and replace the following:
 
             List<String[]> data = new ArrayList<>();
-            // TODO: Refactor the following lines of code
             persons.forEach(person -> {
-                String[] details = new String[10];
-                details[INDEX_PERSON_NAME] = person.getName().toString();
-                details[INDEX_PERSON_PHONE] = person.getPhone().toString();
-                details[INDEX_PERSON_ADDRESS] = person.getAddress().toString();
-                details[INDEX_PERSON_EMAIL] = person.getEmail().toString();
+                String[] details = convertToStringArray(person);
                 data.add(details);
             });
 
             writer.writeAll(data);
-
             writer.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String[] convertToStringArray(Person person) {
+        String[] details = new String[header.length];
+        details[INDEX_PERSON_NAME] = person.getName().toString();
+        details[INDEX_PERSON_PHONE] = person.getPhone().toString();
+        details[INDEX_PERSON_ADDRESS] = person.getAddress().toString();
+        details[INDEX_PERSON_EMAIL] = person.getEmail().toString();
+        return details;
     }
 }
