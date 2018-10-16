adb3ebdc556395b51bca9c5f785a160c588b1743
Andrew Ong <39642465+andrewome@users.noreply.github.com>
2018-10-16 17:36:06 +0800

    Changed imports wildcard to individual imports

--- src/main/java/seedu/recruit/commons/util/EmailUtil.java
+++ src/main/java/seedu/recruit/commons/util/EmailUtil.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
@@ -26,7 +30,6 @@ import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
 import com.google.api.services.gmail.model.Message;
 
-import javafx.collections.ObservableList;
 import seedu.recruit.model.candidate.Candidate;
 import seedu.recruit.model.joboffer.JobOffer;
 
