ebdb34f275a759559dc4c56fcdf595e1a1075480
dyeosy98 <e0201362@u.nus.edu>
2018-10-16 10:23:52 +0800

    person builder altered to test travis

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -16,10 +16,10 @@ import seedu.address.model.util.SampleDataUtil;
  */
 public class PersonBuilder {
 
-    public static final String DEFAULT_NAME = "Alice Pauline";
+    public static final String DEFAULT_NAME = "Apple";
     public static final String DEFAULT_PHONE = "85355255";
-    public static final String DEFAULT_EMAIL = "alice@gmail.com";
-    public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    public static final String DEFAULT_EMAIL = "Ah Huat";
+    public static final String DEFAULT_ADDRESS = "fruit";
 
     private Name name;
     private Phone phone;
