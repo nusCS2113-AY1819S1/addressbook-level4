47af34f1015e1df0cafb5ab8d68dc01bd65d255f
Elston Aw <elston.ayx@u.nus.edu>
2018-10-17 01:17:37 +0800

    Added new CommandSuggestion class Modified AddressBookParser to accept CommandSuggestion in the default case and return the message into ParseException

---
+++ src/main/java/seedu/address/logic/CommandSuggestion.java
@@ -0,0 +1,12 @@
+package seedu.address.logic;
+
+/**
+ * Suggests a command with the closest match to the inputted string.
+ */
+public class CommandSuggestion {
+    private static final String SUGGESTION_NOT_IMPLEMENTED = "Suggestion not implemented yet.";
+
+    public String returnSuggestion() {
+        return SUGGESTION_NOT_IMPLEMENTED;
+    }
+}
