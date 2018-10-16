89935a3d0ba7d0aad33e282e221cab848b81468d
feijunzi <e0253704@u.nus.edu>
2018-10-16 19:10:11 +0800

    listExpenditure

--- src/main/java/seedu/address/logic/commands/ListExpenditureCommand.java
+++ src/main/java/seedu/address/logic/commands/ListExpenditureCommand.java
@@ -1,7 +1,26 @@
 package seedu.address.logic.commands;
-/**
- * Lists all expenditures in the expenditure tracker to the user.
- */
 
-public class ListExpenditureCommand {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EXPENDITURES;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class ListExpenditureCommand extends Command {
+
+    public static final String COMMAND_WORD = "ET_list";
+    public static final String COMMAND_ALIAS = "l";
+
+    public static final String MESSAGE_SUCCESS = "Listed all expenditures";
+
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
+        model.updateFilteredExpenditureList(PREDICATE_SHOW_ALL_EXPENDITURES);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
 }
