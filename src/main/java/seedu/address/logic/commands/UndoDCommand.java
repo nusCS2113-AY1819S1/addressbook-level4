f48069ab3620606d97658989b74c536d6c4b612e
dyeosy98 <e0201362@u.nus.edu>
2018-10-16 13:24:02 +0800

    checkstyle edited

--- src/main/java/seedu/address/logic/commands/UndoDCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoDCommand.java
@@ -12,9 +12,9 @@ import seedu.address.model.Model;
 */
 public class UndoDCommand extends Command {
 
- public static final String COMMAND_WORD = "undoD";
- public static final String MESSAGE_SUCCESS = "Undo success!";
- public static final String MESSAGE_FAILURE = "No more commands to undo!";
+    public static final String COMMAND_WORD = "undoD";
+    public static final String MESSAGE_SUCCESS = "Undo success!";
+    public static final String MESSAGE_FAILURE = "No more commands to undo!";
 
 
     @Override
