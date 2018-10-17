c0c0a27f5711d0ca658b5269df0fb013773aff7e
tristyxxnana <tristyxxnana@gmail.com>
2018-10-17 19:38:09 +0800

    resolved checkstyle

--- src/main/java/seedu/address/logic/commands/GradebookDeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/GradebookDeleteCommand.java
@@ -9,7 +9,6 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.gradebook.Gradebook;
 import seedu.address.model.gradebook.GradebookManager;
-import seedu.address.storage.adapter.XmlAdaptedGradebook;
 
 /**
  * Deletes gradebook component for module in Trajectory to the user.
--- src/main/java/seedu/address/logic/parser/GradebookDeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/GradebookDeleteCommandParser.java
@@ -9,7 +9,6 @@ import java.util.stream.Stream;
 import seedu.address.logic.commands.GradebookDeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.gradebook.Gradebook;
-import seedu.address.storage.adapter.XmlAdaptedGradebook;
 
 /**
  * Parses input arguments and creates a new GradebookDeleteCommand object
