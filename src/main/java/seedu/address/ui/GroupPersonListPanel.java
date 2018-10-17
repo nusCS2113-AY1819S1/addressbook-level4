4c9bf728c98575cb4143cb0bafce550754bc6b9a
rajdeepsh <rajdeepsh95@gmail.com>
2018-10-17 14:32:19 +0800

    Add GUI panel to display persons in group when group card selected
    . Add GroupPersonListPanel.java and GroupPersonListPanel.fxml to display person cards of persons in group when a group card is slected.(have to select another group card first then select the group card again to see an updated list of persons in the group)
    . Modify MainWindow.java and MainWindow.fxml to accommodate GrpupPersonListPanel
    . All test cases passed

---
+++ src/main/java/seedu/address/ui/GroupPersonListPanel.java
@@ -0,0 +1,68 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import com.google.common.eventbus.Subscribe;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Region;
+
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.GroupPanelSelectionChangedEvent;
+import seedu.address.model.group.Group;
+import seedu.address.model.person.Person;
+
+
+/**
+ * Panel containing the list of persons in groups.
+ */
+public class GroupPersonListPanel extends UiPart<Region> {
+    private static final String FXML = "GroupPersonListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(GroupPersonListPanel.class);
+
+    @FXML
+    private ListView<Person> groupPersonListView;
+
+    public GroupPersonListPanel() {
+        super(FXML);
+        registerAsAnEventHandler(this);
+    }
+
+    private void loadGroupPersons(Group group){
+        ObservableList<Person> personObservableList = FXCollections.observableArrayList();
+        for (Person p: group.getPersons()) {
+            personObservableList.add(p);
+        }
+
+        groupPersonListView.setItems(personObservableList);
+        groupPersonListView.setCellFactory(listView -> new PersonListViewCell());
+    }
+
+    @Subscribe
+    private void handleGroupPanelSelectionChangedEvent(GroupPanelSelectionChangedEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        loadGroupPersons(event.getNewSelection());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
+     */
+    class PersonListViewCell extends ListCell<Person> {
+        @Override
+        protected void updateItem(Person person, boolean empty) {
+            super.updateItem(person, empty);
+
+            if (empty || person == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
