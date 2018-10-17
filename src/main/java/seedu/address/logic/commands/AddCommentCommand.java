af3b52b72b7951a2e1c0462aed0cdefa255d88b8
Unknown <geraldcdx5@gmail.com>
2018-10-17 14:30:54 +0800

    AddCommentCommand

--- src/main/java/seedu/address/logic/commands/AddCommentCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommentCommand.java
@@ -1,7 +1,106 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_COMMENT;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.comments.AddComment;
+import seedu.address.model.Model;
+import seedu.address.model.event.Event;
+
 /**
- * Replies a comment in the comment section of the event
+ * Adds a comment to the end comment section of the event
  */
-public class AddCommentCommand {
+public class AddCommentCommand extends Command {
+    public static final String COMMAND_WORD = "addComment";
+
+    public static final String MESSAGE = COMMAND_WORD + ": Edits the details of the event identified "
+            + "by the index number used in the displayed event list. "
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "[" + PREFIX_COMMENT + "COMMENT] "
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_COMMENT + "johndoe@example.com is here";
+
+    public static final String MESSAGE_REPLY_COMMENT = "Comment [%1$s] replied for Event %2$s";
+
+    private final Index index;
+    private final EditCommand.EditPersonDescriptor editCommentDescriptor;
+    private String comment = null;
+
+    /**
+     * @param index of the event in the filtered event list to edit
+     * @param editPersonDescriptor details to edit the event with
+     */
+    public AddCommentCommand(Index index, String comment) {
+        requireNonNull(index);
+        requireNonNull(comment);
+
+        this.index = index;
+        this.comment = comment;
+        this.editCommentDescriptor = new EditCommand.EditPersonDescriptor();
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        String test =
+                "<span>Comment Section</span>\n"
+                        + "<ol>\n"
+                        + "<li>hello</li>\n"
+                        + "<li>My name is Gerald</li>\n"
+                        + "<li>What is your name?</li>\n"
+                        + "</ol>";
+
+        AddComment comments = new AddComment(test);
+        test = comments.addComment(getComment());
+
+        File savingFile = new File("C:/Users/Gerald/Desktop/test/1.html");
+        FileOutputStream fop = null;
+        try {
+            fop = new FileOutputStream(savingFile);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            fop.write(test.getBytes());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        requireNonNull(model);
+        List<Event> filteredEventList = model.getFilteredEventList();
+
+
+        if (index.getZeroBased() >= filteredEventList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
+        }
+
+        EventsCenter.getInstance().post(new JumpToListRequestEvent(index));
+
+        Event eventToEdit = filteredEventList.get(index.getZeroBased());
+        Event editedEvent = EditCommand.createEditedPerson(eventToEdit, editCommentDescriptor);
+
+        model.updateEvent(eventToEdit, editedEvent);
+        model.commitEventManager();
+
+        return new CommandResult(String.format(MESSAGE_REPLY_COMMENT, getComment(), index.getOneBased()));
+    }
+
 }
--- src/main/java/seedu/address/logic/parser/EventManagerParser.java
+++ src/main/java/seedu/address/logic/parser/EventManagerParser.java
@@ -6,22 +6,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.HistoryCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.LoginCommand;
-import seedu.address.logic.commands.RedoCommand;
-import seedu.address.logic.commands.RegisterCommand;
-import seedu.address.logic.commands.ReplyCommentCommand;
-import seedu.address.logic.commands.SelectCommand;
-import seedu.address.logic.commands.UndoCommand;
+import seedu.address.logic.commands.*;
 
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -93,6 +78,9 @@ public class EventManagerParser {
 
         case RegisterCommand.COMMAND_WORD:
             return new RegisterCommandParser().parse(arguments);
+
+            case AddCommentCommand.COMMAND_WORD:
+                return new AddCommentCommandParser().parse(arguments);
 
         case ReplyCommentCommand.COMMAND_WORD:
             return new ReplyCommentCommandParser().parse(arguments);
