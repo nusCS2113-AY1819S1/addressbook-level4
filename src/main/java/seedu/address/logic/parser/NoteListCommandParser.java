9db1363ba62f672a2cde32385bae34c2af446fcf
jomcruz93 <jomcruz93@gmail.com>
2018-10-17 11:41:53 +0800

    Fixed a checkstyle error.

--- src/main/java/seedu/address/logic/parser/NoteListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/NoteListCommandParser.java
@@ -14,7 +14,6 @@ public class NoteListCommandParser implements Parser<NoteListCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the NoteListCommand
      * and returns a NoteListCommand object for execution.
-     * 
      * @throws ParseException if the user input does not conform to the expected format
      */
     public NoteListCommand parse(String args) throws ParseException {
