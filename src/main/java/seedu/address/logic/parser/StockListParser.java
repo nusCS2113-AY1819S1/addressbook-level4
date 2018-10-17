145a8029d14aad2a758e7ccc2c2f6692cffb6849
Davindran Sukumaran <spartan48@gmail.com>
2018-10-17 19:38:14 +0800

    fixed gradle issues with style and unused imports

--- src/main/java/seedu/address/logic/parser/StockListParser.java
+++ src/main/java/seedu/address/logic/parser/StockListParser.java
@@ -50,55 +50,38 @@ public class StockListParser {
         final String commandWord = matcher.group("commandWord");
         final String arguments = matcher.group("arguments");
         switch (commandWord) {
-                
-            case AddCommand.COMMAND_WORD:
-                return new AddCommandParser().parse(arguments);
-                
-            case EditCommand.COMMAND_WORD:
-                return new EditCommandParser().parse(arguments);
-                
-            case SelectCommand.COMMAND_WORD:
-                return new SelectCommandParser().parse(arguments);
-                
-            case DeleteCommand.COMMAND_WORD:
-                return new DeleteCommandParser().parse(arguments);
-                
-            case ClearCommand.COMMAND_WORD:
-                return new ClearCommand();
-                
-            case FindCommand.COMMAND_WORD:
-                return new FindCommandParser().parse(arguments);
-                
-            case ListCommand.COMMAND_WORD:
-                return new ListCommand();
-                
-            case LoginCommand.COMMAND_WORD:
-                return new LoginCommandParser().parse(arguments);
-                
-            case HistoryCommand.COMMAND_WORD:
-                return new HistoryCommand();
-                
-            case ExitCommand.COMMAND_WORD:
-                return new ExitCommand();
-                
-            case HelpCommand.COMMAND_WORD:
-                return new HelpCommand();
-                
-            case UndoCommand.COMMAND_WORD:
-                return new UndoCommand();
-                
-            case RedoCommand.COMMAND_WORD:
-                return new RedoCommand();
-                
-            case ChangeStatusCommand.COMMAND_WORD:
-                return new ChangeStatusCommandParser().parse(arguments);
-                
-            case SaveCommand.COMMAND_WORD:
-                return new SaveCommandParser().parse(arguments);
-                
-            default:
-                throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
-                
+        case AddCommand.COMMAND_WORD:
+            return new AddCommandParser().parse(arguments);
+        case EditCommand.COMMAND_WORD:
+            return new EditCommandParser().parse(arguments);
+        case SelectCommand.COMMAND_WORD:
+            return new SelectCommandParser().parse(arguments);
+        case DeleteCommand.COMMAND_WORD:
+            return new DeleteCommandParser().parse(arguments);
+        case ClearCommand.COMMAND_WORD:
+            return new ClearCommand();
+        case FindCommand.COMMAND_WORD:
+            return new FindCommandParser().parse(arguments);
+        case ListCommand.COMMAND_WORD:
+            return new ListCommand();
+        case LoginCommand.COMMAND_WORD:
+            return new LoginCommandParser().parse(arguments);
+        case HistoryCommand.COMMAND_WORD:
+            return new HistoryCommand();
+        case ExitCommand.COMMAND_WORD:
+            return new ExitCommand();
+        case HelpCommand.COMMAND_WORD:
+            return new HelpCommand();
+        case UndoCommand.COMMAND_WORD:
+            return new UndoCommand();
+        case RedoCommand.COMMAND_WORD:
+            return new RedoCommand();
+        case ChangeStatusCommand.COMMAND_WORD:
+            return new ChangeStatusCommandParser().parse(arguments);
+        case SaveCommand.COMMAND_WORD:
+            return new SaveCommandParser().parse(arguments);
+        default:
+            throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
     }
 
