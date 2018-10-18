90c29f6a41aee723facee6aa8a676e85e2be5e5f
Unknown <geraldcdx5@gmail.com>
2018-10-18 17:09:01 +0800

    getting coverage in testcases

--- src/test/java/seedu/address/logic/comments/DeleteCommentTest.java
+++ src/test/java/seedu/address/logic/comments/DeleteCommentTest.java
@@ -1,8 +1,10 @@
 package seedu.address.logic.comments;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.DeleteCommentCommand;
 
 class DeleteCommentTest {
     private String comments =
@@ -32,8 +34,24 @@ class DeleteCommentTest {
                     + "<li>hello</li>\n"
                     + "</ol>";
     @Test
-    void deleteComment() throws Exception {
+    void deleteComment_correctInput_success() throws Exception {
         DeleteComment test = new DeleteComment(comments);
         assertEquals(testcase, test.deleteComment(4));
+    }
+
+    @Test
+    void deleteComment_incorrectInput_failure() throws Exception {
+        DeleteComment test = new DeleteComment(comments);
+        assertNotEquals(testcase, test.deleteComment(3));
+    }
+
+    @Test
+    void deleteComment_lineNotPresent_exceptionThrown() throws Exception {
+        DeleteComment test = new DeleteComment(comments);
+        try {
+            assertNotEquals(testcase, test.deleteComment( 15));
+        } catch (Exception e) {
+            assertEquals("Line is invalid, try again", DeleteCommentCommand.MESSAGE_LINE_INVALID);
+        }
     }
 }
