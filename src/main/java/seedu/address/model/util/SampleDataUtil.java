462079eebaba5a3075a80fb92f988a708e97b123
derpyplops <jonathan.ng1@gmail.com>
2018-10-18 22:31:33 +0800

    added implementation details for addSkillCommand

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -29,25 +29,24 @@ public class SampleDataUtil {
     public static final Skill EMPTY_SKILL = new Skill("");
     public static final SkillLevel EMPTY_SKILLLEVEL = new SkillLevel(0);
     public static Person[] getSamplePersons() {
-        //TODO: update with more legit data
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"), EMPTY_SKILL, EMPTY_SKILLLEVEL,
+                new Address("Blk 30 Geylang Street 29, #06-40"), new Skill("Photography"), new SkillLevel(90),
                 getTagSet("friends")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), EMPTY_SKILL, EMPTY_SKILLLEVEL,
+                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Skill("Videography"), new SkillLevel(50),
                 getTagSet("colleagues", "friends")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), EMPTY_SKILL, EMPTY_SKILLLEVEL,
+                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Skill("Design"), new SkillLevel(45),
                 getTagSet("neighbours")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), EMPTY_SKILL, EMPTY_SKILLLEVEL,
+                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Skill("Photoshop"), new SkillLevel(50),
                 getTagSet("family")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"), EMPTY_SKILL, EMPTY_SKILLLEVEL,
+                new Address("Blk 47 Tampines Street 20, #17-35"), new Skill("Stage Managing"), new SkillLevel(30),
                 getTagSet("classmates")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"), EMPTY_SKILL, EMPTY_SKILLLEVEL,
+                new Address("Blk 45 Aljunied Street 85, #11-31"), new Skill("Public Speaking"), new SkillLevel(80),
                 getTagSet("colleagues"))
         };
     }
