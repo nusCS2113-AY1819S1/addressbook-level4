eb14ef07b85d78c1cff245f560f0645813305b1b
SHININGGGG <wangshining814@gmail.com>
2018-10-18 18:15:58 +0800

    ET storage update

--- src/test/java/seedu/address/storage/StorageManagerTest.java
+++ src/test/java/seedu/address/storage/StorageManagerTest.java
@@ -3,6 +3,7 @@ package seedu.address.storage;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static seedu.address.testutil.TypicalExpenditures.getTypicalExpenditureTracker;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalTasks.getTypicalTodoList;
 
@@ -16,10 +17,13 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import seedu.address.commons.events.model.AddressBookChangedEvent;
+import seedu.address.commons.events.model.ExpenditureTrackerChangedEvent;
 import seedu.address.commons.events.model.TodoListChangedEvent;
 import seedu.address.commons.events.storage.DataSavingExceptionEvent;
 import seedu.address.model.AddressBook;
+import seedu.address.model.ExpenditureTracker;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyExpenditureTracker;
 import seedu.address.model.ReadOnlyTodoList;
 import seedu.address.model.TodoList;
 import seedu.address.model.UserPrefs;
@@ -37,9 +41,10 @@ public class StorageManagerTest {
     @Before
     public void setUp() {
         XmlAddressBookStorage addressBookStorage = new XmlAddressBookStorage(getTempFilePath("ab"));
+        XmlExpenditureTrackerStorage expenditureTrackerStorage = new XmlExpenditureTrackerStorage(getTempFilePath("et"));
         XmlTodoListStorage todoListStorage = new XmlTodoListStorage(getTempFilePath("td"));
         JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(getTempFilePath("prefs"));
-        storageManager = new StorageManager(addressBookStorage, todoListStorage, userPrefsStorage);
+        storageManager = new StorageManager(addressBookStorage, expenditureTrackerStorage, todoListStorage, userPrefsStorage);
     }
 
     private Path getTempFilePath(String fileName) {
@@ -83,9 +88,39 @@ public class StorageManagerTest {
     public void handleAddressBookChangedEventExceptionThrownEventRaised() {
         // Create a StorageManager while injecting a stub that  throws an exception when the save method is called
         Storage storage = new StorageManager(new XmlAddressBookStorageExceptionThrowingStub(Paths.get("dummy")),
+                new XmlExpenditureTrackerStorageExceptionThrowingStub(Paths.get("dummy")),
                 new XmlTodoListStorageExceptionThrowingStub(Paths.get("dummy")),
                 new JsonUserPrefsStorage(Paths.get("dummy")));
         storage.handleAddressBookChangedEvent(new AddressBookChangedEvent(new AddressBook()));
+        assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof DataSavingExceptionEvent);
+    }
+
+    @Test
+    public void expenditureTrackerReadSave() throws Exception {
+        /*
+         * Note: This is an integration test that verifies the StorageManager is properly wired to the
+         * {@link XmlExpenditureTrackerStorage} class.
+         * More extensive testing of UserPref saving/reading is done in {@link XmlExpenditureTrackerStorageTest} class.
+         */
+        ExpenditureTracker original = getTypicalExpenditureTracker();
+        storageManager.saveExpenditureTracker(original);
+        ReadOnlyExpenditureTracker retrieved = storageManager.readExpenditureTracker().get();
+        assertEquals(original, new ExpenditureTracker(retrieved));
+    }
+
+    @Test
+    public void getExpenditureTrackerFilePath() {
+        assertNotNull(storageManager.getExpenditureTrackerFilePath());
+    }
+
+    @Test
+    public void handleExpenditureTrackerChangedEventExceptionThrownEventRaised() {
+        // Create a StorageManager while injecting a stub that  throws an exception when the save method is called
+        Storage storage = new StorageManager(new XmlAddressBookStorageExceptionThrowingStub(Paths.get("dummy")),
+                new XmlExpenditureTrackerStorageExceptionThrowingStub(Paths.get("dummy")),
+                new XmlTodoListStorageExceptionThrowingStub(Paths.get("dummy")),
+                new JsonUserPrefsStorage(Paths.get("dummy")));
+        storage.handleExpenditureTrackerChangedEvent(new ExpenditureTrackerChangedEvent(new ExpenditureTracker()));
         assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof DataSavingExceptionEvent);
     }
 
@@ -111,6 +146,7 @@ public class StorageManagerTest {
     public void handleTodoListChangedEventExceptionThrownEventRaised() {
         // Create a StorageManager while injecting a stub that throws an exception when the save method is called
         Storage storage = new StorageManager(new XmlAddressBookStorageExceptionThrowingStub(Paths.get("dummy")),
+                new XmlExpenditureTrackerStorageExceptionThrowingStub(Paths.get("dummy")),
                 new XmlTodoListStorageExceptionThrowingStub(Paths.get("dummy")),
                 new JsonUserPrefsStorage(Paths.get("dummy")));
         storage.handleTodoListChangedEvent(new TodoListChangedEvent(new TodoList()));
@@ -129,6 +165,21 @@ public class StorageManagerTest {
 
         @Override
         public void saveAddressBook(ReadOnlyAddressBook addressBook, Path filePath) throws IOException {
+            throw new IOException("dummy exception");
+        }
+    }
+
+    /**
+     * A Stub class to throw an exception when the save method is called
+     */
+    class XmlExpenditureTrackerStorageExceptionThrowingStub extends XmlExpenditureTrackerStorage {
+
+        public XmlExpenditureTrackerStorageExceptionThrowingStub(Path filePath) {
+            super(filePath);
+        }
+
+        @Override
+        public void saveExpenditureTracker(ReadOnlyExpenditureTracker expenditureTracker, Path filePath) throws IOException {
             throw new IOException("dummy exception");
         }
     }
