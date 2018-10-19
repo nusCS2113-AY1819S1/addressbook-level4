f0e81ca4d4020c1a052ccd85eae15ccbc26b41cf
jamesyaputra <jamesyaputra1704@gmail.com>
2018-10-19 16:21:47 +0800

    Revert "Merge pull request #60 from CS2113-AY1819S1-T12-1/user-getter"
    This reverts commit f39b48a396caae2408bcec982c70a69fb10cc413, reversing
    changes made to 9488b66b25d670e16db2d43e24424225baf11085.

--- src/main/java/seedu/address/model/UserAccount.java
+++ src/main/java/seedu/address/model/UserAccount.java
@@ -58,21 +58,6 @@ public class UserAccount {
         return isPresent;
     }
 
-
-    /**
-     * Returns true if user is logged in.
-     */
-    public boolean authenticate() {
-        return loginStatus;
-    }
-
-    /**
-     * Returns logged in user.
-     */
-    public User getUser(User user) {
-        return user;
-    }
-
     /**
      * Sets the current user.
      */
