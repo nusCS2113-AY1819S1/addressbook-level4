f591a198e2430f13f44eca53d81d5d50ecc6e30e
nguyenngoclinhchi <nguyenngoclinhchi@u.nus.edu>
2018-10-19 09:34:20 +0800

    Export_Excel  command modified v1.2

--- src/main/java/seedu/planner/logic/commands/ExportExcelCommand.java
+++ src/main/java/seedu/planner/logic/commands/ExportExcelCommand.java
@@ -12,6 +12,7 @@ import java.util.logging.Logger;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.charts.LineChartData;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
