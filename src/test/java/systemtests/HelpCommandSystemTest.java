093eb80afc1153c62eb4aa5ebda51e420f38904b
Vincent <vencintgamer@gmail.com>
2018-10-19 15:58:22 +0800

    Included more tests

--- src/test/java/systemtests/HelpCommandSystemTest.java
+++ src/test/java/systemtests/HelpCommandSystemTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertTrue;
 import static seedu.planner.testutil.TypicalIndexes.INDEX_FIRST_RECORD;
 import static seedu.planner.ui.testutil.GuiTestAssert.assertListMatching;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import guitests.GuiRobot;
@@ -20,7 +19,6 @@ import seedu.planner.ui.StatusBarFooter;
 /**
  * A system test class for the help window, which contains interaction with other UI components.
  */
-@Ignore
 public class HelpCommandSystemTest extends FinancialPlannerSystemTest {
     private static final String ERROR_MESSAGE = "ATTENTION!!!! : On some computers, this test may fail when run on "
             + "non-headless mode as FxRobot#clickOn(Node, MouseButton...) clicks on the wrong location. We suspect "
