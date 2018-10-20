53ffdd86d5e0589b5b1c932d5d9a5cfcf3e32226
lws803 <lws803@gmail.com>
2018-10-20 21:01:57 +0800

    Added new file encryptor exception

--- src/main/java/seedu/address/commons/util/FileEncryptor.java
+++ src/main/java/seedu/address/commons/util/FileEncryptor.java
@@ -1,6 +1,8 @@
 //@@author lws803
 package seedu.address.commons.util;
 
+import seedu.address.commons.exceptions.FileEncryptorException;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -27,10 +29,11 @@ public class FileEncryptor {
     public static final String MESSAGE_ADDRESS_BOOK_LOCKED = "Address book is locked, "
             + "please key in password";
     public static final String MESSAGE_PASSWORD_ALNUM = "Password must be alpha numeric";
+    public static final String MESSAGE_DECRYPTED = "decrypted";
+    public static final String MESSAGE_ENCRYPTED = "encrypted";
 
     private static String extension = ".encrypted";
     private static String filename = "";
-    private static String message = "";
 
     private static final byte[] salt = {
         (byte) 0x43, (byte) 0x76, (byte) 0x95, (byte) 0xc7,
@@ -46,30 +49,37 @@ public class FileEncryptor {
      * will also check if file is present first
      * @param password is obtained from PasswordCommand class
      */
-    public void process (String password) {
+    public String process (String password) throws FileEncryptorException {
 
         File f = new File(filename);
         File fEncrypted = new File(filename + extension);
 
         try {
+            if (!isAlphanumeric(password)) {
+                throw new FileEncryptorException(MESSAGE_PASSWORD_ALNUM);
+            }
+
             if (fEncrypted.exists() && !fEncrypted.isDirectory() && f.exists() && !f.isDirectory()) {
+                String message;
                 message = "File not decrypted, existing encrypted file already exist\n"
                         + "Please delete the newly created XML file";
+                throw new FileEncryptorException(message);
             } else if (f.exists() && !f.isDirectory()) {
                 encryptFile(filename, password);
-                message = "File encrypted!";
-                // TODO: Send a request to refresh the addressbook
+                return MESSAGE_ENCRYPTED;
+
             } else if (fEncrypted.exists() && !fEncrypted.isDirectory()) {
                 decryptFile(filename, password);
-                message = "File decrypted!";
+                return MESSAGE_DECRYPTED;
             }
 
         } catch (IOException e) {
-            message = e.getMessage();
+            throw new FileEncryptorException(e.getMessage());
         } catch (GeneralSecurityException e) {
-            // message = e.getMessage();
-            message = "Password mismatch!";
+            throw new FileEncryptorException("Password mismatch!");
         }
+
+        return "decrypted";
     }
 
     /**
@@ -204,20 +214,13 @@ public class FileEncryptor {
         target.close();
     }
 
-    /**
-     * Gets return message
-     */
-    public String getMessage () {
-        return this.message;
-    }
-
 
     /**
      * Check for alphanumeric
      * @param str
      * @return
      */
-    public boolean isAlphanumeric (String str) {
+    private boolean isAlphanumeric (String str) {
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             if (c < 0x30 || (c >= 0x3a && c <= 0x40) || (c > 0x5a && c <= 0x60) || c > 0x7a) {
--- src/test/java/seedu/address/logic/commands/PasswordCommandTest.java
+++ src/test/java/seedu/address/logic/commands/PasswordCommandTest.java
@@ -9,6 +9,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import seedu.address.commons.util.FileEncryptor;
@@ -22,30 +24,44 @@ public class PasswordCommandTest {
     private String tempFileName = "test.tmp";
     private String toWrite = "Hello";
 
+    @Before
+    public void setup () throws IOException {
+        File tmpFile = new File(tempFileName);
+        FileWriter writer = new FileWriter(tmpFile);
+        writer.write(toWrite);
+        writer.close();
+    }
+
     /**
      * Encryption and decryption test command
      */
     @Test
     public void encryptDecryptTest () throws IOException {
-        File tmpFile = new File(tempFileName);
-        FileWriter writer = new FileWriter(tmpFile);
-        writer.write(toWrite);
-        writer.close();
 
         FileEncryptor feEncrypt = new FileEncryptor(tempFileName);
         feEncrypt.process(password);
         assertEquals("File encrypted!", feEncrypt.getMessage());
 
-
         FileEncryptor feDecrypt = new FileEncryptor(tempFileName);
         feDecrypt.process(password);
         assertEquals("File decrypted!", feDecrypt.getMessage());
-
 
         BufferedReader reader = new BufferedReader(new FileReader(tempFileName));
         assertEquals(toWrite, reader.readLine());
         reader.close();
 
+        String expectedMessage =
+                String.format("");
+
+
+    }
+
+
+    @After
+    public void cleanup () {
+        File tmpFile = new File(tempFileName);
         tmpFile.delete(); // Delete and end off
     }
+
+
 }
