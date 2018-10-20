fe386e6eab99ed53342607414c069f0da0126384
novin997 <e0176909@u.nus.edu>
2018-10-20 23:31:28 +0800

    add testing to ScriptCommand
    -add test text files
    -add ScriptCommandTest and CommandTypeTest for doing unit testing

---
+++ src/test/java/seedu/address/model/scripts/CommandTypeTest.java
@@ -0,0 +1,84 @@
+package seedu.address.model.scripts;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.model.script.CommandType;
+import seedu.address.testutil.Assert;
+
+public class CommandTypeTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new CommandType(null));
+    }
+
+    @Test
+    public void constructor_invalidCommandType_throwsIllegalArgumentException() {
+        String invalidCommandType = "";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new CommandType(invalidCommandType));
+    }
+
+    @Test
+    public void isValidCommand() {
+        // null command
+        Assert.assertThrows(NullPointerException.class, () -> CommandType.isValidCommand(null));
+
+        // blank command
+        assertFalse(CommandType.isValidCommand("")); // empty string
+        assertFalse(CommandType.isValidCommand(" ")); // spaces only
+
+        // invalid commands
+        assertFalse(CommandType.isValidCommand("abc"));
+        assertFalse(CommandType.isValidCommand("efg"));
+        assertFalse(CommandType.isValidCommand("@2123"));
+        assertFalse(CommandType.isValidCommand("123"));
+        assertFalse(CommandType.isValidCommand("novin"));
+
+        // valid commands
+        assertTrue(CommandType.isValidCommand("add"));
+        assertTrue(CommandType.isValidCommand("a"));
+        assertTrue(CommandType.isValidCommand("addgroup"));
+        assertTrue(CommandType.isValidCommand("addgrp"));
+        assertTrue(CommandType.isValidCommand("add_testmarks"));
+        assertTrue(CommandType.isValidCommand("addt"));
+        assertTrue(CommandType.isValidCommand("clear"));
+        assertTrue(CommandType.isValidCommand("c"));
+        assertTrue(CommandType.isValidCommand("group"));
+        assertTrue(CommandType.isValidCommand("grp"));
+        assertTrue(CommandType.isValidCommand("delete"));
+        assertTrue(CommandType.isValidCommand("d"));
+        assertTrue(CommandType.isValidCommand("distinto"));
+        assertTrue(CommandType.isValidCommand("di"));
+        assertTrue(CommandType.isValidCommand("edit"));
+        assertTrue(CommandType.isValidCommand("e"));
+        assertTrue(CommandType.isValidCommand("edit_test"));
+        assertTrue(CommandType.isValidCommand("et"));
+        assertTrue(CommandType.isValidCommand("sendmail"));
+        assertTrue(CommandType.isValidCommand("sm"));
+        assertTrue(CommandType.isValidCommand("exit"));
+        assertTrue(CommandType.isValidCommand("ex"));
+        assertTrue(CommandType.isValidCommand("find"));
+        assertTrue(CommandType.isValidCommand("f"));
+        assertTrue(CommandType.isValidCommand("genlist"));
+        assertTrue(CommandType.isValidCommand("gl"));
+        assertTrue(CommandType.isValidCommand("display"));
+        assertTrue(CommandType.isValidCommand("disp"));
+        assertTrue(CommandType.isValidCommand("help"));
+        assertTrue(CommandType.isValidCommand("h"));
+        assertTrue(CommandType.isValidCommand("history"));
+        assertTrue(CommandType.isValidCommand("his"));
+        assertTrue(CommandType.isValidCommand("list"));
+        assertTrue(CommandType.isValidCommand("l"));
+        assertTrue(CommandType.isValidCommand("listgroup"));
+        assertTrue(CommandType.isValidCommand("lg"));
+        assertTrue(CommandType.isValidCommand("redo"));
+        assertTrue(CommandType.isValidCommand("r"));
+        assertTrue(CommandType.isValidCommand("select"));
+        assertTrue(CommandType.isValidCommand("s"));
+        assertTrue(CommandType.isValidCommand("undo"));
+        assertTrue(CommandType.isValidCommand("u"));
+    }
+}
