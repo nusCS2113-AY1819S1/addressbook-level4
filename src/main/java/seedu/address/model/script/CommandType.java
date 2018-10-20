fabd9839e980dc4d38546d299b211e37beeddc8c
novin997 <e0176909@u.nus.edu>
2018-10-20 16:09:30 +0800

    add CommandType and TextFile to model

---
+++ src/main/java/seedu/address/model/script/CommandType.java
@@ -0,0 +1,47 @@
+package seedu.address.model.script;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class CommandType {
+    public static final String MESSAGE_MESSAGE_CONSTRAINTS =
+            "Command string is not valid";
+    public static final String MESSAGE_VALIDATION_REGEX = "^(add|a|addgroup|addgrp|add_testmarks|addt|clear|c|group|"
+            + "grp|delete|d|distinto|di|edit|e|edit_test|et|sendmail|sm|exit|ex|find|f|genlist|gl|display|disp|"
+            + "help|h|history|his|list|l|listgroup|lg|redo|r|select|s|undo|u)$";
+    public final String value;
+    /**
+     * Constructs a {@code CommandType}.
+     *
+     * @param commandType A type of command to be executed.
+     */
+    public CommandType(String commandType) {
+        requireNonNull(commandType);
+        checkArgument(isValidCommand(commandType), MESSAGE_MESSAGE_CONSTRAINTS);
+        value = commandType;
+    }
+
+    /**
+     * Returns true if a given string is a valid message.
+     */
+    public static boolean isValidCommand(String test) {
+        return test.matches(MESSAGE_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof CommandType // instanceof handles nulls
+                && value.equals(((CommandType) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
---
+++ src/main/java/seedu/address/model/script/TextFile.java
@@ -0,0 +1,55 @@
+package seedu.address.model.script;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a text file name.
+ * Guarantees: immutable; is valid as declared in {@link #isValidTextFile(String)}
+ */
+public class TextFile {
+
+    public static final String MESSAGE_MESSAGE_CONSTRAINTS = "Text file name contains invalid characters";
+    public static final String MESSAGE_VALIDATION_REGEX = "([\\w.-]+)";
+    public final String value;
+
+    /**
+     * Constructs a {@code TextFile}.
+     *
+     * @param textFile A command to be executed.
+     */
+    public TextFile(String textFile) {
+        requireNonNull(textFile);
+        checkArgument(isValidTextFile(textFile), MESSAGE_MESSAGE_CONSTRAINTS);
+        value = textFile;
+    }
+
+    /**
+     * Returns true if a given string is a valid text file name.
+     */
+    public static boolean isValidTextFile(String test) {
+        return test.matches(MESSAGE_VALIDATION_REGEX);
+    }
+
+    public String getTextFile() {
+        return this.value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TextFile // instanceof handles nulls
+                && value.equals(((TextFile) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
