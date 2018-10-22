5768e4e2188798f8f18d7062f8048ef358d13206
Jeremy Aw <jeremyinelysium@gmail.com>
2018-10-22 20:57:14 +0800

    Changed Milestone class to no longer be a subclass of the Task class

--- src/main/java/seedu/address/model/task/Milestone.java
+++ src/main/java/seedu/address/model/task/Milestone.java
@@ -4,30 +4,33 @@ package seedu.address.model.task;
 /**
  * Represents a Milestone for any Task in the TaskBook
  */
-public class Milestone extends Task {
+public class Milestone {
     //private final String title;
-    //private final String milestone_description;
+    private final String milestoneDescription;
     private final String rank;
 
-    public Milestone(String title, String milestoneDescription, String rank) {
-        super(title, milestoneDescription, new PriorityLevel("high"));
-        //this.title = title;
-        //this.milestoneDescription = milestoneDescription;
+    public Milestone(String milestoneDescription, String rank) {
+        //super(title, milestoneDescription, new PriorityLevel("high"));
+        this.milestoneDescription = milestoneDescription;
         this.rank = rank;
+    }
+
+    public String getMilestoneDescription() {
+        return milestoneDescription;
     }
 
     public String getRank() {
         return rank;
     }
 
+    //need to edit this also
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
-        builder.append(getTitle())
-                .append(" : ")
-                .append(getDescription())
-                .append(" | ")
-                .append(getRank());
+        builder.append("Milestone ")
+                .append(getRank())
+                .append(": ")
+                .append(getMilestoneDescription());
         return builder.toString();
     }
 
