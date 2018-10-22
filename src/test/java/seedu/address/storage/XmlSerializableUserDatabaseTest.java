5eb8c581d885ea4d811c8fbcf615d5bd51f75e14
amirulmaricar <Amirul@comp.nus.edu.sg>
2018-10-22 16:21:00 +0800

    added duplicate users test

--- src/test/java/seedu/address/storage/XmlSerializableUserDatabaseTest.java
+++ src/test/java/seedu/address/storage/XmlSerializableUserDatabaseTest.java
@@ -15,9 +15,11 @@ import seedu.address.model.UserDatabase;
 import seedu.address.testutil.TypicalUsers;
 
 public class XmlSerializableUserDatabaseTest {
-    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "XmlSerializableUserDatabaseTest");
+    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data",
+            "XmlSerializableUserDatabaseTest");
     private static final Path TYPICAL_USERS_FILE = TEST_DATA_FOLDER.resolve("typicalUsersUserDatabase.xml");
     private static final Path INVALID_USER_FILE = TEST_DATA_FOLDER.resolve("invalidUserUserDatabase.xml");
+    private static final Path DUPLICATE_USER_FILE = TEST_DATA_FOLDER.resolve("duplicateUserUserDatabase.xml");
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -36,6 +38,15 @@ public class XmlSerializableUserDatabaseTest {
         XmlSerializableUserDatabase dataFromFile = XmlUtil.getDataFromFile(INVALID_USER_FILE,
                 XmlSerializableUserDatabase.class);
         thrown.expect(IllegalValueException.class);
+        dataFromFile.toModelType();
+    }
+
+    @Test
+    public void toModelType_duplicatePersons_throwsIllegalValueException() throws Exception {
+        XmlSerializableUserDatabase dataFromFile = XmlUtil.getDataFromFile(DUPLICATE_USER_FILE,
+                XmlSerializableUserDatabase.class);
+        thrown.expect(IllegalValueException.class);
+        thrown.expectMessage(XmlSerializableUserDatabase.MESSAGE_DUPLICATE_USER);
         dataFromFile.toModelType();
     }
 }
