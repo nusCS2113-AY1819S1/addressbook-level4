168092cd4e2d95f1eba86c4c7915699dae824c30
jamesyaputra <jamesyaputra1704@gmail.com>
2018-10-22 22:46:12 +0800

    Update LoginCommandParserTest

--- src/test/java/seedu/address/logic/parser/LoginCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/LoginCommandParserTest.java
@@ -12,7 +12,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
-public class LoginCommandTestParser {
+public class LoginCommandParserTest {
     private LoginCommandParser parser = new LoginCommandParser();
 
     @Test
@@ -21,8 +21,13 @@ public class LoginCommandTestParser {
         Password password = new Password(VALID_PASSWORD);
         User expectedUser = new User(username, password);
 
+        // whitespace only preamble
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + USERNAME_DESC + PASSWORD_DESC,
+                new LoginCommand(expectedUser));
+
         // valid username and password
-        assertParseSuccess(parser, USERNAME_DESC + PASSWORD_DESC, new LoginCommand(expectedUser));
+        assertParseSuccess(parser, USERNAME_DESC + PASSWORD_DESC,
+                new LoginCommand(expectedUser));
     }
 
     @Test
@@ -37,5 +42,24 @@ public class LoginCommandTestParser {
 
         // missing all prefixes
         assertParseFailure(parser, VALID_USERNAME + VALID_PASSWORD, expectedMessage);
+    }
+
+    @Test
+    public void parse_invalidValue_failure() {
+        // invalid username
+        assertParseFailure(parser, INVALID_USERNAME_DESC + PASSWORD_DESC,
+                Username.MESSAGE_USERNAME_CONSTRAINTS);
+
+        // invalid password
+        assertParseFailure(parser, USERNAME_DESC + INVALID_PASSWORD_DESC,
+                Password.MESSAGE_PASSWORD_CONSTRAINTS);
+
+        // two invalid values, only first invalid value reported
+        assertParseFailure(parser, INVALID_USERNAME_DESC + INVALID_PASSWORD_DESC,
+                Username.MESSAGE_USERNAME_CONSTRAINTS);
+
+        // non-empty preamble
+        assertParseFailure(parser, PREAMBLE_NON_EMPTY + USERNAME_DESC + PASSWORD_DESC,
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, LoginCommand.MESSAGE_USAGE));
     }
 }
