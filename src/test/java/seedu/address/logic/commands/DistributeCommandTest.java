d124878955110687c7501fc727637ecc4c9457a1
tsuweiquan <e0176844@u.nus.edu>
2018-10-22 13:51:04 +0800

    Test Case Covered for Equals method in DistributeCommand.java
    Test Case are:
    1. Comparing Same Object
    2. Comparing Same Values
    3. Comparing Different Index in Object
    4. Comparing Different GroupName in Object
    5. Comparing Different Gender Flag in Object
    6. Comparing Different Nationality Flag in Object
    7. Comparing Object with null
    8. Comparing a Different Object

--- src/test/java/seedu/address/logic/commands/DistributeCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DistributeCommandTest.java
@@ -1,13 +1,18 @@
 package seedu.address.logic.commands;
 
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.distribute.Distribute;
+import seedu.address.model.group.GroupName;
 
 public class DistributeCommandTest {
     private Model model;
@@ -18,6 +23,54 @@ public class DistributeCommandTest {
     public void setUp() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void equals() {
+        //TODO create a builder for this.
+        Distribute first = new Distribute(3, new GroupName("First-Group"), true, false);
+        Distribute second = new Distribute(3, new GroupName("First-Group"), true, false);
+        Distribute differentIndex = new Distribute(4, new GroupName("First-Group"), true, false);
+        Distribute differentGroupName = new Distribute(4, new GroupName("Different-Group"), true, false);
+        Distribute differentGenderFlag = new Distribute(4, new GroupName("First-Group"), false, false);
+        Distribute differentNationalityFlag = new Distribute(4, new GroupName("First-Group"), true, true);
+        Distribute allDifferent = new Distribute(2, new GroupName("CS2113-T13-"), false, true);
+
+        DistributeCommand distributeFirstCommand = new DistributeCommand(first);
+        DistributeCommand distributeSecondCommand = new DistributeCommand(second);
+        DistributeCommand differentIndexCommand = new DistributeCommand(differentIndex);
+        DistributeCommand differentGroupNameCommand = new DistributeCommand(differentGroupName);
+        DistributeCommand differentGenderFlagCommand = new DistributeCommand(differentGenderFlag);
+        DistributeCommand differentNationalityFlagCommand = new DistributeCommand(differentNationalityFlag);
+        DistributeCommand allDifferentCommand = new DistributeCommand(allDifferent);
+
+
+        // same object -> returns true
+        assertTrue(distributeFirstCommand.equals(distributeSecondCommand));
+
+        // same values -> returns true
+        DistributeCommand forthCommand = new DistributeCommand(new Distribute(3,
+                new GroupName("First-Group"), true, false));
+        assertTrue(forthCommand.equals(distributeFirstCommand));
+
+
+        // different Index
+        assertFalse(distributeFirstCommand.equals(differentIndexCommand));
+
+        // different GroupName
+        assertFalse(distributeFirstCommand.equals(differentGroupNameCommand));
+
+        // different GenderFlag
+        assertFalse(distributeFirstCommand.equals(differentGenderFlagCommand));
+
+        // different NationalityFlag
+        assertFalse(distributeFirstCommand.equals(differentNationalityFlagCommand));
+
+        // null -> returns false
+        assertFalse(distributeFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(distributeFirstCommand.equals(allDifferentCommand));
     }
 
 }
