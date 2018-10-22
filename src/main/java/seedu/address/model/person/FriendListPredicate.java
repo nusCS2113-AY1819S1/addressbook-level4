3534f7a5dec3f699d52fe62b1bed28fa3c99d6ae
cjinting-nus <e0175290@u.nus.edu>
2018-10-22 09:57:23 +0800

    Removed self from friends and others list

--- src/main/java/seedu/address/model/person/FriendListPredicate.java
+++ src/main/java/seedu/address/model/person/FriendListPredicate.java
@@ -3,12 +3,15 @@ package seedu.address.model.person;
 import java.util.Set;
 import java.util.function.Predicate;
 
+import seedu.address.security.UserStub;
+
 /**
  * Creates a predicate from the Person to be used to create the friend list
  */
 public class FriendListPredicate implements Predicate<Person> {
 
     private final Set<Friend> friendList;
+    private Person currentUser = UserStub.getUser();
 
     public FriendListPredicate(Set<Friend> friendList) {
         this.friendList = friendList;
@@ -17,6 +20,9 @@ public class FriendListPredicate implements Predicate<Person> {
     @Override
     public boolean test(Person person) {
         for (Friend friend : friendList) {
+            if (currentUser.getName().equals(person.getName())) {
+                return false;
+            }
             if (person.getName().equals(friend.getFriendName())) {
                 return true;
             }
--- src/main/java/seedu/address/model/person/OtherListPredicate.java
+++ src/main/java/seedu/address/model/person/OtherListPredicate.java
@@ -3,12 +3,15 @@ package seedu.address.model.person;
 import java.util.Set;
 import java.util.function.Predicate;
 
+import seedu.address.security.UserStub;
+
 /**
  * Creates a predicate to be used to find the non-friends in persons list
  */
 public class OtherListPredicate implements Predicate<Person> {
 
     private final Set<Friend> otherList;
+    private Person currentUser = UserStub.getUser();
 
     public OtherListPredicate(Set<Friend> friendList) {
         this.otherList = friendList;
@@ -17,6 +20,9 @@ public class OtherListPredicate implements Predicate<Person> {
     @Override
     public boolean test(Person person) {
         for (Friend other : otherList) {
+            if (currentUser.getName().equals(person.getName())) {
+                return false;
+            }
             if (person.getName().equals(other.getFriendName())) {
                 return false;
             }
