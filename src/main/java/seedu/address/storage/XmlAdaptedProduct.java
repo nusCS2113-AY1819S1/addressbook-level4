cede120393585ba4f34dd14d86fe45cbe942eefe
Gara Tahir <garagaristahir@hotmail.com>
2018-10-23 15:43:05 +0800

    Fixing checkstyle

--- src/main/java/seedu/address/storage/XmlAdaptedProduct.java
+++ src/main/java/seedu/address/storage/XmlAdaptedProduct.java
@@ -10,10 +10,10 @@ import javax.xml.bind.annotation.XmlElement;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.distributor.DistributorName;
-import seedu.address.model.product.Name;
 import seedu.address.model.product.Email;
-import seedu.address.model.product.Product;
+import seedu.address.model.product.Name;
 import seedu.address.model.product.Address;
+import seedu.address.model.product.Product;
 import seedu.address.model.product.SerialNumber;
 import seedu.address.model.tag.Tag;
 
