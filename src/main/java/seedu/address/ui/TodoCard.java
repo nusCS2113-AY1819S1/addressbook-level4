072b21d947dbb0a13287a51eb984cfe59c0866e4
linnnruoo <linruo2014@gmail.com>
2018-10-23 10:38:43 +0800

    test travis

--- src/main/java/seedu/address/ui/TodoCard.java
+++ src/main/java/seedu/address/ui/TodoCard.java
@@ -6,6 +6,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.todo.Todo;
 
+//@@author linnnruoo
 /**
  * An UI component that displays information of a {@code Todo}.
  */
