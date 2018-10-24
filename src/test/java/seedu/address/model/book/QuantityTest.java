14133cd914c7306d6305edab2482fed83e42b318
guiyong96 <leeguiyong96@gmail.com>
2018-10-24 15:18:35 +0800

    Fix quantity test
    - Include refactors

--- src/test/java/seedu/address/model/book/QuantityTest.java
+++ src/test/java/seedu/address/model/book/QuantityTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.book;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
@@ -15,24 +16,24 @@ public class QuantityTest {
 
     @Test
     public void constructor_invalidAddress_throwsIllegalArgumentException() {
-        String invalidAddress = "";
-        Assert.assertThrows(IllegalArgumentException.class, () -> new Quantity(invalidAddress));
+        String invalidQuantity = "";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Quantity(invalidQuantity));
     }
 
     @Test
-    public void isValidAddress() {
-        // null address
+    public void isValidQuantity() {
+        // null quantities
         Assert.assertThrows(NullPointerException.class, () -> Quantity.isValidQuantity(null));
 
-        // invalid addresses
+        // invalid quantities
         assertFalse(Quantity.isValidQuantity("")); // empty string
         assertFalse(Quantity.isValidQuantity(" ")); // spaces only
+        assertFalse(Quantity.isValidQuantity("12492")); // Out of quantity range.
+        assertFalse(Quantity.isValidQuantity("-1")); // No negative
 
-        // valid addresses
-        /*
-        assertTrue(Quantity.isValidQuantity("Blk 456, Den Road, #01-355"));
-        assertTrue(Quantity.isValidQuantity("-")); // one character
-        assertTrue(Quantity.isValidQuantity("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long address
-        */
+        // valid quantities
+        assertTrue(Quantity.isValidQuantity("1")); // One digit
+        assertTrue(Quantity.isValidQuantity("15")); // Two digits
+        assertTrue(Quantity.isValidQuantity("152")); // Three digits
     }
 }
