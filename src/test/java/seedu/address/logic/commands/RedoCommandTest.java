fd7a44baf2cf850c6ec875e95f3cbb96227b5ff8
arty9 <tayrantian@gmail.com>
2018-10-24 17:09:39 +0800

    minor changes to tests

--- src/test/java/seedu/address/logic/commands/RedoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RedoCommandTest.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.commands;
-/*
+
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.deleteFirstPerson;
+import static seedu.address.logic.commands.CommandTestUtil.deleteFirstTask;
 import static seedu.address.testutil.TypicalExpenditures.getTypicalExpenditureTracker;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalTasks.getTypicalTodoList;
@@ -14,9 +14,9 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-*/
+
 public class RedoCommandTest {
-/*
+
     private final Model model = new ModelManager(getTypicalAddressBook(), getTypicalTodoList(),
             getTypicalExpenditureTracker(), new UserPrefs());
     private final Model expectedModel = new ModelManager(getTypicalAddressBook(), getTypicalTodoList(),
@@ -26,29 +26,29 @@ public class RedoCommandTest {
     @Before
     public void setUp() {
         // set up of both models' undo/redo history
-        deleteFirstPerson(model);
-        deleteFirstPerson(model);
-        model.undoAddressBook();
-        model.undoAddressBook();
+        deleteFirstTask(model);
+        deleteFirstTask(model);
+        model.undoTodoList();
+        model.undoTodoList();
 
-        deleteFirstPerson(expectedModel);
-        deleteFirstPerson(expectedModel);
-        expectedModel.undoAddressBook();
-        expectedModel.undoAddressBook();
+        deleteFirstTask(expectedModel);
+        deleteFirstTask(expectedModel);
+        expectedModel.undoTodoList();
+        expectedModel.undoTodoList();
     }
 
     @Test
     public void execute() {
         // multiple redoable states in model
-        expectedModel.redoAddressBook();
+        expectedModel.redoTodoList();
         assertCommandSuccess(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_SUCCESS, expectedModel);
 
         // single redoable state in model
-        expectedModel.redoAddressBook();
+        expectedModel.redoTodoList();
         assertCommandSuccess(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_SUCCESS, expectedModel);
 
         // no redoable state in model
         assertCommandFailure(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_FAILURE);
     }
-    */
+
 }
