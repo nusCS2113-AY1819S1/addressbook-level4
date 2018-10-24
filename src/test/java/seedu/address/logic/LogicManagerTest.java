8aa08fb96cd71ea3a5cd15168a0c7005034d799b
arty9 <tayrantian@gmail.com>
2018-10-24 17:46:06 +0800

    travis fix

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -1,8 +1,6 @@
 package seedu.address.logic;
 
 import static org.junit.Assert.assertEquals;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
-import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.testutil.TypicalExpenditures.getTypicalExpenditureTracker;
 
 import org.junit.Rule;
