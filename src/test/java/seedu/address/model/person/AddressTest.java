9c6e1130f5be20af980dfdf936035dceb25f2eab
Gara Tahir <garagaristahir@hotmail.com>
2018-10-24 11:28:08 +0800

    Addresstest now works

---
+++ src/test/java/seedu/address/model/person/AddressTest.java
@@ -0,0 +1,24 @@
+package seedu.address.model.product;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.testutil.Assert;
+
+public class AddressTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new Address(null));
+    }
+
+    @Test
+    public void constructor_invalidAddress_throwsIllegalArgumentException() {
+        String invalidAddress = "";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Address(invalidAddress));
+    }
+
+
+}
