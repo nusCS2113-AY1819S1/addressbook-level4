235415d42659f724e29d9cffbee60221fb7b9423
guiyong96 <leeguiyong96@gmail.com>
2018-10-24 15:15:28 +0800

    Fix price test
    - Some refactors

--- src/test/java/seedu/address/model/book/PriceTest.java
+++ src/test/java/seedu/address/model/book/PriceTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.book;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
@@ -14,50 +15,34 @@ public class PriceTest {
     }
 
     @Test
-    public void constructor_invalidEmail_throwsIllegalArgumentException() {
-        String invalidEmail = "";
-        Assert.assertThrows(IllegalArgumentException.class, () -> new Price(invalidEmail));
+    public void constructor_invalidPrice_throwsIllegalArgumentException() {
+        String invalidPrice = "";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Price(invalidPrice));
     }
 
     @Test
-    public void isValidEmail() {
-        // null email
+    public void isValidPrice() {
+        // null price
         Assert.assertThrows(NullPointerException.class, () -> Price.isValidPrice(null));
 
-        // blank email
+        // blank price
         assertFalse(Price.isValidPrice("")); // empty string
         assertFalse(Price.isValidPrice(" ")); // spaces only
 
         // missing parts
-        assertFalse(Price.isValidPrice("@example.com")); // missing local part
-        assertFalse(Price.isValidPrice("peterjackexample.com")); // missing '@' symbol
-        assertFalse(Price.isValidPrice("peterjack@")); // missing domain name
+        assertFalse(Price.isValidPrice("5.5")); // One decimal, no sign
+        assertFalse(Price.isValidPrice("$6.6")); // One decimal, with sign
+        assertFalse(Price.isValidPrice("$")); // missing domain
 
         // invalid parts
-        assertFalse(Price.isValidPrice("peterjack@-")); // invalid domain name
-        assertFalse(Price.isValidPrice("peterjack@exam_ple.com")); // underscore in domain name
-        assertFalse(Price.isValidPrice("peter jack@example.com")); // spaces in local part
-        assertFalse(Price.isValidPrice("peterjack@exam ple.com")); // spaces in domain name
-        assertFalse(Price.isValidPrice(" peterjack@example.com")); // leading space
-        assertFalse(Price.isValidPrice("peterjack@example.com ")); // trailing space
-        assertFalse(Price.isValidPrice("peterjack@@example.com")); // double '@' symbol
-        assertFalse(Price.isValidPrice("peter@jack@example.com")); // '@' symbol in local part
-        assertFalse(Price.isValidPrice("peterjack@example@com")); // '@' symbol in domain name
-        assertFalse(Price.isValidPrice("peterjack@.example.com")); // domain name starts with a period
-        assertFalse(Price.isValidPrice("peterjack@example.com.")); // domain name ends with a period
-        assertFalse(Price.isValidPrice("peterjack@-example.com")); // domain name starts with a hyphen
-        assertFalse(Price.isValidPrice("peterjack@example.com-")); // domain name ends with a hyphen
+        assertFalse(Price.isValidPrice("6.012")); // too many decimals
+        assertFalse(Price.isValidPrice("-3")); // no negative integers
+        assertFalse(Price.isValidPrice("a1.2")); // alphabet in string
 
         // valid email
-        /*
-        assertTrue(Price.isValidPrice("PeterJack_1190@example.com"));
-        assertTrue(Price.isValidPrice("a@bc")); // minimal
-        assertTrue(Price.isValidPrice("test@localhost")); // alphabets only
-        assertTrue(Price.isValidPrice("!#$%&'*+/=?`{|}~^.-@example.org")); // special characters local part
-        assertTrue(Price.isValidPrice("123@145")); // numeric local part and domain name
-        assertTrue(Price.isValidPrice("a1+be!@example1.com")); // mixture of alphanumeric and special characters
-        assertTrue(Price.isValidPrice("peter_jack@very-very-very-long-example.com")); // long domain name
-        assertTrue(Price.isValidPrice("if.you.dream.it_you.can.do.it@example.com")); // long local part
-        */
+        assertTrue(Price.isValidPrice("0.10")); // Omit dollar sign
+        assertTrue(Price.isValidPrice("$0.00")); // Dollar sign included
+        assertTrue(Price.isValidPrice("5")); // No decimals
+        assertTrue(Price.isValidPrice("$0.10")); // Only cents. No dollar.
     }
 }
