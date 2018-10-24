444f300c024c34c9f979d98d0a9748491129069d
feijunzi <e0253704@u.nus.edu>
2018-10-24 20:23:24 +0800

    travis

--- src/main/java/seedu/address/logic/commands/CheckExpenditureCommand.java
+++ src/main/java/seedu/address/logic/commands/CheckExpenditureCommand.java
@@ -68,11 +68,11 @@ public class CheckExpenditureCommand extends Command {
             int month = Integer.parseInt(editedExpenditure.getDate().toString().substring(3, 5));
             int day = Integer.parseInt(editedExpenditure.getDate().toString().substring(0, 2));
 
-            if ((year1 > year) || (year2 < year)) { }
+            if ((year1 > year) || (year2 < year))          { }
             else if ((month1 > month) || (month2 < month)) { }
-            else if ((day1 > day) || (day2 < day)) { }
+            else if ((day1 > day) || (day2 < day))         { }
             else {
-            total = total + Integer.parseInt(editedExpenditure.getMoney().toString());
+                total = total + Integer.parseInt(editedExpenditure.getMoney().toString());
             }
             index++;
         }
