ef5a1ca6d151a48cbe23a528985c992f8560f70e
bluein-green <j.minjia@gmail.com>
2018-10-24 18:47:46 +0800

    fixed 1 checkstyle error

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -30,7 +30,8 @@ import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
 
 /**
- * Contains integration tests (interaction with the DrinkModel, UndoCommand and RedoCommand) and unit tests for EditCommand.
+ * Contains integration tests (interaction with the DrinkModel, UndoCommand and RedoCommand)
+ * and unit tests for EditCommand.
  */
 public class EditCommandTest {
 
