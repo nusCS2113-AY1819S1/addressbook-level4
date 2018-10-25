7f3edc9fa6e99b50fc1025564822949796e309b8
bluein-green <j.minjia@gmail.com>
2018-10-25 18:22:36 +0800

    add stub Date class, waiting for XH's code

---
+++ src/main/java/seedu/address/model/drink/Date.java
@@ -0,0 +1,8 @@
+package seedu.address.model.drink;
+
+/**
+ * Represents a date.
+ * TODO: currently a stub, to put the BatchDate stuff into here, and let BatchDate inherit from Date
+ */
+public class Date {
+}
