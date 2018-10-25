8682a277edbc0c6d9d6e97358f345a058d8b1700
LowGinWee <notginwee@gmail.com>
2018-10-26 01:37:47 +0800

    Fixed linestyles

--- src/test/java/seedu/address/model/schedule/ActivityTest.java
+++ src/test/java/seedu/address/model/schedule/ActivityTest.java
@@ -12,31 +12,31 @@ import seedu.address.testutil.Assert;
 public class ActivityTest {
 
 	@Test
-	public void constructor_null_throwsNullPointerException() {
-		Assert.assertThrows(NullPointerException.class, () -> new Activity(null, null));
-	}
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new Activity(null, null));
+    }
 
-	@Test
-	public void constructor_invalidActivity_throwsIllegalArgumentException() {
-		String invalidActivity = "";
-		Date validDate = Activity.toDate(01, 01, 2000);
-		Assert.assertThrows(IllegalArgumentException.class, () -> new Activity(validDate, invalidActivity));
-	}
+    @Test
+    public void constructor_invalidActivity_throwsIllegalArgumentException() {
+        String invalidActivity = "";
+        Date validDate = Activity.toDate(01, 01, 2000);
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Activity(validDate, invalidActivity));
+    }
 
-	@Test
-	public void isValidDate() {
-		// null date
-		Assert.assertThrows(NullPointerException.class, () -> Activity.isValidDate(null));
+    @Test
+    public void isValidDate() {
+        // null date
+        Assert.assertThrows(NullPointerException.class, () -> Activity.isValidDate(null));
 
-		// invalid date
-		assertFalse(Activity.isValidDate("")); // empty string
-		assertFalse(Activity.isValidDate(" ")); // spaces only
-		assertFalse(Activity.isValidDate("30/02/2000")); // invalid date i.e. 30th feb
-		assertFalse(Activity.isValidDate("01/99/2000")); // invalid month
-		assertFalse(Activity.isValidDate("99/01/2000")); // invalid date
+        // invalid date
+        assertFalse(Activity.isValidDate("")); // empty string
+        assertFalse(Activity.isValidDate(" ")); // spaces only
+        assertFalse(Activity.isValidDate("30/02/2000")); // invalid date i.e. 30th feb
+        assertFalse(Activity.isValidDate("01/99/2000")); // invalid month
+        assertFalse(Activity.isValidDate("99/01/2000")); // invalid date
 
-		// valid date
-		assertTrue(Activity.isValidDate("01/01/2000")); //valid date
-		assertTrue(Activity.isValidDate("29/02/2020")); //leap day
-	}
+        // valid date
+        assertTrue(Activity.isValidDate("01/01/2000")); //valid date
+        assertTrue(Activity.isValidDate("29/02/2020")); //leap day
+    }
 }
