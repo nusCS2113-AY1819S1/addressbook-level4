21c1d434e1c0fc1877022a45ac2cf0ef0405a3d0
lekoook <kokteng1313@gmail.com>
2018-10-25 18:25:04 +0800

    Add TrieNode test code
    Test codes for TrieNode class is added.

---
+++ src/test/java/seedu/address/model/trie/TrieTest.java
@@ -0,0 +1,13 @@
+package seedu.address.model.trie;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TrieTest {
+}
