e30e985d96e522c4f0cfbd2e03bc4d5c4b002664
ladderinc <boon-jun1996@hotmail.com>
2018-10-27 01:46:37 +0800

    Removed unused import from EditCommandSystemTest.java

--- src/test/java/systemtests/EditCommandSystemTest.java
+++ src/test/java/systemtests/EditCommandSystemTest.java
@@ -24,7 +24,6 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.AMY;
