ca552a67879b04dd6a6e4dd1ea2ca3d9dbd0b2f9
cjinting-nus <e0175290@u.nus.edu>
2018-10-26 17:14:45 +0800

    Fix fix

--- src/main/java/seedu/address/ui/FriendListPanel.java
+++ src/main/java/seedu/address/ui/FriendListPanel.java
@@ -39,7 +39,6 @@ public class FriendListPanel extends UiPart<Region> {
     }
 
     private void setEventHandlerForSelectionChangeEvent() {
-        friendListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         friendListView.getSelectionModel().selectedItemProperty()
                 .addListener((observable, oldValue, newValue) -> {
                     if (newValue != null) {
