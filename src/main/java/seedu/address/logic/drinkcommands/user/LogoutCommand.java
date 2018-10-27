c3f47d3610cecc45a1fa27e0d7f15887f5ff1a9f
bluein-green <j.minjia@gmail.com>
2018-10-28 07:56:28 +0800

    LogoutCommand: will function as exit

--- src/main/java/seedu/address/logic/drinkcommands/user/LogoutCommand.java
+++ src/main/java/seedu/address/logic/drinkcommands/user/LogoutCommand.java
@@ -3,24 +3,22 @@ package seedu.address.logic.drinkcommands.user;
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.LogoutEvent;
 import seedu.address.logic.CommandHistory;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.drinkcommands.DrinkCommand;
 import seedu.address.logic.drinkcommands.DrinkCommandResult;
 import seedu.address.model.DrinkModel;
-import seedu.address.model.Model;
 
 /**
- * Terminate Ui and return user to login sceen
+ * Terminate Ui and return user to login sceen.
+ * This functions as the exit command.
  */
 public class LogoutCommand extends DrinkCommand {
     public static final String COMMAND_WORD = "logout";
 
-    public static final String MESSAGE_LOGOUT_ACKNOWLEDGEMENT = "Logout Address Book as requested ...";
+    public static final String MESSAGE_LOGOUT_ACKNOWLEDGEMENT = "Logging out Address Book as requested ...";
 
     @Override
     public DrinkCommandResult execute(DrinkModel model, CommandHistory history) {
-        EventsCenter.getInstance().post(new LogoutEvent ());
+        EventsCenter.getInstance().post(new LogoutEvent());
         return new DrinkCommandResult(MESSAGE_LOGOUT_ACKNOWLEDGEMENT);
     }
 }
