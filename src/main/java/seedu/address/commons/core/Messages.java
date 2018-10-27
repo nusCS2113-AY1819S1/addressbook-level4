1bbf48fd98898fe946bec5aa86588fcb5d93da42
bluein-green <j.minjia@gmail.com>
2018-10-28 07:56:06 +0800

    Messages.java: added in old messages to not conflict. will be removed later

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -10,4 +10,8 @@ public class Messages {
     public static final String MESSAGE_INVALID_DRINK_DISPLAYED_INDEX = "The drink index provided is invalid";
     public static final String MESSAGE_DRINKS_LISTED_OVERVIEW = "%1$d drinks listed!";
 
+    // TODO: remove
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "Person index provided is invalid // TO REMOVE";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "1$d persons listed!";
+
 }
