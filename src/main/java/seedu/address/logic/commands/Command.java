100bed5a7826a763e0ef84bf24370743645ba4dd
bluein-green <j.minjia@gmail.com>
2018-10-27 23:07:07 +0800

    Command.java: supposed to change Model to DrinkModel

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.DrinkModel;
+import seedu.address.model.Model;
 
 
 /**
@@ -18,6 +19,6 @@ public abstract class Command {
      * @return feedback message of the operation result for display
      * @throws CommandException If an error occurs during command execution.
      */
-    public abstract CommandResult execute(DrinkModel model, CommandHistory history) throws CommandException;
+    public abstract CommandResult execute(Model model, CommandHistory history) throws CommandException;
 
 }
