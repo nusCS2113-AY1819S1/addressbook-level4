fc7a1350c7d9d212a354a72b7886b5875ba41365
bluein-green <j.minjia@gmail.com>
2018-10-27 23:03:21 +0800

    DrinkLogic: morphed from logic

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -2,7 +2,7 @@ package seedu.address.logic.commands;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.Model;
+import seedu.address.model.DrinkModel;
 
 
 /**
@@ -18,6 +18,6 @@ public abstract class Command {
      * @return feedback message of the operation result for display
      * @throws CommandException If an error occurs during command execution.
      */
-    public abstract CommandResult execute(Model model, CommandHistory history) throws CommandException;
+    public abstract CommandResult execute(DrinkModel model, CommandHistory history) throws CommandException;
 
 }
