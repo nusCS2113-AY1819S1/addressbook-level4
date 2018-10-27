ddcd29495c267db2e9abc95c8a34566abb1f53b2
chiaxr <chia.xiangrong@gmail.com>
2018-10-27 20:09:14 +0800

    Create RegisterCommandTest.java

---
+++ src/test/java/seedu/address/logic/commands/RegisterCommandTest.java
@@ -0,0 +1,58 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showEventAtIndex;
+import static seedu.address.testutil.TypicalEvents.getTypicalEventManager;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_EVENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_EVENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_EVENT;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.ui.testutil.EventsCollectorRule;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code RegisterCommand}.
+ */
+
+public class RegisterCommandTest {
+    private Model model = new ModelManager(getTypicalEventManager(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void execute_UnregisteredEventUnfilteredList_success() {
+
+    }
+
+    @Test
+    public void execute_UnregisteredEventFilteredList_success() {
+
+    }
+
+    @Test
+    public void executeUndoRedo_UnregisteredEventFilteredList_success() {
+
+    }
+
+    @Test
+    public void execute_RegisteredEventUnfilteredList_failure() {
+
+    }
+
+    @Test
+    public void equals() {
+
+    }
+}
