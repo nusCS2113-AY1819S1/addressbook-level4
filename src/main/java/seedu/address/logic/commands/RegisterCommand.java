68ee45a8f32f7dff960b079aaf8525fe7d994f5b
chiaxr <chia.xiangrong@gmail.com>
2018-10-27 18:31:29 +0800

    Fixed import order

--- src/main/java/seedu/address/logic/commands/RegisterCommand.java
+++ src/main/java/seedu/address/logic/commands/RegisterCommand.java
@@ -2,8 +2,8 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import static seedu.address.logic.commands.EditCommand.createEditedEvent;
 import static seedu.address.logic.commands.EditCommand.EditEventDescriptor;
+import static seedu.address.logic.commands.EditCommand.createEditedEvent;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EVENTS;
 
 import java.util.HashSet;
