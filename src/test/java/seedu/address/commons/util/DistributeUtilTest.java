a486573f3c1cd4928101afaba7cb4665d04d5a22
tsuweiquan <e0176844@u.nus.edu>
2018-10-27 19:12:46 +0800

    Implement several Unit Testing for DistributeUtil methods.
    Modified Several Methods to check for Null inputs.
    Methods affected are:
    1. shuffleTest
    2. createNationalityTest
    3. findPersonTest
    4. numberOfDifferentNationalityTest
    5. paxPerNationalityTest
    6. filterGenderTest
    7. groupNameConcatenationTest
    8. groupBuilderTest
    9. createGroupWithoutCommitTest
    10. addPersonIntoGroupWithoutCommitTest
    11. returnGroupIndexTest
    
    New Test implemented are :
    filterGenderTest
    - Check if first input arg is null
    - Check if second input arg is null
    - Check if third iniput arg is null
    - Check if method output produce a LinkList of all Male Person
    - Check if method output produce a LinkList of all Female Person
    - Check if method output produce a empty LinkList when the Gender Parameter is invalid
    - Check if method output produce an empty linklist when an empty input is fed.

--- src/test/java/seedu/address/commons/util/DistributeUtilTest.java
+++ src/test/java/seedu/address/commons/util/DistributeUtilTest.java
@@ -1,37 +1,56 @@
 package seedu.address.commons.util;
 
-import static java.util.Comparator.comparing;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static seedu.address.commons.util.DistributeUtil.GROUP_LOCATION;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_GENDER_FLAG_WORD;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_GENDER_FEMALE;
+import static seedu.address.model.person.Gender.VALID_GENDER_MALE;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.DANIEL;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalPersons.getTypicalPersonNationalityMap;
 
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Random;
-import java.util.stream.Stream;
+import java.util.Set;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import javafx.collections.ObservableList;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.group.Group;
+import seedu.address.model.group.GroupLocation;
+import seedu.address.model.group.GroupName;
 import seedu.address.model.person.Nationality;
 import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
+import seedu.address.testutil.Assert;
+import seedu.address.testutil.GroupBuilder;
+import seedu.address.testutil.TypicalPersons;
 
 public class DistributeUtilTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     private ObservableList<Person> allPersons = model.getFilteredPersonList();
     private DistributeUtil distUtil = new DistributeUtil();
+    private CommandHistory commandHistory = new CommandHistory();
 
     private ObservableList<Person> setUpObservableListStub() {
         AddressBook stubAddressBook = getTypicalAddressBook();
@@ -43,14 +62,25 @@ public class DistributeUtilTest {
 
     @Test
     public void shuffleTest() {
-        DistributeUtil testPerson = new DistributeUtil();
-        Random testValue = new Random(96259561);
+        Random stubSeed = new Random(96259561);
         LinkedList<Person> allPersonList = new LinkedList<>(allPersons);
-        Collections.shuffle(allPersonList, testValue);
-        assertTrue(testPerson.shuffle(allPersonList, testValue).equals(allPersonList));
+        Collections.shuffle(allPersonList, stubSeed);
+        assertTrue(distUtil.shuffle(allPersonList, stubSeed).equals(allPersonList));
+
+        //Test when the seed is null
+        Assert.assertThrows(NullPointerException.class, () -> distUtil.shuffle(allPersonList, null));
+
+        //Test when LinkList is null
+        Assert.assertThrows(NullPointerException.class, () -> distUtil.shuffle(null, stubSeed));
+
+
+        //if the allperson list is empty
+        allPersonList.clear();
+        Collections.shuffle(allPersonList, stubSeed);
+        assertTrue(distUtil.shuffle(allPersonList, stubSeed).equals(allPersonList));
+
     }
 
-    //TODO junit testing for distutil
     @Test
     public void createNationalityMapTest() {
         //create a stub
@@ -62,6 +92,12 @@ public class DistributeUtilTest {
         LinkedList<Person> allPersonLlStub = new LinkedList<>(allPersonsStub);
         Nationality sgNationality = new Nationality("SG");
         Person actualPerson = distUtil.findPerson(sgNationality, allPersonLlStub);
+
+        // Testing if nationality is null
+        Assert.assertThrows(NullPointerException.class, () -> distUtil.findPerson(null, allPersonLlStub));
+
+        // Testing if LinkedList is null
+        Assert.assertThrows(NullPointerException.class, () -> distUtil.findPerson(sgNationality, null));
 
         // testing if it return the first person it found with SG nationality
         assertEquals(ALICE, actualPerson);
@@ -86,6 +122,9 @@ public class DistributeUtilTest {
         LinkedList<Person> allPersonLlStub = new LinkedList<>(allPersonsStub);
         Map<Nationality, Long> actualMap = distUtil.numberOfDifferentNationality(allPersonLlStub);
         Map<Nationality, Long> expectedMap = getTypicalPersonNationalityMap();
+
+        // Testing if LinkList is null
+        Assert.assertThrows(NullPointerException.class, () -> distUtil.numberOfDifferentNationality(null));
 
         //Comparing both object are the same.
         assertEquals(expectedMap, actualMap);
@@ -116,6 +155,9 @@ public class DistributeUtilTest {
         Map<Nationality, Long> actualSortedMap = distUtil.paxPerNationality(differentNationalityMap);
         String expectedResult = "{SG=4, US=2, MY=1}";
 
+        // Testing if Map is null
+        Assert.assertThrows(NullPointerException.class, () -> distUtil.paxPerNationality(null));
+
         //expects the map to be sorted in value order.
         assertEquals(expectedResult, actualSortedMap.toString());
 
@@ -143,16 +185,50 @@ public class DistributeUtilTest {
 
     @Test
     public void filterGenderTest() {
+        ObservableList<Person> allStubPerson = setUpObservableListStub();
+        LinkedList<Person> allPerson = new LinkedList<>(allStubPerson);
+        LinkedList<Person> actualGenderPersons = new LinkedList<>();
+        distUtil.filterGender(allPerson, actualGenderPersons, VALID_GENDER_MALE);
 
-    }
+        //check if LinkList for allPerson is null
+        Assert.assertThrows(NullPointerException.class, () -> distUtil.filterGender(null,
+                actualGenderPersons, VALID_GENDER_MALE));
 
-    @Test
-    public void groupNameConcatenationTest() {
+        //check if LinkList for filteredGender is null
+        Assert.assertThrows(NullPointerException.class, () -> distUtil.filterGender(allPerson,
+                null, VALID_GENDER_MALE));
 
+        //check if Gender String is null
+        Assert.assertThrows(NullPointerException.class, () -> distUtil.filterGender(allPerson,
+                actualGenderPersons, null));
+
+        //checks object if similar LinkList of all male persons
+        assertEquals(TypicalPersons.allMalePerson(), actualGenderPersons);
+
+        //checks object if similar LinkList of all female persons
+        actualGenderPersons.clear();
+        distUtil.filterGender(allPerson, actualGenderPersons, VALID_GENDER_FEMALE);
+        assertEquals(TypicalPersons.allFemalePerson(), actualGenderPersons);
+
+        //Invalid Gender Parameter
+        actualGenderPersons.clear();
+        distUtil.filterGender(allPerson, actualGenderPersons, INVALID_GENDER_FLAG_WORD);
+        LinkedList<Person> expectedOutput = new LinkedList<>();
+        assertEquals(expectedOutput, actualGenderPersons);
+
+        //empty person list with male filter
+        allPerson.clear();
+        distUtil.filterGender(allPerson, actualGenderPersons, VALID_GENDER_MALE);
+        assertEquals(expectedOutput, actualGenderPersons);
     }
 
     @Test
     public void existDuplicateGroupTest() {
+
+    }
+
+    @Test
+    public void groupNameConcatenationTest() throws CommandException {
 
     }
 
