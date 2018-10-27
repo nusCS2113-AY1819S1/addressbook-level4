4ecc3277351deb9ee602bbb98f9cae2475e93fa9
bluein-green <j.minjia@gmail.com>
2018-10-27 23:42:14 +0800

    Command.java: revert to original, created a new class just for drink commands

--- src/main/java/seedu/address/logic/drinkcommands/AddItemCommand.java
+++ src/main/java/seedu/address/logic/drinkcommands/AddItemCommand.java
@@ -1,12 +1,14 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DEFAULT_SELLING_PRICE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DRINK_ITEM;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.Model;
+import seedu.address.model.DrinkModel;
+import seedu.address.model.drink.Drink;
 import seedu.address.model.user.admin.AdminModel;
 import seedu.address.model.user.admin.AdminModelManager;
 
@@ -14,7 +16,6 @@ import seedu.address.model.user.admin.AdminModelManager;
  * Adds a person to the address book.
  */
 public class AddItemCommand extends Command {
-
     public static final String COMMAND_WORD = "addItem";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a new item to the drink I/0. "
@@ -22,40 +23,41 @@ public class AddItemCommand extends Command {
             + PREFIX_DRINK_ITEM + "Drink item "
             + PREFIX_DEFAULT_SELLING_PRICE + "Default salling price \n"
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_DRINK_ITEM + "coca cola "
-            + PREFIX_DEFAULT_SELLING_PRICE + "3.12 ";
+            + PREFIX_DRINK_ITEM + "Coca Cola Original "
+            + PREFIX_DEFAULT_SELLING_PRICE + "20.00 ";
 
-    public static final String MESSAGE_SUCCESS = "New drinks added: %1$s with default price of %2$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This drink already exists in the drink I/O";
+    public static final String MESSAGE_SUCCESS = "New drink added: %1$s with default price of %2$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This drink already exists in the inventory list";
 
-    private final String drinkName;
-    private final String defaultSellingPrice;
+    // private final String drinkName;
+    // private final String defaultSellingPrice;
+
+    private final Drink toAdd;
 
     /**
-     * Creates an AddCommand to add the specified {@code Person}
+     * Creates an AddCommand to add the specified {@code Drink}
      */
-    public AddItemCommand(String drinkName, String defaultSellingPrice) {
-        requireAllNonNull(drinkName, defaultSellingPrice);
-        this.drinkName = drinkName;
-        this.defaultSellingPrice = defaultSellingPrice;
+    public AddItemCommand(Drink drink) {
+        requireAllNonNull(drink);
+        toAdd = drink;
     }
 
     @Override
-    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-        //        requireNonNull(model);
-        //
-        //        if (model.hasPerson(toAdd)) {
-        //            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
-        //        }
-        //
-        //        model.addPerson(toAdd);
-        //        model.commitAddressBook();
-        if (model instanceof AdminModel) {
-            System.out.println (((AdminModelManager) model).isValid ());
-        } else {
-            System.out.println ("not rights");
+    public CommandResult execute(DrinkModel model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasDrink(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
         }
-        return new CommandResult(String.format(MESSAGE_SUCCESS, drinkName, defaultSellingPrice));
+
+        model.addDrink(toAdd);
+
+        if (model instanceof AdminModel) {
+            System.out.println(((AdminModelManager) model).isValid());
+        } else {
+            System.out.println("not rights");
+        }
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd);
     }
 
     @Override
