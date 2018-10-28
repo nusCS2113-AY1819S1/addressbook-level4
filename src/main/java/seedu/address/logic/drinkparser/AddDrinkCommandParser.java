664908d7888ad52b4b8cdf722e1e15175d9ccd34
bluein-green <j.minjia@gmail.com>
2018-10-28 11:32:24 +0800

    move AddDrinkCommandParser to drinkparser

--- src/main/java/seedu/address/logic/drinkparser/AddDrinkCommandParser.java
+++ src/main/java/seedu/address/logic/drinkparser/AddDrinkCommandParser.java
@@ -6,26 +6,28 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_DRINK_ITEM;
 
 import java.util.stream.Stream;
 
+import seedu.address.logic.drinkcommands.AddDrinkCommand;
 import seedu.address.logic.drinkcommands.AddItemCommand;
+import seedu.address.logic.drinkparser.DrinkParser;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new AddCommand object
+ * Parses input arguments and creates a new AddDrinkCommand object
  */
-public class AddItemCommandParser implements Parser<AddItemCommand> {
+public class AddDrinkCommandParser implements DrinkParser<AddDrinkCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the AddCommand
-     * and returns an AddCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format
+     * Parses the given {@code String} of arguments in the context of the AddDrinkCommand
+     * and returns an AddDrinkCommand object for execution.
+     * @throws DrinkParseException if the user input does not conform the expected format
      */
-    public AddItemCommand parse(String args) throws ParseException {
+    public AddDrinkCommand parse(String args) throws DrinkParseException {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_DRINK_ITEM, PREFIX_DEFAULT_SELLING_PRICE);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_DRINK_ITEM, PREFIX_DEFAULT_SELLING_PRICE)
                 || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddItemCommand.MESSAGE_USAGE));
+            throw new DrinkParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddItemCommand.MESSAGE_USAGE));
         }
 
         String drinkName = ParserUtil.parseItemName(argMultimap.getValue(PREFIX_DRINK_ITEM).get());
