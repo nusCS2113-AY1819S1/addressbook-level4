f5752060f2e0bffbc455be4f70ead78a905b4d77
Unknown <geraldcdx5@gmail.com>
2018-10-28 19:51:09 +0800

    Changing sample data

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -69,7 +69,7 @@ public class EditCommandParser implements Parser
         if (argMultimap.getValue(PREFIX_DATETIME).isPresent()) {
             editEventDescriptor.setDate(ParserUtil.parseDateTime(argMultimap.getValue(PREFIX_DATETIME).get()));
         }
-        editEventDescriptor.setComment(new Comment("{span}This is a comment{/span}")); //TODO for Comment
+        editEventDescriptor.setComment(new Comment("{span}Comment Section{/span}{ol}{/ol}")); //TODO for Comment
         parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editEventDescriptor::setTags);
         parseAttendeesForEdit(argMultimap.getAllValues(PREFIX_ATTENDEE)).ifPresent(editEventDescriptor::setAttendees);
 
