eb3da4b86c84c6f1533fa73e572b35b71fc44c3d
bluein-green <j.minjia@gmail.com>
2018-10-28 18:05:31 +0800

    CliSyntax.java: added support for parseName, parse prices

--- src/main/java/seedu/address/logic/drinkparser/CliSyntax.java
+++ src/main/java/seedu/address/logic/drinkparser/CliSyntax.java
@@ -7,15 +7,17 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
+
 
     /* Common prefix*/
     public static final Prefix PREFIX_DRINK_NAME = new Prefix ("n/");
+
+
     /* Manger prefix */
-    public static final Prefix PREFIX_DEFAULT_SELLING_PRICE = new Prefix("p/");
+    public static final Prefix PREFIX_DRINK_DEFAULT_SELLING_PRICE = new Prefix("p/");
+    public static final Prefix PREFIX_DRINK_COST_PRICE = new Prefix ("cp/");
+    public static final Prefix PREFIX_DRINK_TAG = new Prefix ("t/");
+
     /* Stock taker prefix*/
     public static final Prefix PREFIX_DATE = new Prefix("d/");
     public static final Prefix PREFIX_QUANTITY = new Prefix("q/");
