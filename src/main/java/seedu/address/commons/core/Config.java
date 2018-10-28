f003591040e145466f22fe39027571519a13f01a
tsuweiquan <e0176844@u.nus.edu>
2018-10-28 22:04:52 +0800

    Revert "Update UI AppTitle to "Student Management System""
    This reverts commit ef4f468d5ef723223ff0b33759257053a061dcbf.

--- src/main/java/seedu/address/commons/core/Config.java
+++ src/main/java/seedu/address/commons/core/Config.java
@@ -13,7 +13,7 @@ public class Config {
     public static final Path DEFAULT_CONFIG_FILE = Paths.get("config.json");
 
     // Config values customizable through config file
-    private String appTitle = "Student Management System";
+    private String appTitle = "Address App";
     private Level logLevel = Level.INFO;
     private Path userPrefsFilePath = Paths.get("preferences.json");
 
