befafa9e86713906badf98514b7b069d59b3f54c
LowGinWee <notginwee@gmail.com>
2018-10-28 15:32:18 +0800

    Updated checkstyles

--- src/main/java/seedu/address/ui/SchedulePanel.java
+++ src/main/java/seedu/address/ui/SchedulePanel.java
@@ -11,7 +11,6 @@ import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
