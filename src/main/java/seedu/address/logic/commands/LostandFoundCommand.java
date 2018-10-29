d18341d74187e40a04f98480ed949af2e0a80b29
Haowei <e0253738@u.nus.edu>
2018-10-29 19:29:00 +0800

    Update src/main/java/seedu/address/logic/commands/LostandFoundCommand.java

--- src/main/java/seedu/address/logic/commands/LostandFoundCommand.java
+++ src/main/java/seedu/address/logic/commands/LostandFoundCommand.java
@@ -53,9 +53,14 @@ public class LostandFoundCommand extends Command {
         int counter = 0;
         for (SimpleItem simpleItem : lostItems) {
             counter++;
-            messageOutput += counter + ". " +
-                    simpleItem.getName() +
-                    ": " + simpleItem.getQuantity().toString() + "\n" + "      ";
+            messageOutput += counter + ". "
+                    +
+                    simpleItem.getName()
+                    +
+                    ": "
+                    + simpleItem.getQuantity().toString()
+                    + "\n"
+                    + "      ";
             if (counter != lostItems.size()) {
                 messageOutput += ", ";
             }
