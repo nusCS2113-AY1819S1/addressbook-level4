8e9bc79c6481da44bad474ff3793a767d4660061
kianmeng <kianmeng1996@gmail.com>
2018-10-30 00:41:31 +0800

    fixed findCandidateCommand, currently able to search for different fields (but only 1 at a time)

--- src/main/java/seedu/recruit/model/candidate/CandidateContainsKeywordsPredicate.java
+++ src/main/java/seedu/recruit/model/candidate/CandidateContainsKeywordsPredicate.java
@@ -18,26 +18,24 @@ public class CandidateContainsKeywordsPredicate implements Predicate<Candidate> 
 
     @Override
     public boolean test(Candidate candidate) {
-        return (keywords.get("Name").stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(
-                candidate.getName().fullName, keyword)))
-                || (keywords.get("Gender").stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(
-                        candidate.getGender().value, keyword)))
-                || (keywords.get("Age").stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(
-                        candidate.getAge().value, keyword)))
-                || (keywords.get("Name").stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(
-                        candidate.getName().fullName, keyword)))
-                || (keywords.get("Phone").stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(
-                        candidate.getPhone().value, keyword)))
-                || (keywords.get("Email").stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(
-                        candidate.getEmail().value, keyword)))
-                || (keywords.get("Address").stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(
-                        candidate.getAddress().value, keyword)))
-                || (keywords.get("Job").stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(
-                        candidate.getJob().value, keyword)))
-                || (keywords.get("Education").stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(
-                        candidate.getEducation().value, keyword)))
-                || (keywords.get("Salary").stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(
-                        candidate.getSalary().value, keyword)));
+        return ((keywords.containsKey("Name")) && (keywords.get("Name").stream().anyMatch(keyword ->
+                    StringUtil.containsWordIgnoreCase(candidate.getName().fullName, keyword))))
+                || ((keywords.containsKey("Gender")) && (keywords.get("Gender").stream().anyMatch(keyword ->
+                    StringUtil.containsWordIgnoreCase(candidate.getGender().value, keyword))))
+                || ((keywords.containsKey("Age")) && (keywords.get("Age").stream().anyMatch(keyword ->
+                    StringUtil.containsWordIgnoreCase(candidate.getAge().value, keyword))))
+                || ((keywords.containsKey("Phone")) && (keywords.get("Phone").stream().anyMatch(keyword ->
+                    StringUtil.containsWordIgnoreCase(candidate.getPhone().value, keyword))))
+                || ((keywords.containsKey("Email")) && (keywords.get("Email").stream().anyMatch(keyword ->
+                    StringUtil.containsWordIgnoreCase(candidate.getEmail().value, keyword))))
+                || ((keywords.containsKey("Address")) && (keywords.get("Address").stream().anyMatch(keyword ->
+                    StringUtil.containsWordIgnoreCase(candidate.getAddress().value, keyword))))
+                || ((keywords.containsKey("Job")) && (keywords.get("Job").stream().anyMatch(keyword ->
+                    StringUtil.containsWordIgnoreCase(candidate.getJob().value, keyword))))
+                || ((keywords.containsKey("Education")) && (keywords.get("Education").stream().anyMatch(keyword ->
+                    StringUtil.containsWordIgnoreCase(candidate.getEducation().value, keyword))))
+                || ((keywords.containsKey("Salary")) && (keywords.get("Salary").stream().anyMatch(keyword ->
+                    StringUtil.containsWordIgnoreCase(candidate.getSalary().value, keyword))));
     }
 
     @Override
