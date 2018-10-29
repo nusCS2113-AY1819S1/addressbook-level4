79a37ccd44408d8ceeb8a512411b308350328ad3
harriuscai <harriuscai@gmail.com>
2018-10-29 14:44:25 +0800

    Module enrol now supports multiple matric no. to enrol many students at once

--- src/main/java/seedu/address/logic/commands/ModuleEnrolCommand.java
+++ src/main/java/seedu/address/logic/commands/ModuleEnrolCommand.java
@@ -4,12 +4,15 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MATRIC;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE_CODE;
 
+import java.util.Set;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.module.Module;
 import seedu.address.model.module.ModuleCode;
 import seedu.address.model.module.ModuleManager;
+import seedu.address.model.person.MatricNo;
 import seedu.address.model.person.Person;
 import seedu.address.model.student.StudentManager;
 
@@ -29,17 +32,19 @@ public class ModuleEnrolCommand extends Command {
             + PREFIX_MODULE_CODE + "CS2113 "
             + PREFIX_MATRIC + "A0161234B";
 
-    public static final String MESSAGE_MODULE_NOT_FOUND = "Module with code %s doesn't exist in Trajectory!";
-    public static final String MESSAGE_STUDENT_NOT_FOUND = "Student with matric no. %s doesn't exist in Trajectory!";
-    public static final String MESSAGE_ALREADY_ENROLLED = "%1$s (%2$s) is already enrolled in %3$s %4$s.";
-    public static final String MESSAGE_ENROL_SUCCESS = "Enrolled %1$s (%2$s) in %3$s %4$s.";
+    private static final String MESSAGE_MODULE_NOT_FOUND = "Module with code %s doesn't exist in Trajectory!";
+    private static final String MESSAGE_STUDENT_NOT_FOUND = "Student with matric no. %s doesn't exist in Trajectory!";
+    private static final String MESSAGE_ALREADY_ENROLLED = "%1$s (%2$s) is already enrolled in %3$s %4$s.";
+    private static final String MESSAGE_ENROL_SUCCESS = "Enrolled %1$s (%2$s) in %3$s %4$s.";
+    private static final String MESSAGE_COMMAND_SUMMARY =
+            "%1$s student%2$s enrolled, %3$s student%4$s already enrolled, %5$s invalid matric number%6$s.";
 
     private final ModuleCode moduleCode;
-    private final String studentMatricNo;
+    private final Set<MatricNo> matricNoSet;
 
-    public ModuleEnrolCommand(ModuleCode moduleCode, String studentMatricNo) {
+    public ModuleEnrolCommand(ModuleCode moduleCode, Set<MatricNo> matricNoSet) {
         this.moduleCode = moduleCode;
-        this.studentMatricNo = studentMatricNo;
+        this.matricNoSet = matricNoSet;
     }
 
     @Override
@@ -47,32 +52,57 @@ public class ModuleEnrolCommand extends Command {
         requireNonNull(model);
 
         ModuleManager moduleManager = ModuleManager.getInstance();
+        StudentManager studentManager = StudentManager.getInstance();
 
         if (!moduleManager.doesModuleExist(moduleCode.moduleCode)) {
             throw new CommandException(String.format(MESSAGE_MODULE_NOT_FOUND, moduleCode.moduleCode));
         }
-        if (!StudentManager.getInstance().doesStudentExistForGivenMatricNo(studentMatricNo)) {
-            throw new CommandException(String.format(MESSAGE_STUDENT_NOT_FOUND, studentMatricNo));
+
+        int numEnrolled = 0;
+        int numDuplicates = 0;
+        int numInvalid = 0;
+        StringBuilder sb = new StringBuilder();
+        for (MatricNo matricNo : matricNoSet) {
+            if (studentManager.doesStudentExistForGivenMatricNo(matricNo.matricNo)) {
+                Module moduleToEnrol = moduleManager.getModuleByModuleCode(moduleCode.moduleCode);
+                Person studentToEnrol = studentManager.retrieveStudentByMatricNo(matricNo.matricNo);
+
+                if (moduleManager.isStudentEnrolledInModule(moduleToEnrol, studentToEnrol)) {
+                    sb.append(String.format(MESSAGE_ALREADY_ENROLLED,
+                            studentToEnrol.getName().fullName,
+                            studentToEnrol.getMatricNo().matricNo,
+                            moduleToEnrol.getModuleCode().moduleCode,
+                            moduleToEnrol.getModuleName().moduleName));
+
+                    numDuplicates++;
+                } else {
+                    moduleManager.enrolStudentInModule(moduleToEnrol, studentToEnrol);
+
+                    sb.append(String.format(MESSAGE_ENROL_SUCCESS,
+                            studentToEnrol.getName().fullName,
+                            studentToEnrol.getMatricNo(),
+                            moduleToEnrol.getModuleCode().moduleCode,
+                            moduleToEnrol.getModuleName().moduleName));
+
+                    numEnrolled++;
+                }
+            } else {
+                sb.append(String.format(MESSAGE_STUDENT_NOT_FOUND, matricNo));
+                numInvalid++;
+            }
+            sb.append("\n");
         }
 
-        Module moduleToEnrol = moduleManager.getModuleByModuleCode(moduleCode.moduleCode);
-        Person studentToEnrol = StudentManager.getInstance().retrieveStudentByMatricNo(studentMatricNo);
-
-        if (moduleManager.isStudentEnrolledInModule(moduleToEnrol, studentToEnrol)) {
-            throw new CommandException(String.format(MESSAGE_ALREADY_ENROLLED,
-                    studentToEnrol.getName().fullName,
-                    studentToEnrol.getMatricNo().matricNo,
-                    moduleToEnrol.getModuleCode().moduleCode,
-                    moduleToEnrol.getModuleName().moduleName));
-        }
-
-        moduleManager.enrolStudentInModule(moduleToEnrol, studentToEnrol);
         moduleManager.saveModuleList();
+        sb.append(String.format(MESSAGE_COMMAND_SUMMARY,
+                numEnrolled, determinePluralOrSingular(numEnrolled),
+                numDuplicates, determinePluralOrSingular(numDuplicates),
+                numInvalid, determinePluralOrSingular(numInvalid)));
 
-        return new CommandResult(String.format(MESSAGE_ENROL_SUCCESS,
-                studentToEnrol.getName().fullName,
-                studentToEnrol.getMatricNo(),
-                moduleToEnrol.getModuleCode().moduleCode,
-                moduleToEnrol.getModuleName().moduleName));
+        return new CommandResult(sb.toString());
+    }
+
+    private char determinePluralOrSingular(int number) {
+        return number == 0 || number > 1 ? 's' : Character.MIN_VALUE;
     }
 }
