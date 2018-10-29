65692ad25d67c35dfe7b0f5ef52301f987011c29
Haowei <e0253738@u.nus.edu>
2018-10-29 21:51:01 +0800

    fixing

--- src/main/java/seedu/address/model/item/Item.java
+++ src/main/java/seedu/address/model/item/Item.java
@@ -10,7 +10,7 @@ import java.util.Set;
 import seedu.address.model.tag.Tag;
 
 /**
- * Represents a Item in the address book.
+ * Represents a Item in the Stock List.
  * Guarantees: details are present and not null, field values are validated, immutable.
  */
 public class Item {
@@ -121,7 +121,7 @@ public class Item {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, quantity, minQuantity, status, tags);
+        return Objects.hash(name, quantity, minQuantity, status, loststatus, tags);
     }
 
     @Override
