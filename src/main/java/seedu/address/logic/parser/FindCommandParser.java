6c27b1893133f5b1dd326abb3df89a753fc68044
Tertium3 <longdung9900@gmail.com>
2018-10-29 17:19:55 +0800

    Incomplete work

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,8 +1,17 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATETIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_KEYWORD;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -20,14 +29,48 @@ public class FindCommandParser implements Parser<FindCommand> {
      */
     public FindCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
+
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(trimmedArgs, PREFIX_KEYWORD, PREFIX_NAME,
+                PREFIX_CONTACT, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_DATETIME, PREFIX_TAG);
+
+        if (trimmedArgs.isEmpty() || !anyPrefixesPresent(argMultimap, PREFIX_KEYWORD, PREFIX_NAME, PREFIX_CONTACT,
+                PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_DATETIME, PREFIX_TAG)) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        String keywords = argMultimap.getValue(PREFIX_KEYWORD).get().trim();
+        String nameKeywords = argMultimap.getValue(PREFIX_NAME).get().trim();
+        String contactKeywords = argMultimap.getValue(PREFIX_CONTACT).get().trim();
+        String emailKeywords = argMultimap.getValue(PREFIX_EMAIL).get().trim();
+        String phoneKeywords = argMultimap.getValue(PREFIX_PHONE).get().trim();
+        String addressKeywords = argMultimap.getValue(PREFIX_ADDRESS).get().trim();
+        String datetimeKeywords = argMultimap.getValue(PREFIX_DATETIME).get().trim();
+        String tagKeywords = argMultimap.getValue(PREFIX_TAG).get();
 
-        return new FindCommand(new EventContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        String[] keywordsArray = keywords.split("\\s+");
+        String[] nameKeywordsArray = nameKeywords.split("\\s+");
+        String[] contactKeywordsArray = contactKeywords.split("\\s+");
+        String[] emailKeywordsArray = emailKeywords.split("\\s+");
+        String[] phoneKeywordsArray = phoneKeywords.split("\\s+");
+        String[] addressKeywordsArray = addressKeywords.split("\\s+");
+        String[] datetimeKeywordsArray = datetimeKeywords.split("\\s+");
+        String[] tagKeywordsArray = tagKeywords.split("\\s+");
+
+
+        //TODO: Deal with optional value
+        return new FindCommand(
+                new EventContainsKeywordsPredicate(Arrays.asList(keywordsArray), Arrays.asList(nameKeywordsArray),
+                        Arrays.asList(contactKeywordsArray), Arrays.asList(emailKeywordsArray),
+                        Arrays.asList(phoneKeywordsArray), Arrays.asList(addressKeywordsArray),
+                        Arrays.asList(datetimeKeywordsArray), Arrays.asList(tagKeywordsArray)));
     }
 
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean anyPrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
 }
--- src/main/java/seedu/address/model/event/EventContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/event/EventContainsKeywordsPredicate.java
@@ -11,21 +11,47 @@ import seedu.address.commons.util.StringUtil;
  */
 public class EventContainsKeywordsPredicate implements Predicate<Event> {
     private final List<String> keywords;
+    private final List<String> nameKeywords;
+    private final List<String> contactKeywords;
+    private final List<String> emailKeywords;
+    private final List<String> phoneKeywords;
+    private final List<String> addresKeywords;
+    private final List<String> datetimeKeywords;
+    private final List<String> tagKeywords;
 
-    public EventContainsKeywordsPredicate(List<String> keywords) {
+    public EventContainsKeywordsPredicate(List<String> keywords,
+                                          List<String> nameKeywords,
+                                          List<String> contactKeywords,
+                                          List<String> emailKeywords,
+                                          List<String> phoneKeywords,
+                                          List<String> addressKeywords,
+                                          List<String> datetimeKeywords,
+                                          List<String> tagKeywords) {
         this.keywords = keywords;
+        this.nameKeywords = nameKeywords;
+        this.contactKeywords = contactKeywords;
+        this.emailKeywords = emailKeywords;
+        this.phoneKeywords = phoneKeywords;
+        this.addresKeywords = addressKeywords;
+        this.datetimeKeywords = datetimeKeywords;
+        this.tagKeywords = tagKeywords;
     }
 
     //TODO: update when contact is added
     @Override
     public boolean test(Event event) {
         return keywords.stream()
-                .anyMatch(keyword ->
-                        StringUtil.containsWordIgnoreCase(event.getName().fullName, keyword)
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(event.getName().fullName, keyword)
+                        || StringUtil.containsWordIgnoreCase(event.getContact().fullContactName, keyword)
+                        || StringUtil.containsWordIgnoreCase(event.getEmail().value, keyword)
+                        || StringUtil.containsWordIgnoreCase(event.getPhone().value, keyword)
                         || StringUtil.containsWordIgnoreCase(event.getAddress().value, keyword)
                         || StringUtil.containsWordIgnoreCase(event.getDateTime().toString(), keyword)
                         || event.getTags().stream()
-                                .anyMatch(tag -> StringUtil.containsWordIgnoreCase(tag.tagName, keyword)));
+                                .anyMatch(tag -> StringUtil.containsWordIgnoreCase(tag.tagName, keyword)))
+                && nameKeywords.stream()
+                .anyMatch(nameKeyword -> StringUtil.containsWordIgnoreCase(event.getName().fullName, nameKeyword))
+                ;
     }
 
     @Override
@@ -34,5 +60,4 @@ public class EventContainsKeywordsPredicate implements Predicate<Event> {
                 || (other instanceof EventContainsKeywordsPredicate // instanceof handles nulls
                 && keywords.equals(((EventContainsKeywordsPredicate) other).keywords)); // state check
     }
-
 }
