1baedf1c990e371a53411188f18acde8a956d020
kennethcsj <kennethcsj@gmail.com>
2018-10-30 22:28:40 +0800

    solve deletecommand test issues + change to stats

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -6,8 +6,11 @@ import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalArgTypes.ARGS_INDEX;
 import static seedu.address.testutil.TypicalBooks.getTypicalBookInventory;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_BOOK;
+import static seedu.address.testutil.TypicalIndexes.INDEX_ONE_BASED_FIRST_BOOK;
+import static seedu.address.testutil.TypicalIndexes.INDEX_ONE_BASED_SECOND_BOOK;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_BOOK;
 
 import org.junit.Test;
@@ -32,7 +35,7 @@ public class DeleteCommandTest {
     @Test
     public void execute_validIndexUnfilteredList_success() {
         Book bookToDelete = model.getFilteredBookList().get(INDEX_FIRST_BOOK.getZeroBased());
-        DeleteCommand deleteCommand = new DeleteCommand(INDEX_FIRST_BOOK);
+        DeleteCommand deleteCommand = new DeleteCommand(INDEX_ONE_BASED_FIRST_BOOK, ARGS_INDEX);
 
         String expectedMessage = String.format(DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS, bookToDelete);
 
@@ -46,7 +49,7 @@ public class DeleteCommandTest {
     @Test
     public void execute_invalidIndexUnfilteredList_throwsCommandException() {
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredBookList().size() + 1);
-        DeleteCommand deleteCommand = new DeleteCommand(outOfBoundIndex);
+        DeleteCommand deleteCommand = new DeleteCommand(Integer.toString(outOfBoundIndex.getOneBased()) , ARGS_INDEX);
 
         assertCommandFailure(deleteCommand, model, commandHistory, Messages.MESSAGE_INVALID_BOOK_DISPLAYED_INDEX);
     }
@@ -56,7 +59,7 @@ public class DeleteCommandTest {
         showPersonAtIndex(model, INDEX_FIRST_BOOK);
 
         Book bookToDelete = model.getFilteredBookList().get(INDEX_FIRST_BOOK.getZeroBased());
-        DeleteCommand deleteCommand = new DeleteCommand(INDEX_FIRST_BOOK);
+        DeleteCommand deleteCommand = new DeleteCommand(INDEX_ONE_BASED_FIRST_BOOK, ARGS_INDEX);
 
         String expectedMessage = String.format(DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS, bookToDelete);
 
@@ -76,7 +79,7 @@ public class DeleteCommandTest {
         // ensures that outOfBoundIndex is still in bounds of BookInventory list
         assertTrue(outOfBoundIndex.getZeroBased() < model.getBookInventory().getBookList().size());
 
-        DeleteCommand deleteCommand = new DeleteCommand(outOfBoundIndex);
+        DeleteCommand deleteCommand = new DeleteCommand(Integer.toString(outOfBoundIndex.getOneBased()) , ARGS_INDEX);
 
         assertCommandFailure(deleteCommand, model, commandHistory, Messages.MESSAGE_INVALID_BOOK_DISPLAYED_INDEX);
     }
@@ -84,7 +87,7 @@ public class DeleteCommandTest {
     @Test
     public void executeUndoRedo_validIndexUnfilteredList_success() throws Exception {
         Book bookToDelete = model.getFilteredBookList().get(INDEX_FIRST_BOOK.getZeroBased());
-        DeleteCommand deleteCommand = new DeleteCommand(INDEX_FIRST_BOOK);
+        DeleteCommand deleteCommand = new DeleteCommand(INDEX_ONE_BASED_FIRST_BOOK, ARGS_INDEX);
         Model expectedModel = new ModelManager(model.getBookInventory(), new UserPrefs());
         expectedModel.deleteBook(bookToDelete);
         expectedModel.commitBookInventory();
@@ -104,7 +107,7 @@ public class DeleteCommandTest {
     @Test
     public void executeUndoRedo_invalidIndexUnfilteredList_failure() {
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredBookList().size() + 1);
-        DeleteCommand deleteCommand = new DeleteCommand(outOfBoundIndex);
+        DeleteCommand deleteCommand = new DeleteCommand(Integer.toString(outOfBoundIndex.getOneBased()) , ARGS_INDEX);
 
         // execution failed -> BookInventory state not added into model
         assertCommandFailure(deleteCommand, model, commandHistory, Messages.MESSAGE_INVALID_BOOK_DISPLAYED_INDEX);
@@ -123,7 +126,7 @@ public class DeleteCommandTest {
      */
     @Test
     public void executeUndoRedo_validIndexFilteredList_samePersonDeleted() throws Exception {
-        DeleteCommand deleteCommand = new DeleteCommand(INDEX_FIRST_BOOK);
+        DeleteCommand deleteCommand = new DeleteCommand(INDEX_ONE_BASED_FIRST_BOOK, ARGS_INDEX);
         Model expectedModel = new ModelManager(model.getBookInventory(), new UserPrefs());
 
         showPersonAtIndex(model, INDEX_SECOND_BOOK);
@@ -134,7 +137,7 @@ public class DeleteCommandTest {
         // delete -> deletes second book in unfiltered book list / first book in filtered book list
         deleteCommand.execute(model, commandHistory);
 
-        // undo -> reverts addressbook back to previous state and filtered book list to show all persons
+        // undo -> reverts inventory book back to previous state and filtered book list to show all persons
         expectedModel.undoAddressBook();
         assertCommandSuccess(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_SUCCESS, expectedModel);
 
@@ -146,14 +149,14 @@ public class DeleteCommandTest {
 
     @Test
     public void equals() {
-        DeleteCommand deleteFirstCommand = new DeleteCommand(INDEX_FIRST_BOOK);
-        DeleteCommand deleteSecondCommand = new DeleteCommand(INDEX_SECOND_BOOK);
+        DeleteCommand deleteFirstCommand = new DeleteCommand(INDEX_ONE_BASED_FIRST_BOOK, ARGS_INDEX);
+        DeleteCommand deleteSecondCommand = new DeleteCommand(INDEX_ONE_BASED_SECOND_BOOK, ARGS_INDEX);
 
         // same object -> returns true
         assertTrue(deleteFirstCommand.equals(deleteFirstCommand));
 
         // same values -> returns true
-        DeleteCommand deleteFirstCommandCopy = new DeleteCommand(INDEX_FIRST_BOOK);
+        DeleteCommand deleteFirstCommandCopy = new DeleteCommand(INDEX_ONE_BASED_FIRST_BOOK, ARGS_INDEX);
         assertTrue(deleteFirstCommand.equals(deleteFirstCommandCopy));
 
         // different types -> returns false
---
+++ src/test/java/seedu/address/testutil/TypicalArgTypes.java
@@ -0,0 +1,9 @@
+package seedu.address.testutil;
+
+/**
+ * A utility class containing a list of {@code String} objects to be used in tests.
+ */
+public class TypicalArgTypes {
+    public static final String ARGS_ISBN = "Isbn";
+    public static final String ARGS_INDEX = "Index";
+}
