92936bdce03b889c5bd89ee817d2098cbe69cfdb
arty9 <tayrantian@gmail.com>
2018-10-30 10:52:46 +0800

    extra conditions for TaskDate

--- src/main/java/seedu/address/model/task/TaskDate.java
+++ src/main/java/seedu/address/model/task/TaskDate.java
@@ -9,8 +9,14 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class TaskDate {
 
+    private static final int DAY_MAX_LIMIT = 31;
+    private static final int DAY_MIN_LIMIT = 1;
+    private static final int MONTH_MAX_LIMIT = 12;
+    private static final int MONTH_MIN_LIMIT = 1;
+
     public static final String MESSAGE_DATE_CONSTRAINTS =
-            "Dates should be of the format DD-MM, where DD and MM are numbers";
+            "Dates should be of the format DD-MM, where DD and MM are numbers.\n" +
+            "DD must be in the range of 01 to 31, and MM must be in the range of 01 to 12.";
     public static final String DATE_VALIDATION_REGEX = "[\\d]{2}" + "-" + "[\\d]{2}";
 
     public final String value;
@@ -30,7 +36,19 @@ public class TaskDate {
      * Returns if a given string is a valid date.
      */
     public static boolean isValidDate(String test) {
-        return test.matches(DATE_VALIDATION_REGEX);
+        if (test.matches(DATE_VALIDATION_REGEX)) {
+            String[] numbers = test.split("-");
+            int day = Integer.parseInt(numbers[0]);
+            int month = Integer.parseInt(numbers[1]);
+            if (day > DAY_MAX_LIMIT || day < DAY_MIN_LIMIT) {
+                return false;
+            } else if (month > MONTH_MAX_LIMIT || month < MONTH_MIN_LIMIT) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
