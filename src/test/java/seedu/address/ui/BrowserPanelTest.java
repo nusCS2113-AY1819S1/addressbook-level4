984884919247061ca14a25b72f0a138e0ebc436c
driedmelon <sourmilo@gmail.com>
2018-10-31 00:48:00 +0800

    Halting browserpanel GUI test

--- src/test/java/seedu/address/ui/BrowserPanelTest.java
+++ src/test/java/seedu/address/ui/BrowserPanelTest.java
@@ -40,7 +40,8 @@
 //
 //        // associated web page of a person
 //        postNow(selectionChangedEventStub);
-//        URL expectedPersonUrl = new URL(BrowserPanel.SEARCH_PAGE_URL + ALICE.getName().fullName.replaceAll(" ", "%20"));
+//        URL expectedPersonUrl = new URL(BrowserPanel.SEARCH_PAGE_URL
+//              + ALICE.getName().fullName.replaceAll(" ", "%20"));
 //
 //        waitUntilBrowserLoaded(browserPanelHandle);
 //        assertEquals(expectedPersonUrl, browserPanelHandle.getLoadedUrl());
