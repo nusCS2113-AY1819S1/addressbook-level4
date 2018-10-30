23baeef8fc5bba87a29d8d9f3d0485cc4aa67c12
chiaxr <chia.xiangrong@gmail.com>
2018-10-31 00:54:12 +0800

    Removed unused import

--- src/main/java/seedu/address/logic/commands/RegisterCommand.java
+++ src/main/java/seedu/address/logic/commands/RegisterCommand.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 
 import static seedu.address.logic.commands.EditCommand.EditEventDescriptor;
 import static seedu.address.logic.commands.EditCommand.createEditedEvent;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EVENTS;
 
 import java.util.HashSet;
 import java.util.List;
