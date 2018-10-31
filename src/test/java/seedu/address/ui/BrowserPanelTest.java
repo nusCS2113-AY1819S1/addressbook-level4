4c15a897ca014f07f410708d202c8540fadf3415
clara1234566 <e0191559@u.nus.edu>
2018-10-31 16:04:35 +0800

    try solve trives

--- src/test/java/seedu/address/ui/BrowserPanelTest.java
+++ src/test/java/seedu/address/ui/BrowserPanelTest.java
@@ -1,63 +1,63 @@
-//package seedu.address.ui;
-//
-//import static guitests.guihandles.WebViewUtil.waitUntilBrowserLoaded;
-//import static org.junit.Assert.assertEquals;
-//import static seedu.address.testutil.EventsUtil.postNow;
-//import static seedu.address.testutil.TypicalGroups.TUT_1;
-//import static seedu.address.testutil.TypicalPersons.ALICE;
-//import static seedu.address.ui.BrowserPanel.DEFAULT_PAGE;
-//import static seedu.address.ui.BrowserPanel.STUDENT_GRADE_PAGE_URL;
-//import static seedu.address.ui.UiPart.FXML_FILE_FOLDER;
-//
-//import java.net.URL;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import guitests.guihandles.BrowserPanelHandle;
-//import seedu.address.MainApp;
-//import seedu.address.commons.events.ui.GroupPanelSelectionChangedEvent;
-//import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
-//
-//public class BrowserPanelTest extends GuiUnitTest {
-//    private PersonPanelSelectionChangedEvent selectionChangedEventStub;
-//    private GroupPanelSelectionChangedEvent selectionChangedEventGroupStub;
-//
-//    private BrowserPanel browserPanel;
-//    private BrowserPanelHandle browserPanelHandle;
-//
-//    @Before
-//    public void setUp() {
-//        selectionChangedEventStub = new PersonPanelSelectionChangedEvent(ALICE);
-//        selectionChangedEventGroupStub = new GroupPanelSelectionChangedEvent(TUT_1);
-//
-//        guiRobot.interact(() -> browserPanel = new BrowserPanel());
-//        uiPartRule.setUiPart(browserPanel);
-//
-//        browserPanelHandle = new BrowserPanelHandle(browserPanel.getRoot());
-//    }
-//
-//    @Test
-//    public void display() throws Exception {
-//        // default web page
-//        URL expectedDefaultPageUrl = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
-//        assertEquals(expectedDefaultPageUrl, browserPanelHandle.getLoadedUrl());
-//        ///C:/Users/clara/Desktop/main/out/production/resources/view/default.html
-//        // associated web page of a person
-//        postNow(selectionChangedEventStub);
-//
-//
-//        URL expectedPersonUrl = new URL(MainApp.class.getResource(STUDENT_GRADE_PAGE_URL).toString() + "?data=[");
-//
-//        waitUntilBrowserLoaded(browserPanelHandle);
-//        assertEquals(expectedPersonUrl, browserPanelHandle.getLoadedUrl());
-//
-//        // associated web page of a group
-//        postNow(selectionChangedEventGroupStub);
-//        URL expectedGroupUrl = new URL(BrowserPanel.SEARCH_PAGE_URL
-//                + TUT_1.getGroupName().groupName.replaceAll(" ", "%20"));
-//
-//        waitUntilBrowserLoaded(browserPanelHandle);
-//        assertEquals(expectedGroupUrl, browserPanelHandle.getLoadedUrl());
-//    }
-//}
+package seedu.address.ui;
+
+import static guitests.guihandles.WebViewUtil.waitUntilBrowserLoaded;
+import static org.junit.Assert.assertEquals;
+import static seedu.address.testutil.EventsUtil.postNow;
+import static seedu.address.testutil.TypicalGroups.TUT_1;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.ui.BrowserPanel.DEFAULT_PAGE;
+import static seedu.address.ui.BrowserPanel.STUDENT_GRADE_PAGE_URL;
+import static seedu.address.ui.UiPart.FXML_FILE_FOLDER;
+
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import guitests.guihandles.BrowserPanelHandle;
+import seedu.address.MainApp;
+import seedu.address.commons.events.ui.GroupPanelSelectionChangedEvent;
+import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
+
+public class BrowserPanelTest extends GuiUnitTest {
+    private PersonPanelSelectionChangedEvent selectionChangedEventStub;
+    private GroupPanelSelectionChangedEvent selectionChangedEventGroupStub;
+
+    private BrowserPanel browserPanel;
+    private BrowserPanelHandle browserPanelHandle;
+
+    @Before
+    public void setUp() {
+        selectionChangedEventStub = new PersonPanelSelectionChangedEvent(ALICE);
+        selectionChangedEventGroupStub = new GroupPanelSelectionChangedEvent(TUT_1);
+
+        guiRobot.interact(() -> browserPanel = new BrowserPanel());
+        uiPartRule.setUiPart(browserPanel);
+
+        browserPanelHandle = new BrowserPanelHandle(browserPanel.getRoot());
+    }
+
+    @Test
+    public void display() throws Exception {
+        // default web page
+        URL expectedDefaultPageUrl = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
+        assertEquals(expectedDefaultPageUrl, browserPanelHandle.getLoadedUrl());
+        ///C:/Users/clara/Desktop/main/out/production/resources/view/default.html
+        // associated web page of a person
+        postNow(selectionChangedEventStub);
+
+
+        URL expectedPersonUrl = new URL(MainApp.class.getResource(STUDENT_GRADE_PAGE_URL).toString() + "?data=[");
+
+        waitUntilBrowserLoaded(browserPanelHandle);
+        assertEquals(expectedPersonUrl, browserPanelHandle.getLoadedUrl());
+
+        // associated web page of a group
+        postNow(selectionChangedEventGroupStub);
+        URL expectedGroupUrl = new URL(BrowserPanel.SEARCH_PAGE_URL
+                + TUT_1.getGroupName().groupName.replaceAll(" ", "%20"));
+
+        waitUntilBrowserLoaded(browserPanelHandle);
+        assertEquals(expectedGroupUrl, browserPanelHandle.getLoadedUrl());
+    }
+}
