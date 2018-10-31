9ee5ac62cdd6554e6555fc2360e8eafabec06c0b
jieliangang <jieliangang@gmail.com>
2018-10-31 23:30:47 +0800

    Update Sample Data for PE1 preparation

--- src/main/java/seedu/address/model/util/SampleEventUtil.java
+++ src/main/java/seedu/address/model/util/SampleEventUtil.java
@@ -1,11 +1,13 @@
 package seedu.address.model.util;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import seedu.address.model.EventList;
 import seedu.address.model.ReadOnlyEventList;
+import seedu.address.model.event.Attendees;
 import seedu.address.model.event.Description;
 import seedu.address.model.event.EndTime;
 import seedu.address.model.event.Event;
@@ -22,15 +24,52 @@ import seedu.address.model.tag.Tag;
 public class SampleEventUtil {
     public static Event[] getSampleEvents() {
 
+        Set<String> attendeesOne = new HashSet<>();
+        Set<String> attendeesTwo = new HashSet<>();
+        Set<String> attendeesThree = new HashSet<>();
+        Set<String> attendeesFour = new HashSet<>();
+        attendeesOne.add("Alex Yeoh");
+        attendeesOne.add("Bernice Yu");
+        attendeesOne.add("Charlotte Oliveiro");
+        attendeesTwo.add("Alex Yeoh");
+        attendeesTwo.add("David Li");
+        attendeesTwo.add("Irfan Ibrahim");
+        attendeesTwo.add("Roy Balakrishnan");
+        attendeesTwo.add("Bernice Yu");
+        attendeesThree.add("Alex Yeoh");
+        attendeesThree.add("Roy Balakrishnan");
+        attendeesThree.add("Bernice Yu");
+        attendeesFour.add("Charlotte Oliveiro");
+        attendeesFour.add("David Li");
+        attendeesFour.add("Irfan Ibrahim");
+        attendeesFour.add("Alex Yeoh");
+        attendeesFour.add("Roy Balakrishnan");
+
         return new Event[]{
-            new Event(new EventName("CS2113 Lecture"), new Description("Very Hard"), new EventDate("2018-09-28"),
-                      new StartTime("16:00"), new EndTime("18:00"), new Location("LT 100")),
-            new Event(new EventName("CS2113 Tutorial"), new Description("Can i skip this?"),
-                    new EventDate("2018-10-28"), new StartTime("16:00"),
-                    new EndTime("17:00"), new Location("Tut room 1000")),
-            new Event(new EventName("CS2113 Java session"), new Description("Java Camp help!"),
-                    new EventDate("2018-09-28"), new StartTime("16:00"),
-                    new EndTime("23:00"), new Location("Utown")),
+            new Event(new EventName("Monthly Team Meeting"), new Description("Review monthly progress"),
+                    new EventDate("2018-11-06"), new StartTime("10:00"), new EndTime("12:00"),
+                    new Location("Meeting Room 5"), new Attendees(attendeesOne)),
+            new Event(new EventName("Dinner with ABC Client"), new Description("Attire: Smart Casual. No Sandles"),
+                    new EventDate("2018-11-14"), new StartTime("18:00"),
+                    new EndTime("20:00"), new Location("Marina Bay Sands"), new Attendees(attendeesThree)),
+            new Event(new EventName("Contract Negotiation Meeting"), new Description("For CX-2143 Project"),
+                    new EventDate("2018-11-07"), new StartTime("14:00"),
+                    new EndTime("16:00"), new Location("Seminar Room 3"), new Attendees(attendeesThree)),
+            new Event(new EventName("Tender Discussion with Supplier"), new Description("Get ready tender draft"),
+                    new EventDate("2018-12-15"), new StartTime("16:00"),
+                    new EndTime("18:00"), new Location("Jurong East"), new Attendees(attendeesTwo)),
+            new Event(new EventName("Farewell Party for Roy"), new Description("Goodbye Roy!!! Have fun in US"),
+                    new EventDate("2019-01-20"), new StartTime("18:00"),
+                    new EndTime("23:00"), new Location("Bugis+"), new Attendees(attendeesFour)),
+            new Event(new EventName("Team Annual Outing"), new Description("Do come if you are free."),
+                    new EventDate("2018-12-24"), new StartTime("12:00"),
+                    new EndTime("18:00"), new Location("Sentosa")),
+            new Event(new EventName("AlphaQ Project Meeting"), new Description("Finalize project final report"),
+                    new EventDate("2018-11-10"), new StartTime("20:00"),
+                    new EndTime("23:00"), new Location("Alex's House"), new Attendees(attendeesOne)),
+            new Event(new EventName("Practical Exam 1"), new Description("good luck have fun. have mercy"),
+                    new EventDate("2018-11-03"), new StartTime("16:00"),
+                    new EndTime("18:00"), new Location("LT15"))
         };
     }
 
