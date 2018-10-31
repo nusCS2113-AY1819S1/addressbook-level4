41e34f549a800071c8f86f2e2897870e8f353741
e0191729 <e0191729@u.nus.edu>
2018-10-31 20:37:06 +0800

    Checkstyle Checked

--- src/main/java/com/t13g2/forum/storage/forum/IStorage.java
+++ src/main/java/com/t13g2/forum/storage/forum/IStorage.java
@@ -1,15 +1,8 @@
 package com.t13g2.forum.storage.forum;
 
-<<<<<<< Updated upstream
 /**
-<<<<<<< HEAD
- * IStorage
-=======
  * storage interface.
->>>>>>> da8292eaadbda97d0f97d93a465a249ed24a7543
  */
-=======
->>>>>>> Stashed changes
 public interface IStorage {
     <T> T read(Class clazz);
 
