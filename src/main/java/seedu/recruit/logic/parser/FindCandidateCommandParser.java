6d9965f29238d5f15b8badb7ac3ae7f4f7ef135a
kianmeng <kianmeng1996@gmail.com>
2018-10-31 12:09:53 +0800

    change some name

--- src/main/java/seedu/recruit/logic/parser/FindCandidateCommandParser.java
+++ src/main/java/seedu/recruit/logic/parser/FindCandidateCommandParser.java
@@ -38,40 +38,40 @@ public class FindCandidateCommandParser implements Parser<FindCandidateCommand> 
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_GENDER, PREFIX_AGE, PREFIX_PHONE, PREFIX_EMAIL,
                         PREFIX_ADDRESS, PREFIX_JOB, PREFIX_EDUCATION, PREFIX_SALARY, PREFIX_TAG);
 
-        HashMap<String, List<String>> keywordsList = new HashMap<>();
+        HashMap<String, List<String>> keywordsMap = new HashMap<>();
         if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-            keywordsList.put("Name", (argMultimap.getAllValues(PREFIX_NAME)));
+            keywordsMap.put("Name", (argMultimap.getAllValues(PREFIX_NAME)));
         }
         if (argMultimap.getValue(PREFIX_GENDER).isPresent()) {
-            keywordsList.put("Gender", (argMultimap.getAllValues(PREFIX_GENDER)));
+            keywordsMap.put("Gender", (argMultimap.getAllValues(PREFIX_GENDER)));
         }
         if (argMultimap.getValue(PREFIX_AGE).isPresent()) {
-            keywordsList.put("Age", (argMultimap.getAllValues(PREFIX_AGE)));
+            keywordsMap.put("Age", (argMultimap.getAllValues(PREFIX_AGE)));
         }
         if (argMultimap.getValue(PREFIX_PHONE).isPresent()) {
-            keywordsList.put("Phone", (argMultimap.getAllValues(PREFIX_PHONE)));
+            keywordsMap.put("Phone", (argMultimap.getAllValues(PREFIX_PHONE)));
         }
         if (argMultimap.getValue(PREFIX_EMAIL).isPresent()) {
-            keywordsList.put("Email", (argMultimap.getAllValues(PREFIX_EMAIL)));
+            keywordsMap.put("Email", (argMultimap.getAllValues(PREFIX_EMAIL)));
         }
         if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
-            keywordsList.put("Address", (argMultimap.getAllValues(PREFIX_ADDRESS)));
+            keywordsMap.put("Address", (argMultimap.getAllValues(PREFIX_ADDRESS)));
         }
         if (argMultimap.getValue(PREFIX_JOB).isPresent()) {
-            keywordsList.put("Job", (argMultimap.getAllValues(PREFIX_JOB)));
+            keywordsMap.put("Job", (argMultimap.getAllValues(PREFIX_JOB)));
         }
         if (argMultimap.getValue(PREFIX_EDUCATION).isPresent()) {
-            keywordsList.put("Education", (argMultimap.getAllValues(PREFIX_EDUCATION)));
+            keywordsMap.put("Education", (argMultimap.getAllValues(PREFIX_EDUCATION)));
         }
         if (argMultimap.getValue(PREFIX_SALARY).isPresent()) {
-            keywordsList.put("Salary", (argMultimap.getAllValues(PREFIX_SALARY)));
+            keywordsMap.put("Salary", (argMultimap.getAllValues(PREFIX_SALARY)));
         }
 
-        if (keywordsList.isEmpty()) {
+        if (keywordsMap.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCandidateCommand.MESSAGE_USAGE));
         }
 
-        return new FindCandidateCommand(new CandidateContainsKeywordsPredicate(keywordsList));
+        return new FindCandidateCommand(new CandidateContainsKeywordsPredicate(keywordsMap));
     }
 }
