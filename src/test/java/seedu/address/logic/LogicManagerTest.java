eb896792c4ad4279affe8c21f4cf9190039663d5
sanjukta99 <sanjuktasaha0906@gmail.com>
2018-11-01 14:30:09 +0800

    edited logic manager test

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -52,6 +52,18 @@ public class LogicManagerTest {
         logic.getFilteredPersonList().remove(0);
     }
 
+    @Test
+    public void getFilteredClubsList_modifyList_throwsUnsupportedOperationException() {
+        thrown.expect(UnsupportedOperationException.class);
+        logic.getFilteredClubsList().remove(0);
+    }
+
+    @Test
+    public void getFilteredClubBudgetsList_modifyList_throwsUnsupportedOperationException() {
+        thrown.expect(UnsupportedOperationException.class);
+        logic.getFilteredClubBudgetsList().remove(0);
+    }
+
     /**
      * Executes the command, confirms that no exceptions are thrown and that the result message is correct.
      * Also confirms that {@code expectedModel} is as specified.
