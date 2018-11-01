1ef802104ee00bb49ba8ceedea3155fec0bd1f67
Lunastryke <e0203190@u.nus.edu>
2018-11-01 12:27:30 +0800

    BrowserPanel.java: Checkstyle

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -13,7 +13,6 @@ import javafx.scene.web.WebView;
 import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.InventoryPanelSelectionChangedEvent;
-import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.drink.Drink;
 
 /**
