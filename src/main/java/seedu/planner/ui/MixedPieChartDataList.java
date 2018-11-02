fbf0c63719bef111adcce90379f7bf9b999cb62f
Vincent <vencintgamer@gmail.com>
2018-11-02 20:56:13 +0800

    Created an intermediate class to handle the mixed data required by CustomPieChart

---
+++ src/main/java/seedu/planner/ui/MixedPieChartDataList.java
@@ -0,0 +1,93 @@
+package seedu.planner.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javafx.collections.ObservableList;
+import seedu.planner.model.summary.CategoryStatistic;
+
+/**
+ * This class is responsible for converting data as a {@code ObservableList<CategoryStatistic>} into data that can be
+ * easily read by the custom pieChart implementation which requires different values for the label and the legend
+ */
+public class MixedPieChartDataList {
+
+    private static final String untaggedLabel = "<<untagged>>";
+
+    private List<ChartData> expenseChartLabelData;
+    private List<ChartData> incomeChartLabelData;
+    private List<ChartData> expenseChartLegendData;
+    private List<ChartData> incomeChartLegendData;
+
+    private Double totalIncome = 0.0;
+    private Double totalExpense = 0.0;
+
+    public MixedPieChartDataList(ObservableList<CategoryStatistic> data) {
+        expenseChartLabelData = new ArrayList<>();
+        incomeChartLabelData = new ArrayList<>();
+        for (CategoryStatistic d : data) {
+            addToExpenseLabelData(d);
+            addToIncomeLabelData(d);
+        }
+        expenseChartLegendData = expenseChartLabelData.stream().map(d -> convertToPercentages(d, totalExpense))
+                .collect(Collectors.toList());
+        incomeChartLegendData = incomeChartLabelData.stream().map(d -> convertToPercentages(d, totalIncome))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Process given data and adds new ChartData to the incomeChartLabelData
+     * @param data data to be added
+     */
+    private void addToIncomeLabelData(CategoryStatistic data) {
+        if (data.getTotalIncome() > 0.0) {
+            String label;
+            if (data.getTags().isEmpty()) {
+                label = untaggedLabel;
+            } else {
+                label = data.getTags().toString();
+            }
+            incomeChartLabelData.add(new ChartData(label, data.getTotalIncome()));
+            totalIncome += data.getTotalIncome();
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * Process given data and adds new ChartData to the expenseChartLabelData
+     * @param data data to be added
+     */
+    private void addToExpenseLabelData(CategoryStatistic data) {
+        if (data.getTotalExpense() > 0.0) {
+            String label;
+            if (data.getTags().isEmpty()) {
+                label = untaggedLabel;
+            } else {
+                label = data.getTags().toString();
+            }
+            expenseChartLabelData.add(new ChartData(label, data.getTotalExpense()));
+            totalExpense += data.getTotalExpense();
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * Computes the proportion of a given {@code ChartData} data over the given total amount
+     * @param data data to be converted
+     * @param total reference total amount
+     * @return
+     */
+    private ChartData convertToPercentages(ChartData data, Double total) {
+        if (total > 0.0) {
+            Double newValue = Double.parseDouble(String.format("%.2f", data.value / total * 100.0));
+            return new ChartData(data.key, newValue);
+        } else {
+            return data;
+        }
+    }
+
+
+}
