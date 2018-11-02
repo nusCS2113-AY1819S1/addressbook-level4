7f669fb8cdce6c1cb64f4093e7fa875ea4011b46
XiaoYunhan <2028427786@qq.com>
2018-11-02 15:29:49 +0800

    fix travis

--- src/test/java/seedu/address/logic/commands/ViewTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewTaskCommandTest.java
@@ -1,24 +1,36 @@
 package seedu.address.logic.commands;
 
-import org.junit.Test;
-import seedu.address.logic.CommandHistory;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalExpenditures.getTypicalExpenditureTracker;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalTasks.getTypicalTodoList;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ViewTaskCommand.
+ * {@code ViewTaskCommand}.
+ */
 public class ViewTaskCommandTest {
-    private Model model = new ModelManager(getTypicalAddressBook(), getTypicalTodoList(),
-            getTypicalExpenditureTracker(), new UserPrefs());
-    private Model expectedModel = new ModelManager(getTypicalAddressBook(), getTypicalTodoList(),
-            getTypicalExpenditureTracker(), new UserPrefs());
+    private Model model;
+    private Model expectedModel;
     private CommandHistory commandHistory = new CommandHistory();
+
+    @Before
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), getTypicalTodoList(), getTypicalExpenditureTracker(), new UserPrefs());
+        expectedModel = new ModelManager(getTypicalAddressBook(), getTypicalTodoList(), getTypicalExpenditureTracker(), new UserPrefs());
+    }
+
     @Test
     public void execute_view_showsCompletedSuccess() {
+
         assertCommandSuccess(new ViewTaskCommand("completed"), model, commandHistory, ViewTaskCommand.MESSAGE_SUCCESS_COMPLETED, expectedModel);
     }
     @Test
