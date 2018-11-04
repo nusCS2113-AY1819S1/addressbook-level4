bbdaad311286611c07135c2d9a98248cadc7b689
nianfei97 <tehnianfei@gmail.com>
2018-11-04 11:52:23 +0800

    Accepts non-zero minute timeslots

--- src/main/java/seedu/address/model/person/TimeSlot.java
+++ src/main/java/seedu/address/model/person/TimeSlot.java
@@ -26,7 +26,10 @@ public class TimeSlot {
 
     // TODO: Change to accept times with non 0 minutes
     public static final String VALIDATION_REGEX =
-            "\\w+(\\s*)([0-9]|[0-1][0-9]|[2][0-3])[:]?[0]?[0]?(\\s*)[-](\\s*)([0-9]|[0-1][0-9]|[2][0-3])[:]?[0]?[0]?";
+            "\\w+(\\s*)"
+            + "([0-9]|[0-1][0-9]|[2][0-3])[:]?[0-5]?[0-9]?"
+            + "(\\s*)[-](\\s*)"
+            + "([0-9]|[0-1][0-9]|[2][0-3])[:]?[0-5]?[0-9]?";
 
     private static final Color DEFAULT_COLOR = Color.BLACK;
 
