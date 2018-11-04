84573323b7a0011c4fd752dde60d53d505a0405a
Unknown <geraldcdx5@gmail.com>
2018-11-04 21:46:57 +0800

    Adding system test

--- src/test/java/systemtests/CommentCommandSystemTest.java
+++ src/test/java/systemtests/CommentCommandSystemTest.java
@@ -1,23 +1,31 @@
 package systemtests;
 
+import static org.junit.Assert.assertEquals;
+
 import static seedu.address.logic.commands.CommandTestUtil.ADMIN_PASSWORD_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.ADMIN_USERNAME_DESC;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADMIN_PASSWORD;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADMIN_USERNAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_COMMENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_LINE;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EVENTS;
 import static seedu.address.testutil.TypicalEvents.DANIEL;
+import static seedu.address.testutil.TypicalEvents.getTypicalEventManager;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_EVENT;
 
 import org.junit.Test;
 
 import seedu.address.commons.core.index.Index;
+
 import seedu.address.logic.commands.AddCommentCommand;
-import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.CommentAssertion;
+import seedu.address.logic.commands.DeleteCommentCommand;
 import seedu.address.logic.commands.LoginCommand;
+import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.ReplyCommentCommand;
+import seedu.address.logic.commands.UndoCommand;
 import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.event.Event;
 import seedu.address.model.user.User;
 import seedu.address.testutil.EventBuilder;
@@ -35,56 +43,91 @@ public class CommentCommandSystemTest extends EventManagerSystemTest {
                 + ADMIN_USERNAME_DESC + "  " + ADMIN_PASSWORD_DESC + "  ";
         assertCommandSuccess(command, toLogin);
 
-
+        //Add a comment
         Index index = INDEX_FIRST_EVENT;
         command = "   " + AddCommentCommand.COMMAND_WORD + " " + index.getOneBased() + " " + PREFIX_COMMENT + "Hi";
         Event editedEvent = new EventBuilder(DANIEL).withComment("{span}Comment Section{/span}{ol}{li}"
                 + "admin : Hi{/li}{/ol}").build();
-        assertCommandSuccess(command, index, editedEvent);
+        Model expectedModel = new ModelManager(getTypicalEventManager(), new UserPrefs());
+        expectedModel.logUser(new UserBuilder().build());
+        expectedModel.updateEvent(expectedModel.getFilteredEventList().get(0), editedEvent);
+        String successMessage = String.format(AddCommentCommand.MESSAGE_ADD_COMMENT, "Hi", index.getOneBased());
+        assertCommandSuccess(expectedModel, model, command, successMessage, index);
 
+        //Reply First Comment
         command = ReplyCommentCommand.COMMAND_WORD + " " + index.getOneBased() + " " + PREFIX_COMMENT + "Hello" + " "
                 + PREFIX_LINE + "1";
         editedEvent = new EventBuilder(DANIEL).withComment("{span}Comment Section{/span}{ol}{li}"
                 + "admin : Hi{/li}{li} (REPLY) admin : Hello{/li}{/ol}").build();
-        assertCommandSuccess(command, index, editedEvent);
+        expectedModel = new ModelManager(getTypicalEventManager(), new UserPrefs());
+        expectedModel.logUser(new UserBuilder().build());
+        expectedModel.updateEvent(expectedModel.getFilteredEventList().get(0), editedEvent);
+        successMessage = String.format(
+                ReplyCommentCommand.MESSAGE_REPLY_COMMENT, "Hello", index.getOneBased(), 1);
+        assertCommandSuccess(expectedModel, model, command, successMessage, index);
+
+        //Undo
+        command = UndoCommand.COMMAND_WORD;
+        successMessage = UndoCommand.MESSAGE_SUCCESS;
+        editedEvent = new EventBuilder(DANIEL).withComment("{span}Comment Section{/span}{ol}{li}"
+                + "admin : Hi{/li}{/ol}").build();
+        expectedModel.undoEventManager();
+        assertCommandSuccess(expectedModel, model, command, successMessage);
+
+        //Redo
+        command = RedoCommand.COMMAND_WORD;
+        successMessage = RedoCommand.MESSAGE_SUCCESS;
+        editedEvent = new EventBuilder(DANIEL).withComment("{span}Comment Section{/span}{ol}{li}"
+                + "admin : Hi{/li}{li} (REPLY) admin : Hello{/li}{/ol}").build();
+        expectedModel = new ModelManager(getTypicalEventManager(), new UserPrefs());
+        expectedModel.logUser(new UserBuilder().build());
+        expectedModel.updateEvent(expectedModel.getFilteredEventList().get(0), editedEvent);
+        assertCommandSuccess(expectedModel, model, command, successMessage);
+
+        //Delete replied comment
+        command = DeleteCommentCommand.COMMAND_WORD + " " + index.getOneBased() + " " + PREFIX_LINE + "2";
+        editedEvent = new EventBuilder(DANIEL).withComment("{span}Comment Section{/span}{ol}{li}"
+                + "admin : Hi{/li}{/ol}").build();
+        expectedModel = new ModelManager(getTypicalEventManager(), new UserPrefs());
+        expectedModel.logUser(new UserBuilder().build());
+        expectedModel.updateEvent(expectedModel.getFilteredEventList().get(0), editedEvent);
+        successMessage = String.format(
+                DeleteCommentCommand.MESSAGE_DELETE_COMMENT, index.getOneBased(), 2);
+        assertCommandSuccess(expectedModel, model, command, successMessage, index);
 
     }
 
     /**
-     * Performs the same verification as {@code assertCommandSuccess(String, Index, Event, Index)} except that
-     * the browser url and selected card remain unchanged.
-     * @param toEdit the index of the current model's filtered list
-     * @see EditCommandSystemTest#assertCommandSuccess(String, Index, Event, Index)
+     *
+     * @param expectedModel
+     * @param model
+     * @param command
+     * @param successMessage
+     * @param index
      */
-    private void assertCommandSuccess(String command, Index toEdit, Event editedEvent) {
-        assertCommandSuccess(command, toEdit, editedEvent, null);
+    private void assertCommandSuccess(Model expectedModel, Model model, String command, String successMessage,
+                                      Index index) {
+        expectedModel.commitEventManager();
+        executeCommand(command);
+        assertSelectedCardChanged(index);
+        assertEquals(getResultDisplay().getText(), successMessage);
+        CommentAssertion handle = new CommentAssertion();
+        handle.assertSuccessModel(expectedModel, model);
     }
 
     /**
-     * Performs the same verification as {@code assertCommandSuccess(String, Model, String, Index)} except that the
-     * browser url and selected card remain unchanged.
-     * @see EditCommandSystemTest#assertCommandSuccess(String, Model, String, Index)
+     *
+     * @param expectedModel
+     * @param model
+     * @param command
+     * @param successMessage
      */
-    private void assertCommandSuccess(String command, Model expectedModel, String expectedResultMessage) {
-        assertCommandSuccess(command, expectedModel, expectedResultMessage, null);
-    }
-
-    /**
-     * Performs the same verification as {@code assertCommandSuccess(String, Model, String, Index)} and in addition,<br>
-     * 1. Asserts that result display box displays the success message of executing {@code EditCommand}.<br>
-     * 2. Asserts that the model related components are updated to reflect the event at index {@code toEdit} being
-     * updated to values specified {@code editedEvent}.<br>
-     * @param toEdit the index of the current model's filtered list.
-     * @see EditCommandSystemTest#assertCommandSuccess(String, Model, String, Index)
-     */
-    private void assertCommandSuccess(String command, Index toEdit, Event editedEvent,
-                                      Index expectedSelectedCardIndex) {
-        Model expectedModel = getModel();
-        expectedModel.updateEvent(expectedModel.getFilteredEventList().get(toEdit.getZeroBased()), editedEvent);
-        expectedModel.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
-
-        assertCommandSuccess(command, expectedModel,
-                String.format(EditCommand.MESSAGE_EDIT_EVENT_SUCCESS, editedEvent), expectedSelectedCardIndex);
+    private void assertCommandSuccess(Model expectedModel, Model model, String command, String successMessage) {
+        expectedModel.commitEventManager();
+        executeCommand(command);
+        assertEquals(getResultDisplay().getText(), successMessage);
+        CommentAssertion handle = new CommentAssertion();
+        handle.assertSuccessModel(expectedModel, model);
     }
 
     /**
@@ -94,35 +137,7 @@ public class CommentCommandSystemTest extends EventManagerSystemTest {
         Model expectedModel = getModel();
         expectedModel.logUser(toLogin);
         String expectedResultMessage = String.format(LoginCommand.MESSAGE_SUCCESS, toLogin.getUsername().toString());
-
         assertCommandSuccessLogin(command, expectedModel, expectedResultMessage);
-    }
-
-    /**
-     * Executes {@code command} and in addition,<br>
-     * 1. Asserts that the command box displays an empty string.<br>
-     * 2. Asserts that the result display box displays {@code expectedResultMessage}.<br>
-     * 3. Asserts that the browser url and selected card update accordingly depending on the card at
-     * {@code expectedSelectedCardIndex}.<br>
-     * 4. Asserts that the status bar's sync status changes.<br>
-     * 5. Asserts that the command box has the default style class.<br>
-     * Verifications 1 and 2 are performed by
-     * {@code EventManagerSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
-     * @see EventManagerSystemTest#assertApplicationDisplaysExpected(String, String, Model)
-     * @see EventManagerSystemTest#assertSelectedCardChanged(Index)
-     */
-    private void assertCommandSuccess(String command, Model expectedModel, String expectedResultMessage,
-                                      Index expectedSelectedCardIndex) {
-        executeCommand(command);
-        expectedModel.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
-        assertApplicationDisplaysExpected("", expectedResultMessage, expectedModel);
-        assertCommandBoxShowsDefaultStyle();
-        if (expectedSelectedCardIndex != null) {
-            assertSelectedCardChanged(expectedSelectedCardIndex);
-        } else {
-            assertSelectedCardUnchanged();
-        }
-        assertStatusBarUnchangedExceptSyncStatus();
     }
 
     /**
@@ -139,22 +154,4 @@ public class CommentCommandSystemTest extends EventManagerSystemTest {
         assertCommandBoxShowsDefaultStyle();
     }
 
-    /**
-     * Executes {@code command} and in addition,<br>
-     * 1. Asserts that the command box displays {@code command}.<br>
-     * 2. Asserts that result display box displays {@code expectedResultMessage}.<br>
-     * 3. Asserts that the browser url, selected card and status bar remain unchanged.<br>
-     * 4. Asserts that the command box has the error style.<br>
-     * Verifications 1 and 2 are performed by
-     * {@code EventManagerSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
-     * @see EventManagerSystemTest#assertApplicationDisplaysExpected(String, String, Model)
-     */
-    private void assertCommandFailure(String command, String expectedResultMessage) {
-        Model expectedModel = getModel();
-        executeCommand(command);
-        assertApplicationDisplaysExpected(command, expectedResultMessage, expectedModel);
-        assertSelectedCardUnchanged();
-        assertCommandBoxShowsErrorStyle();
-        assertStatusBarUnchanged();
-    }
 }
