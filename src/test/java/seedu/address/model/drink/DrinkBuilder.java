fd6067d7fffda8ac9e7aaea16f9230b1e2f516a0
bluein-green <j.minjia@gmail.com>
2018-11-04 09:31:31 +0800

    new stuff?

--- src/test/java/seedu/address/model/drink/DrinkBuilder.java
+++ src/test/java/seedu/address/model/drink/DrinkBuilder.java
@@ -17,55 +17,57 @@ public class DrinkBuilder {
         tags = new HashSet<>();
     }
 
-    /**
-     * Initializes the PersonBuilder with the data of {@code drinkToCopy}.
-     */
-    public DrinkBuilder(Drink drinkToCopy) {
-        name = drinkToCopy.getName();
-        tags = new HashSet<>(drinkToCopy.getTags());
-    }
+//    /**
+//     * Initializes the PersonBuilder with the data of {@code drinkToCopy}.
+//     */
+//    public DrinkBuilder(Drink drinkToCopy) {
+//        name = drinkToCopy.getName();
+//        tags = new HashSet<>(drinkToCopy.getTags());
+//    }
+//
+//    /**
+//     * Sets the {@code Name} of the {@code Person} that we are building.
+//     */
+//    public DrinkBuilder withName(String name) {
+//        this.name = new Name(name);
+//        return this;
+//    }
+//
+//    /**
+//     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Person} that we are building.
+//     */
+//    public DrinkBuilder withTags(String... tags) {
+//        this.tags = SampleDataUtil.getTagSet(tags);
+//        return this;
+//    }
+//
+//    /**
+//     * Sets the {@code Address} of the {@code Person} that we are building.
+//     */
+//    public DrinkBuilder withAddress(String address) {
+//        this.address = new Address(address);
+//        return this;
+//    }
+//
+//    /**
+//     * Sets the {@code Phone} of the {@code Person} that we are building.
+//     */
+//    public DrinkBuilder withPhone(String phone) {
+//        this.phone = new Phone(phone);
+//        return this;
+//    }
+//
+//    /**
+//     * Sets the {@code Email} of the {@code Person} that we are building.
+//     */
+//    public DrinkBuilder withEmail(String email) {
+//        this.email = new Email(email);
+//        return this;
+//    }
+//
+//    public Drink build() {
+//        return new Person(name, phone, email, address, tags);
+//    }
 
-    /**
-     * Sets the {@code Name} of the {@code Person} that we are building.
-     */
-    public PersonBuilder withName(String name) {
-        this.name = new Name(name);
-        return this;
-    }
 
-    /**
-     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Person} that we are building.
-     */
-    public DrinkBuilder withTags(String... tags) {
-        this.tags = SampleDataUtil.getTagSet(tags);
-        return this;
-    }
-
-    /**
-     * Sets the {@code Address} of the {@code Person} that we are building.
-     */
-    public PersonBuilder withAddress(String address) {
-        this.address = new Address(address);
-        return this;
-    }
-
-    /**
-     * Sets the {@code Phone} of the {@code Person} that we are building.
-     */
-    public PersonBuilder withPhone(String phone) {
-        this.phone = new Phone(phone);
-        return this;
-    }
-
-    /**
-     * Sets the {@code Email} of the {@code Person} that we are building.
-     */
-    public PersonBuilder withEmail(String email) {
-        this.email = new Email(email);
-        return this;
-    }
-
-    public Person build() {
-        return new Person(name, phone, email, address, tags);
-    }
 }
