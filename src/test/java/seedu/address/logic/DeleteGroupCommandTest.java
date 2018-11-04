ac9243ba7f8565a247d91d4cee8c4be8dbbe5c98
novin997 <e0176909@u.nus.edu>
2018-11-04 14:55:27 +0800

    fix checkstyle errors

--- src/test/java/seedu/address/logic/DeleteGroupCommandTest.java
+++ src/test/java/seedu/address/logic/DeleteGroupCommandTest.java
@@ -256,7 +256,6 @@ public class DeleteGroupCommandTest {
         public void deleteGroupPerson(Group group, Person target) {
             throw new AssertionError("This method should not be called.");
         }
-        
         @Override
         public String getScriptFolderLocation() {
             throw new AssertionError("This method should not be called.");
