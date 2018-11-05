01554fc5accae9eec5884f95858adc211f686e73
cjinting-nus <e0175290@u.nus.edu>
2018-11-05 17:33:16 +0800

    Fixed FindCommand

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -31,6 +31,8 @@ public class FindCommand extends Command {
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
         model.updateFilteredPersonList(predicate);
+        model.updateOtherList(predicate);
+        model.updateFriendList(predicate);
         return new CommandResult(
                 String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
     }
