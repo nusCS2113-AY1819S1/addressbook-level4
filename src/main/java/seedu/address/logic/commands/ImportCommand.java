03c625f3f2df49e702e0723eb505ba7958e11d3a
jitwei98 <limjitwei@gmail.com>
2018-11-06 15:58:35 +0800

    ImportCommand.java: Display the number of persons and todos imported

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -22,7 +22,8 @@ public class ImportCommand extends Command {
             + "Parameters: FILENAME (required)\n"
             + "Example: " + COMMAND_WORD + " export.xml ";
 
-    public static final String MESSAGE_IMPORT_SUCCESS = "Import success!";
+    // TODO: do the same for todos
+    public static final String MESSAGE_IMPORT_SUCCESS = "Imported %1$s persons and %2$s todos!";
     // = "Imported Persons: %1$s, Imported Todos: %1$s";
     private static final String MESSAGE_FAILURE = "Import failed!";
 
@@ -38,6 +39,9 @@ public class ImportCommand extends Command {
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
 
+        int initialNumberOfPersons = model.getFilteredPersonList().size();
+        int initialNumberOfTodos = model.getFilteredTodoList().size();
+
         // TODO: Write better Exception messages
         try {
             model.importAddressBook(filePath);
@@ -47,7 +51,33 @@ public class ImportCommand extends Command {
             throw new CommandException("DataConversionException");
         }
 
-        return new CommandResult(MESSAGE_IMPORT_SUCCESS);
+        int finalNumberOfPersons = model.getFilteredPersonList().size();
+        int finalNumberOfTodos = model.getFilteredTodoList().size();
+
+        int personsImported = calculateImportedEntries(initialNumberOfPersons, finalNumberOfPersons);
+        int todosImported = calculateImportedEntries(initialNumberOfTodos, finalNumberOfTodos);
+
+        return new CommandResult(String.format(MESSAGE_IMPORT_SUCCESS, personsImported, todosImported));
+    }
+
+    /**
+     * Returns the number of entries imported to the address book.
+     * @throws CommandException If an error occurs during command execution.
+     */
+    private static int calculateImportedEntries(int initialListSize, int finalListSize) throws CommandException {
+        requireNonNull(initialListSize);
+        requireNonNull(finalListSize);
+
+        int importedEntries = finalListSize - initialListSize;
+        if (!isValidSize(importedEntries)) {
+            // TODO: Write better Exception message
+            throw new CommandException("Invalid imported person list size");
+        }
+        return importedEntries;
+    }
+
+    private static boolean isValidSize(int size) {
+        return size >= 0;
     }
 
     @Override
