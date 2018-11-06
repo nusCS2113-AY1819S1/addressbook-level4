298ac043b50cd83190ee9e9396780b7dd63827bc
jitwei98 <limjitwei@gmail.com>
2018-11-06 20:55:10 +0800

    AddCommandTest.java: Update ModelStub

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -169,6 +169,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void addToAddressBook(ReadOnlyAddressBook addressBookToImported) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public void exportFilteredAddressBook(Path exportFilePath) throws IOException {
             throw new AssertionError("This method should not be called.");
         }
