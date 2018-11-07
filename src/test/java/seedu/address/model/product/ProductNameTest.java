5344cbbc424588259d220f6eb917333039e2b95d
garagaristahir <garagaristahir@hotmail.com>
2018-11-07 15:00:51 +0800

    Fixing errors

--- src/test/java/seedu/address/model/product/ProductNameTest.java
+++ src/test/java/seedu/address/model/product/ProductNameTest.java
@@ -11,31 +11,31 @@ public class ProductNameTest {
 
     @Test
     public void constructor_null_throwsNullPointerException() {
-        Assert.assertThrows(NullPointerException.class, () -> new ProductName(null));
+        Assert.assertThrows(NullPointerException.class, () -> new Name(null));
     }
 
     @Test
     public void constructor_invalidName_throwsIllegalArgumentException() {
         String invalidName = "";
-        Assert.assertThrows(IllegalArgumentException.class, () -> new ProductName(invalidName));
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Name(invalidName));
     }
 
     @Test
     public void isValidName() {
         // null name
-        Assert.assertThrows(NullPointerException.class, () -> ProductName.isValidName(null));
+        Assert.assertThrows(NullPointerException.class, () -> Name.isValidName(null));
 
         // invalid name
-        assertFalse(ProductName.isValidName("")); // empty string
-        assertFalse(ProductName.isValidName(" ")); // spaces only
-        assertFalse(ProductName.isValidName("^")); // only non-alphanumeric characters
-        assertFalse(ProductName.isValidName("peter*")); // contains non-alphanumeric characters
+        assertFalse(Name.isValidName("")); // empty string
+        assertFalse(Name.isValidName(" ")); // spaces only
+        assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
+        assertFalse(Name.isValidName("peter*")); // contains non-alphanumeric characters
 
         // valid name
-        assertTrue(ProductName.isValidName("peter jack")); // alphabets only
-        assertTrue(ProductName.isValidName("12345")); // numbers only
-        assertTrue(ProductName.isValidName("peter the 2nd")); // alphanumeric characters
-        assertTrue(ProductName.isValidName("Capital Tan")); // with capital letters
-        assertTrue(ProductName.isValidName("David Roger Jackson Ray Jr 2nd")); // long names
+        assertTrue(Name.isValidName("peter jack")); // alphabets only
+        assertTrue(Name.isValidName("12345")); // numbers only
+        assertTrue(Name.isValidName("peter the 2nd")); // alphanumeric characters
+        assertTrue(Name.isValidName("Capital Tan")); // with capital letters
+        assertTrue(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long names
     }
 }
