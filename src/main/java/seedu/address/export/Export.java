b3f77ca92c294cf619e482fcc119d9841247cd31
jitwei98 <limjitwei@gmail.com>
2018-11-07 15:48:58 +0800

    Export.java: Remove unused import

--- src/main/java/seedu/address/export/Export.java
+++ src/main/java/seedu/address/export/Export.java
@@ -5,7 +5,6 @@ import java.nio.file.Path;
 
 import javafx.collections.transformation.FilteredList;
 import seedu.address.model.person.Person;
-import seedu.address.model.todo.Todo;
 
 /**
  * The API of the Export component.
@@ -13,7 +12,7 @@ import seedu.address.model.todo.Todo;
 public interface Export {
     // TODO: add header in the interface header, refer to XmlAddressBookStorage
     /**
-     * Saves the filteredPersons and filteredTodos to the storage.
+     * Saves the filteredPersons to the storage.
      * @throws IOException if there was any problem writing to the file.
      */
     void saveFilteredAddressBook() throws IOException;
