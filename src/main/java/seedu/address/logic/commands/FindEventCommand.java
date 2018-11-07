51dac3f30104bf9e6bf56d486962b4d6b9c6b279
Ruslan Farkhutdinov <fr3ddy4@yandex.ru>
2018-11-07 16:03:09 +0700

    Find event command created

---
+++ src/main/java/seedu/address/logic/commands/FindEventCommand.java
@@ -0,0 +1,45 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.event.EventContainsKeywordsPredicate;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Finds and lists all persons in address book whose name contains any of the argument keywords
+ * or email equals the keyword.
+ * Keyword and email matching is case insensitive.
+ */
+public class FindEventCommand extends Command {
+
+    public static final String COMMAND_WORD = "findEvent";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all events whose names contain any of "
+            + "the specified keywords (case-insensitive) or the description matches the keywords (case-insensitive)"
+            + "and displays them.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " meeting";
+
+    private final EventContainsKeywordsPredicate predicate;
+
+    public FindEventCommand(EventContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
+        model.updateFilteredEventList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FindEventCommand // instanceof handles nulls
+                && predicate.equals(((FindEventCommand) other).predicate)); // state check
+    }
+}
