a5266478e02aaa4b8ad32ac71b59e560358bd0b6
cqinkai <qinkai96@gmail.com>
2018-11-07 22:56:46 +0800

    Again check-style error

--- src/main/java/seedu/address/logic/commands/UpdateStatusCommand.java
+++ src/main/java/seedu/address/logic/commands/UpdateStatusCommand.java
@@ -48,11 +48,11 @@ public class UpdateStatusCommand extends Command {
             model.updateEvent(updatingEvent, updatedEvent);
             model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
             EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex));
-             try {
-                 Thread.sleep(100);
-             } catch (InterruptedException e) {
-                 e.printStackTrace();
-             }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         return new CommandResult(MESSAGE_SUCCESS);
     }
