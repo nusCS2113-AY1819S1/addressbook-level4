422aeb8f3b3ae3e87c5e66b145a523ec3a576fa9
ParasK26 <bhttprs@gmail.com>
2018-11-07 12:57:54 +0800

    Fixed empty transaction with empty product tag or no product bug

--- src/main/java/seedu/address/logic/parser/AddTransactionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddTransactionCommandParser.java
@@ -2,6 +2,9 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PRODUCT;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import seedu.address.logic.commands.AddTransactionCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.timeidentifiedclass.Transaction;
@@ -12,25 +15,46 @@ import seedu.address.model.timeidentifiedclass.exceptions.ClosedTransactionExcep
  */
 public class AddTransactionCommandParser implements Parser<AddTransactionCommand> {
 
+    private boolean containsEmptyProductAtEnd(String args) {
+        int argLength = args.length();
+        if (argLength < 3) {
+            return false;
+        }
+        if (args.charAt(argLength - 1) == '/'
+                && args.charAt(argLength - 2) == 'r'
+                && args.charAt(argLength - 3) == 'p') {
+            return true;
+        }
+        return false;
+    }
     /**
      * Parses the given {@code String} of arguments in the context of the AddTransactionCommand
      * and returns an AddTransactionCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddTransactionCommand parse(String args) throws ParseException {
-        String[] productList = args.split(PREFIX_PRODUCT.toString());
-        if (productList.length == 0) {
+        args = args.trim();
+        if (containsEmptyProductAtEnd(args)) {
+            throw new ParseException("Some products in the transaction have no name! "
+                    + "Please enter their names before trying again");
+        }
+        ArrayList<String> productsToAdd = new ArrayList<>(Arrays.asList(args.split(PREFIX_PRODUCT.toString())));
+        if (productsToAdd.size() > 0 && productsToAdd.get(0).equals("")) {
+            productsToAdd.remove(0);
+        }
+        productsToAdd.trimToSize();
+        if (productsToAdd.size() == 0) {
             throw new ParseException("Transaction entered has no products!");
         }
         Transaction transaction = new Transaction();
-        for (int i = 0; i < productList.length; i++) {
+        for (String product : productsToAdd) {
+            product = product.trim();
             try {
-                productList[i].trim();
-                if (productList[i].length() == 0) {
-                    throw new ParseException("Some products in the transaction have no name!"
-                            + "Press the up arrow and enter their names before trying again";
+                if (product.length() == 0) {
+                    throw new ParseException("Some products in the transaction have no name! "
+                            + "Please enter their names before trying again");
                 }
-                transaction.addProduct(productList[i].trim());
+                transaction.addProduct(product);
             } catch (ClosedTransactionException e) {
                 // TODO: Exception handling in AddTransaction command parse. Closed transaction not possible as of yet.
             }
