15e58879a9064c416f491d524627cc5453d5d8a5
Ming Liang <lee.mingliang@outlook.com>
2018-11-08 16:26:07 +0800

    Fixed checkstyle

--- src/main/java/seedu/address/logic/parser/LogoutCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LogoutCommandParser.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.parser;
 
-import seedu.address.logic.commands.LoginCommand;
 import seedu.address.logic.commands.LogoutCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
