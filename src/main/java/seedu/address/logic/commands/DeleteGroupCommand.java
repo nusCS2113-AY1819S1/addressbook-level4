1d88d6c3415559ece354b262784f34cc155293b0
rajdeepsh <rajdeepsh95@gmail.com>
2018-11-10 01:12:40 +0800

    Add logger for DeleteGroupCommand

--- src/main/java/seedu/address/logic/commands/DeleteGroupCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteGroupCommand.java
@@ -3,7 +3,10 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
@@ -25,6 +28,11 @@ public class DeleteGroupCommand extends Command {
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_DELETE_GROUP_SUCCESS = "Deleted Group: %1$s";
+    public static final String LOG_COMMIT = "Version Committed";
+    public static final String LOG_COMMAND_SUCCESS = "Group has been deleted";
+    public static final String LOG_INVALID_GROUP_INDEX = "Invalid group index detected";
+
+    private static final Logger logger = LogsCenter.getLogger(DeleteGroupCommand.class);
 
     private final Index targetIndex;
 
@@ -41,12 +49,15 @@ public class DeleteGroupCommand extends Command {
         List<Group> lastShownList = model.getFilteredGroupList();
 
         if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            logger.log(Level.WARNING, LOG_INVALID_GROUP_INDEX);
             throw new CommandException(Messages.MESSAGE_INVALID_GROUP_DISPLAYED_INDEX);
         }
 
         Group groupToDelete = lastShownList.get(targetIndex.getZeroBased());
         model.deleteGroup(groupToDelete);
+        logger.log(Level.INFO, LOG_COMMAND_SUCCESS);
         model.commitAddressBook();
+        logger.log(Level.INFO, LOG_COMMIT);
         return new CommandResult(String.format(MESSAGE_DELETE_GROUP_SUCCESS, groupToDelete));
     }
 
