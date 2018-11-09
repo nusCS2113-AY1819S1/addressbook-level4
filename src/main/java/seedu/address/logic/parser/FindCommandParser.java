d2a6f4094c657d8c0c1e9557f40370404df5bf69
ladderinc <boon-jun1996@hotmail.com>
2018-11-09 21:30:36 +0800

    Removing useless javadocs from FindCommandParser

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -56,10 +56,6 @@ public class FindCommandParser implements Parser<FindCommand> {
         }
     }
 
-    /**
-     * Returns true if the first two arguments list implies that
-     * Exclude Tag Search has to be executed.
-     */
     private boolean isExcludeTagSearch(List<String> argumentsList) {
         return argumentsList.size() > 2 && (argumentsList.get(0).equals(TAG_OPTION_STRING)
                 && argumentsList.get(1).equals(EXCLUDE_OPTION_STRING) || argumentsList.get(1).equals(TAG_OPTION_STRING)
