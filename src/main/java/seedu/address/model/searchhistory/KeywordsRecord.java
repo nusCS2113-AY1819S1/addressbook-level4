7e1f06c4965d622b6f2e5efaf6f88c3e547ac613
ladderinc <boon-jun1996@hotmail.com>
2018-11-10 18:34:41 +0800

    Added logging to keywordsrecord and searchhistorymanager

--- src/main/java/seedu/address/model/searchhistory/KeywordsRecord.java
+++ src/main/java/seedu/address/model/searchhistory/KeywordsRecord.java
@@ -4,13 +4,17 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.SortedSet;
+import java.util.logging.Logger;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.searchhistory.exceptions.EmptyHistoryException;
 
 /**
  * Represents the in-app memory of all keywords relevant to the current Persons search.
  */
 public class KeywordsRecord implements ReadOnlyKeywordsRecord {
+
+    private static final Logger logger = LogsCenter.getLogger(KeywordsRecord.class);
 
     protected HashMap<KeywordType, KeywordsSet> map = new HashMap<>();
 
@@ -34,13 +38,14 @@ public class KeywordsRecord implements ReadOnlyKeywordsRecord {
      */
     public void undoKeywordsHistory() throws EmptyHistoryException {
         KeywordsBundle bundle = historyStack.pop();
+        logger.info("Removing from keywords history: "
+                + bundle.getType().name() + ":" + bundle.getKeywords().toString());
         KeywordsSet set = map.get(bundle.getType());
         set.removeKeywordsFromSet(bundle.getKeywords());
     }
 
     /**
      * Records keywords according to their type.
-     *
      * @param type KeywordType.
      * @param keywords list of words to be stored in memory.
      */
@@ -48,6 +53,7 @@ public class KeywordsRecord implements ReadOnlyKeywordsRecord {
         if (type == null || keywords == null) {
             throw new NullPointerException();
         }
+        logger.info("Adding to keywords history: " + type.name() + "," + keywords.toString());
         KeywordsSet set = map.get(type);
         set.addKeywordsToSet(keywords);
         updateHistoryStack(type, keywords);
