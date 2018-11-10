75539d7db13ebc86c93c78d2a9225ef65fd17d1a
Chocological <melvintan196@gmail.com>
2018-11-10 14:48:25 +0800

    Added logging and generified LoginCommand code

--- src/main/java/seedu/address/logic/commands/CreateAccountCommand.java
+++ src/main/java/seedu/address/logic/commands/CreateAccountCommand.java
@@ -1,7 +1,12 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.LoginManager;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -15,13 +20,14 @@ public class CreateAccountCommand extends Command {
 
     public static final String COMMAND_WORD = "createaccount";
 
+    public static final String MESSAGE_SUCCESS = "New account created: %1$s";
+    public static final String MESSAGE_DUPLICATE_ACCOUNT = "This account already exists in the login book";
+
+    private static final Logger logger = LogsCenter.getLogger(LoginUserIdPasswordRoleCommand.class);
+
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Creates an account for the login book. "
             + "Parameters: USERID PASSWORD ROLE\n"
             + "Example: " + COMMAND_WORD + " A1234567M zaq1xsw2cde3 treasurer";
-
-
-    public static final String MESSAGE_SUCCESS = "New account created: %1$s";
-    public static final String MESSAGE_DUPLICATE_ACCOUNT = "This account already exists in the login book";
 
     private final LoginDetails toAdd;
 
@@ -29,6 +35,9 @@ public class CreateAccountCommand extends Command {
      * Creates a CreateAccountCommand to add the specified {@code LoginDetails}
      */
     public CreateAccountCommand(LoginDetails loginDetails) {
+        if (isNull(loginDetails)) {
+            logger.log(Level.WARNING, "new account details is null!");
+        }
         requireNonNull(loginDetails);
         this.toAdd = loginDetails;
     }
@@ -38,11 +47,15 @@ public class CreateAccountCommand extends Command {
         requireNonNull(model);
 
         if (model.hasAccount(toAdd)) {
+            logger.log(Level.WARNING, "Duplicate account detected!");
             throw new CommandException(MESSAGE_DUPLICATE_ACCOUNT);
         }
 
         model.createAccount(toAdd);
+        logger.log(Level.INFO, "Account successfully created!");
         LoginManager.setIsCurrentlyCreatingAccount(false);
+        assert !LoginManager.getIsCurrentlyCreatingAccount()
+                : "LoginManager.getIsCurrentlyCreatingAccount() should be false";
         return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
     }
 
