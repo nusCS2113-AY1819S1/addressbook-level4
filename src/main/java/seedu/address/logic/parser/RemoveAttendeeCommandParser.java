409655dff1e39509dd1ddecb0dbe5df05ffd21f3
chiaxr <chia.xiangrong@gmail.com>
2018-11-10 18:44:56 +0800

    Edit required input style of RemoveAttendeeCommandParser

--- src/main/java/seedu/address/logic/parser/RemoveAttendeeCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemoveAttendeeCommandParser.java
@@ -1,6 +1,9 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_USERNAME;
+
+import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.RemoveAttendeeCommand;
@@ -18,15 +21,32 @@ public class RemoveAttendeeCommandParser implements Parser<RemoveAttendeeCommand
      * @throws ParseException if the user input does not conform the expected format
      */
     public RemoveAttendeeCommand parse(String args) throws ParseException {
-        String[] argsSplit = args.split("\\s+", 3);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_USERNAME);
 
-        // Check if index and username are provided
-        if (argsSplit.length < 3) {
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemoveAttendeeCommand.MESSAGE_USAGE), pe);
+        }
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_USERNAME)) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemoveAttendeeCommand.MESSAGE_USAGE));
         }
-        Index index = ParserUtil.parseIndex(argsSplit[1]);
-        Username username = ParserUtil.parseUsername(argsSplit[2]);
+
+        Username username = ParserUtil.parseUsername(argMultimap.getValue(PREFIX_USERNAME).get());
+
         return new RemoveAttendeeCommand(index, username);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
