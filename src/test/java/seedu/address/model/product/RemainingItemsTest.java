e1b1d3b03082576b3d73e2018fe8e262a9bd2384
garagaristahir <garagaristahir@hotmail.com>
2018-11-10 16:52:21 +0800

    Test Added for RemainingItems

---
+++ src/test/java/seedu/address/model/product/RemainingItemsTest.java
@@ -0,0 +1,39 @@
+package seedu.address.model.product;
+
+import org.junit.Test;
+import seedu.address.testutil.Assert;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+//@@author Gara
+public class RemainingItemsTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new RemainingItems(null));
+    }
+
+    @Test
+    public void constructor_invalidPhone_throwsIllegalArgumentException() {
+        String invalidPhone = "";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new RemainingItems(invalidPhone));
+    }
+
+    @Test
+    public void isValidPhone() {
+        // null phone number
+        Assert.assertThrows(NullPointerException.class, () -> SerialNumber.isValidPhone(null));
+
+        // invalid phone numbers
+        assertFalse(RemainingItems.isValidRemainingItems("")); // empty string
+        assertFalse(RemainingItems.isValidRemainingItems(" ")); // spaces only
+        assertFalse(RemainingItems.isValidRemainingItems("phone")); // non-numeric
+        assertFalse(RemainingItems.isValidRemainingItems("9011p041")); // alphabets within digits
+        assertFalse(RemainingItems.isValidRemainingItems("9312 1534")); // spaces within digits
+
+        // valid phone numbers
+        assertTrue(RemainingItems.isValidRemainingItems("93121534"));
+        assertTrue(RemainingItems.isValidRemainingItems("124293842033123")); // long phone numbers
+    }
+}
