4af083cc634620f22437a2118b650f055889f4bc
Tertium3 <longdung9900@gmail.com>
2018-11-10 23:33:53 +0800

    a

--- src/main/java/seedu/address/logic/commands/ExportCalendarCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCalendarCommand.java
@@ -184,7 +184,8 @@ public class ExportCalendarCommand extends Command {
      * @throws IOException when file stream have problems
      */
     public void exportICalenderFile(ObservableList<Event> registeredEventList, String fileName) throws IOException {
-        String outputFilename = CALENDAR_FILE_PATH + File.separator + String.format("%1$s.ics", fileName);
+        String outputFilename = CALENDAR_FILE_PATH + File.separator + String.format("%1$s.ics", fileName)
+                + File.separator ;
         File outputFile = new File(outputFilename);
 
         FileOutputStream fileOut = new FileOutputStream(outputFile, false);
