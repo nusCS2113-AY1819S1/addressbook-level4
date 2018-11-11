255d5174e6ca948814e5a8f21c89187671dff999
guiyong96 <leeguiyong96@gmail.com>
2018-11-11 09:34:28 +0800

    Fixing BookInventoryParserTest

--- src/test/java/seedu/address/logic/parser/BookInventoryParserTest.java
+++ src/test/java/seedu/address/logic/parser/BookInventoryParserTest.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.Rule;
-//import org.junit.Test;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
@@ -48,19 +47,21 @@ public class BookInventoryParserTest {
     @Test
     public void parseCommand_add() throws Exception {
         Book book = new BookBuilder().build();
-        AddCommand command = (AddCommand) parser.parseCommand(BookUtil.getAddCommand(book));
-        assertEquals(new AddCommand(book), command);
+        AddCommand command = (AddCommand) parser.parseCommand_withoutAuthentication(BookUtil.getAddCommand(book));
+        String left = new AddCommand(book).toString();
+        String right = command.toString();
+        assertEquals(left, right);
     }
 
     @Test
     public void parseCommand_clear() throws Exception {
-        assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD) instanceof ClearCommand);
-        assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
+        assertTrue(parser.parseCommand_withoutAuthentication(ClearCommand.COMMAND_WORD) instanceof ClearCommand);
+        assertTrue(parser.parseCommand_withoutAuthentication(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
     }
 
     @Test
     public void parseCommand_delete() throws Exception {
-        DeleteCommand command = (DeleteCommand) parser.parseCommand(
+        DeleteCommand command = (DeleteCommand) parser.parseCommand_withoutAuthentication(
                 DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_BOOK.getOneBased());
         assertEquals(new DeleteCommand(INDEX_ONE_BASED_FIRST_BOOK, ARGS_INDEX), command);
     }
@@ -69,38 +70,38 @@ public class BookInventoryParserTest {
     public void parseCommand_edit() throws Exception {
         Book book = new BookBuilder().build();
         EditBookDescriptor descriptor = new EditBookDescriptorBuilder(book).build();
-        EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
+        EditCommand command = (EditCommand) parser.parseCommand_withoutAuthentication(EditCommand.COMMAND_WORD + " "
                 + INDEX_FIRST_BOOK.getOneBased() + " " + BookUtil.getEditPersonDescriptorDetails(descriptor));
         assertNotEquals(new EditCommand(INDEX_FIRST_BOOK, descriptor), command);
     }
 
     @Test
     public void parseCommand_exit() throws Exception {
-        assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD) instanceof ExitCommand);
-        assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
+        assertTrue(parser.parseCommand_withoutAuthentication(ExitCommand.COMMAND_WORD) instanceof ExitCommand);
+        assertTrue(parser.parseCommand_withoutAuthentication(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
     }
 
     @Test
     public void parseCommand_find() throws Exception {
         List<String> keywords = Arrays.asList("foo", "bar", "baz");
-        FindCommand command = (FindCommand) parser.parseCommand(
+        FindCommand command = (FindCommand) parser.parseCommand_withoutAuthentication(
                 FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
         assertEquals(new FindCommand(new NameContainsKeywordsPredicate(keywords)), command);
     }
 
     @Test
     public void parseCommand_help() throws Exception {
-        assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD) instanceof HelpCommand);
-        assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD + " 3") instanceof HelpCommand);
+        assertTrue(parser.parseCommand_withoutAuthentication(HelpCommand.COMMAND_WORD) instanceof HelpCommand);
+        assertTrue(parser.parseCommand_withoutAuthentication(HelpCommand.COMMAND_WORD + " 3") instanceof HelpCommand);
     }
 
     @Test
     public void parseCommand_history() throws Exception {
-        assertTrue(parser.parseCommand(HistoryCommand.COMMAND_WORD) instanceof HistoryCommand);
-        assertTrue(parser.parseCommand(HistoryCommand.COMMAND_WORD + " 3") instanceof HistoryCommand);
-
+        assertTrue(parser.parseCommand_withoutAuthentication(HistoryCommand.COMMAND_WORD) instanceof HistoryCommand);
+        assertTrue(parser
+                .parseCommand_withoutAuthentication(HistoryCommand.COMMAND_WORD + " 3") instanceof HistoryCommand);
         try {
-            parser.parseCommand("histories");
+            parser.parseCommand_withoutAuthentication("histories");
             throw new AssertionError("The expected ParseException was not thrown.");
         } catch (ParseException pe) {
             assertNotEquals(MESSAGE_UNKNOWN_COMMAND, pe.getMessage());
@@ -109,41 +110,41 @@ public class BookInventoryParserTest {
 
     @Test
     public void parseCommand_list() throws Exception {
-        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
-        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
+        assertTrue(parser.parseCommand_withoutAuthentication(ListCommand.COMMAND_WORD) instanceof ListCommand);
+        assertTrue(parser.parseCommand_withoutAuthentication(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
     }
 
     @Test
     public void parseCommand_select() throws Exception {
-        SelectCommand command = (SelectCommand) parser.parseCommand(
+        SelectCommand command = (SelectCommand) parser.parseCommand_withoutAuthentication(
                 SelectCommand.COMMAND_WORD + " " + INDEX_FIRST_BOOK.getOneBased());
         assertEquals(new SelectCommand(INDEX_FIRST_BOOK), command);
     }
 
     @Test
     public void parseCommand_redoCommandWord_returnsRedoCommand() throws Exception {
-        assertTrue(parser.parseCommand(RedoCommand.COMMAND_WORD) instanceof RedoCommand);
-        assertTrue(parser.parseCommand("redo 1") instanceof RedoCommand);
+        assertTrue(parser.parseCommand_withoutAuthentication(RedoCommand.COMMAND_WORD) instanceof RedoCommand);
+        assertTrue(parser.parseCommand_withoutAuthentication("redo 1") instanceof RedoCommand);
     }
 
     @Test
     public void parseCommand_undoCommandWord_returnsUndoCommand() throws Exception {
-        assertTrue(parser.parseCommand(UndoCommand.COMMAND_WORD) instanceof UndoCommand);
-        assertTrue(parser.parseCommand("undo 3") instanceof UndoCommand);
+        assertTrue(parser.parseCommand_withoutAuthentication(UndoCommand.COMMAND_WORD) instanceof UndoCommand);
+        assertTrue(parser.parseCommand_withoutAuthentication("undo 3") instanceof UndoCommand);
     }
 
     @Test
     public void parseCommand_unrecognisedInput_throwsParseException() throws Exception {
         thrown.expect(ParseException.class);
         thrown.expectMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
-        parser.parseCommand("");
+        parser.parseCommand_withoutAuthentication("");
     }
 
     @Test
     public void parseCommand_unknownCommand_throwsParseException() throws Exception {
         thrown.expect(ParseException.class);
         thrown.expectMessage(MESSAGE_UNKNOWN_COMMAND);
-        parser.parseCommand("unknownCommand");
+        parser.parseCommand_withoutAuthentication("unknownCommand");
     }
 
 }
