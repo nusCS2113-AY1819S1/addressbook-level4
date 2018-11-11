499a1e5120d2991e64e20fad6db3bf53fc4fc497
Jie Liang Ang <32808126+jieliangang@users.noreply.github.com>
2018-11-11 16:21:12 +0800

    Add FindEvent to EmployeeParser andd ManagerParser  (#143)
    * Add FindEvent to EmployeeParser andd ManagerParser and tests
    
    * Add Ruslan's author name to RepoSense config

--- src/main/java/seedu/address/logic/parser/EmployeeParser.java
+++ src/main/java/seedu/address/logic/parser/EmployeeParser.java
@@ -15,6 +15,7 @@ import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.EditEventCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.FindEventCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.InviteCommand;
@@ -74,6 +75,9 @@ public class EmployeeParser extends CommandsParser {
 
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
+
+        case FindEventCommand.COMMAND_WORD:
+            return new FindEventCommandParser().parse(arguments);
 
         case ListCommand.COMMAND_WORD:
             return new ListCommandParser().parse(arguments);
