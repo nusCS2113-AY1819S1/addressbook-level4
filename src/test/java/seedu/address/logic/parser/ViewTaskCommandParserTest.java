6fa04f229143f3366db8b52541f8b30692b9c7c2
XiaoYunhan <2028427786@qq.com>
2018-11-11 20:11:58 +0800

    update test cases

--- src/test/java/seedu/address/logic/parser/ViewTaskCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewTaskCommandParserTest.java
@@ -1,4 +1,47 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+//import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.ViewTaskCommand;
+
 public class ViewTaskCommandParserTest {
+
+    private ViewTaskCommandParser parser = new ViewTaskCommandParser();
+
+    /*
+    @Test
+    public void parseParameterCompletedSuccess() {
+        Command command = new ViewTaskCommand("completed");
+        assertParseSuccess(parser, "completed", command);
+    }
+
+    @Test
+    public void parseParameterUncompletedSuccess() {
+        Command command = new ViewTaskCommand("uncompleted");
+        assertParseSuccess(parser, "uncompleted", command);
+    }
+
+    @Test
+    public void parseParameterAllSuccess() {
+        Command command = new ViewTaskCommand("all");
+        assertParseSuccess(parser, "all", command);
+    }
+    */
+
+    @Test
+    public void parseBlankFailure() {
+        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                ViewTaskCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parseInvalidFailure() {
+        assertParseFailure(parser, "invalid", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                ViewTaskCommand.MESSAGE_USAGE));
+    }
 }
