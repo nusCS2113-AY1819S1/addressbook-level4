43da5abfefb92367b1eebe0983e955898c4a8b70
guiyong96 <leeguiyong96@gmail.com>
2018-11-11 09:56:39 +0800

    Minor updates on Test Cases

--- src/test/java/seedu/address/logic/parser/DifferentiatingParserTest.java
+++ src/test/java/seedu/address/logic/parser/DifferentiatingParserTest.java
@@ -1,5 +1,24 @@
 package seedu.address.logic.parser;
 
-public class DifferentiatingParserTest {
+import org.junit.Rule;
+import org.junit.Test;
 
+public class DifferentiatingParserTest {
+    @Rule
+    private final DifferentiatingParser parser = new DifferentiatingParser();
+
+    @Test
+    public void parseInput_requestCommand() {
+
+    }
+
+    @Test
+    public void parseInput_deleteRequestCommand() {
+
+    }
+
+    @Test
+    public void parseInput_toggleRequestCommand() {
+
+    }
 }
--- src/test/java/seedu/address/logic/parser/SimilarityParserTest.java
+++ src/test/java/seedu/address/logic/parser/SimilarityParserTest.java
@@ -1,4 +1,25 @@
 package seedu.address.logic.parser;
 
+import org.junit.Rule;
+import org.junit.Test;
+
 public class SimilarityParserTest {
+    @Rule
+    private final SimilarityParser parser = new SimilarityParser();
+
+    @Test
+    public void performSimilarityCheck_addCommand() {
+
+    }
+
+    @Test
+    public void performSimilarityCheck_editCommand() {
+
+    }
+
+    @Test
+    public void performSimilarityCheck_requestCommand() {
+
+    }
+}
 }
