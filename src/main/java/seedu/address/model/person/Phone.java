444472391e9cd9878eb0ce5a1d6b738c8ee612a6
elstonayx <elston.ayx@u.nus.edu>
2018-11-11 22:35:36 +0800

    Resolves #152

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,8 +11,9 @@ public class Phone {
 
 
     public static final String MESSAGE_PHONE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
-    public static final String PHONE_VALIDATION_REGEX = "\\d{3,}";
+            "Phone numbers should only contain numbers, and it should be at least 3 digits long "
+                + "and at most 20 digits long";
+    public static final String PHONE_VALIDATION_REGEX = "\\d{3,20}";
     public final String value;
 
     /**
