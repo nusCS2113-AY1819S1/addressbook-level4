592ffa0bfe2c6b535e456c874ee98e1ce79651b8
XiaoYunhan <2028427786@qq.com>
2018-11-11 20:57:25 +0800

    fix travis

--- src/test/java/seedu/address/logic/parser/ViewTaskCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewTaskCommandParserTest.java
@@ -7,6 +7,7 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailur
 import org.junit.Test;
 
 //import seedu.address.logic.commands.Command;
+
 import seedu.address.logic.commands.ViewTaskCommand;
 
 public class ViewTaskCommandParserTest {
