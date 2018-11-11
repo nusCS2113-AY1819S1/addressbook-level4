d27707083db30d234a0e6c8c32b6bcc93344a3fc
XiaoYunhan <2028427786@qq.com>
2018-11-12 03:16:42 +0800

    fix travis

--- src/test/java/seedu/address/logic/commands/UncomplateTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UncomplateTaskCommandTest.java
@@ -1,14 +1,15 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.testutil.TypicalExpenditures.getTypicalExpenditureTracker;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalTasks.getTypicalTodoList;
+
 import org.junit.Before;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-
-import static seedu.address.testutil.TypicalExpenditures.getTypicalExpenditureTracker;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-import static seedu.address.testutil.TypicalTasks.getTypicalTodoList;
 
 public class UncomplateTaskCommandTest {
 
--- src/test/java/seedu/address/logic/parser/UncompleteTaskCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/UncompleteTaskCommandParserTest.java
@@ -1,5 +1,16 @@
 package seedu.address.logic.parser;
 
+/*
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_TASK;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.UncompleteTaskCommand;
+*/
+
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
  * outside of the UncompleteTaskCommand code. For example, inputs "1" and "1 abc" take the
@@ -8,5 +19,16 @@ package seedu.address.logic.parser;
  * therefore should be covered by the ParserUtilTest.
  */
 public class UncompleteTaskCommandParserTest {
-
+    /*
+    private UncompleteTaskCommandParser parser = new UncompleteTaskCommandParser();
+    @Test
+    public void parseValidArgsReturnsCompleteCommand() {
+        assertParseSuccess(parser, "1", new UncompleteTaskCommand(INDEX_FIRST_TASK));
+    }
+    @Test
+    public void parseInvalidArgsThrowsException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                UncompleteTaskCommand.MESSAGE_USAGE));
+    }
+    */
 }
