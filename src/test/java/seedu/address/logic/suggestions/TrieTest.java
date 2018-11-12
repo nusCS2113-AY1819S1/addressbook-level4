0f919a240dd37557c4ce0c7e2aa218463b87499f
elstonayx <elston.ayx@u.nus.edu>
2018-11-12 10:55:50 +0800

    Removed unused import statements

--- src/test/java/seedu/address/logic/suggestions/TrieTest.java
+++ src/test/java/seedu/address/logic/suggestions/TrieTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
