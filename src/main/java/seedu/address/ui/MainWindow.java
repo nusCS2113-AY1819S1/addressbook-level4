cdc9497a1f0d2cb5c7cf252af0b1db233552028b
LiuTianhang <E0175471@u.nus.edu>
2018-11-12 17:48:20 +0800

    update on bugs(login logout from transaction list (#178)
    * update on test and welcome message
    
    * change login page
    
    * added event call to change logic
    
    * fixed the bug regarding mutiplle subsribe of event after login logout
    
    * remove debug sentence
    
    * change command into their respective model
    
    asd
    
    * EOF
    
    * update ppp ug dg
    
    * update on logout login bug
    
    * Update loginInfoManager.json
    
    * update on the ug dg bug
    
    * Update docs/team/tianhang.adoc
    
    * update on bottom list confirmation message and "import" to sell command
    
    * let coder choose to have confirmation or not
    
    * move help window from docs to help
    
    this help developer as they can pull form github
    as /docs are auto generated by grandle
    
    *  new line eof
    
    * last minute check
    
    * Added newline at EOF
    
    * added some test
    
    need to fix a bug of login window
    (realted to empty username and password0
    
    * UG: fix #134
    
    * AnalysisManager: reduce code redundancies
    
    * analyseProfit and analyseRevenue (for all transactions) added
    
    * AnalysisPeriodType added
    
    * Modified Analysis to account for period
    
    * added more test
    
    * Date: added methods to return date 6 and 29 days before. created predicates to test for transaction within previous 7 and 30 days
    
    * used Predicates to filter transactions for analysis of costs
    
    * added TransactionList test
    
    * checkstyle for TransactionList
    
    * applay SLAP
    
    * changed
    
    * change test location
    
    * travis
    
    * travis EOF
    
    * travis please go alive
    
    * travis go heaven pls
    
    * travis go fly pls
    
    * added
    
    * move add and delete to manager
    
    * settle command
    
    * travis
    
    * change in ppp
    
    as make changes in UG /DG
    
    * changes to ppp
    
    * added
    
    * fix about us pic not showing
    
    * Update docs/AboutUs.adoc
    
    * update on UG
    
    * EOF
    
    * clean code
    
    * Update src/test/seedu/address/logic/commands/AddDrinkCommandTest.java
    
    * Revert "clean code"
    
    This reverts commit 717c9bd5094dcfbc12ce00745a24ebb52996424f.
    
    * Revert "Update src/test/seedu/address/logic/commands/AddDrinkCommandTest.java"
    
    This reverts commit d39bf0fb9cd1f499127b34125239d9d36f4f90ae.
    
    * added
    
    * Revert "Revert "clean code""
    
    This reverts commit ce7b3d48d0ab2323d321850a506845ec1012a0ec.
    
    * added
    
    * eof
    
    * Update src/test/seedu/address/logic/commands/AddDrinkCommandTest.java
    
    * update on test
    
    * add test
    
    * eof i love u.....
    
    * added more test to increase coverage
    
    * delete manager UG
    
    * update on uf
    
    * create a new interface for loginINformanager
    
    * Update src/main/java/seedu/address/init/InventoryListInitializer.java
    
    * same
    
    * add interface of LoginINfoManager
    
    * checkstyle
    
    * changed to fix logout login bug

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -17,6 +17,7 @@ import seedu.address.commons.core.Config;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.model.DrinkAttributeChangedEvent;
+import seedu.address.commons.events.model.TransactionListChangedEvent;
 import seedu.address.commons.events.ui.ExitAppRequestEvent;
 import seedu.address.commons.events.ui.InventoryPanelSelectionChangedEvent;
 import seedu.address.commons.events.ui.ShowHelpRequestEvent;
@@ -179,6 +180,11 @@ public class MainWindow extends UiPart<Stage> {
         drinkDetailPanePlaceholder.getChildren().add(drinkDetailPane.getRoot());
     }
 
+    @Subscribe
+    private void handleTransactionListChangedEvent(TransactionListChangedEvent event) {
+        transactionsPanel = new TransactionsPanel(logic.getFilteredTransactionList());
+        transactionsPanelPlaceholder.getChildren().add(transactionsPanel.getRoot());
+    }
 
     void hide() {
         primaryStage.hide();
