4302d283762099b071a5f99d2a690eff57c799b0
Andrew Ong <39642465+andrewome@users.noreply.github.com>
2018-11-12 13:13:32 +0800

    added more tests to preview command

--- src/test/java/seedu/recruit/logic/commands/emailcommand/EmailSendPreviewCommandTest.java
+++ src/test/java/seedu/recruit/logic/commands/emailcommand/EmailSendPreviewCommandTest.java
@@ -5,10 +5,13 @@ import static seedu.recruit.logic.commands.emailcommand.EmailSendCommand.EMAIL_S
 import static seedu.recruit.testutil.TypicalCompaniesAndJobOffers.AUDI;
 import static seedu.recruit.testutil.TypicalCompaniesAndJobOffers.BENTLEY;
 import static seedu.recruit.testutil.TypicalCompaniesAndJobOffers.CASHIER_AUDI;
+import static seedu.recruit.testutil.TypicalCompaniesAndJobOffers.CASHIER_BENTLEY;
+import static seedu.recruit.testutil.TypicalCompaniesAndJobOffers.CASHIER_CHEVROLET;
 import static seedu.recruit.testutil.TypicalCompaniesAndJobOffers.SALESPERSON_AUDI;
 import static seedu.recruit.testutil.TypicalCompaniesAndJobOffers.SALESPERSON_BENTLEY;
 import static seedu.recruit.testutil.TypicalCompaniesAndJobOffers.getTypicalCompanyBookWithoutJobOffers;
 import static seedu.recruit.testutil.TypicalPersons.ALICE;
+import static seedu.recruit.testutil.TypicalPersons.GEORGE;
 import static seedu.recruit.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.Before;
@@ -53,7 +56,7 @@ public class EmailSendPreviewCommandTest {
     }
 
     @Test
-    public void execute_emailSendPreviewCandidateRecipients() {
+    public void execute_emailSendPreviewCandidateRecipientsOneJobOffer() {
         model.getEmailUtil().setAreRecipientsCandidates(true);
         model.getEmailUtil().addCandidate(testCandidate);
         model.getEmailUtil().addJobOffer(testJobOffer);
@@ -70,7 +73,31 @@ public class EmailSendPreviewCommandTest {
     }
 
     @Test
-    public void execute_emailSendPreviewCompanyRecipient() {
+    public void execute_emailSendPreviewCandidateRecipientsTwoJobOffer() {
+        model.getEmailUtil().setAreRecipientsCandidates(true);
+        model.getEmailUtil().addCandidate(testCandidate);
+        model.getEmailUtil().addJobOffer(testJobOffer);
+        JobOffer secondJobOffer = new JobOfferBuilder(CASHIER_BENTLEY).build();
+        model.getEmailUtil().addJobOffer(secondJobOffer);
+        command.execute(model, commandHistory, userPrefs);
+
+        String expectedEmailPreview = "To: [" + testCandidate.getEmail().toString() + "]\n"
+                + "Subject: " + model.getEmailUtil().getEmailSettings().getSubjectCandidateAsRecipient()
+                + "\nContents of the email:\n\n"
+                + model.getEmailUtil().getEmailSettings().getBodyTextCandidateAsRecipient()
+                + "\nCompany: " + testJobOffer.getCompanyName().toString()
+                + "\nJob: " + testJobOffer.getJob().toString()
+                + "\nSalary offered: " + testJobOffer.getSalary().toString()
+                + '\n'
+                + "\nCompany: " + secondJobOffer.getCompanyName().toString()
+                + "\nJob: " + secondJobOffer.getJob().toString()
+                + "\nSalary offered: " + secondJobOffer.getSalary().toString()
+                + '\n';
+        assertEquals(expectedEmailPreview, command.getEmailPreviewToString());
+    }
+
+    @Test
+    public void execute_emailSendPreviewCompanyRecipientOneCandidate() {
         model.getEmailUtil().setAreRecipientsCandidates(false);
         model.getEmailUtil().addCandidate(testCandidate);
         model.getEmailUtil().addJobOffer(testJobOffer);
@@ -89,6 +116,39 @@ public class EmailSendPreviewCommandTest {
                 + "\nEducation: " + testCandidate.getEducation().toString()
                 + "\nEmail: " + testCandidate.getEmail().toString()
                 + "\nPhone Number: " + testCandidate.getPhone().toString()
+                + '\n';
+
+        assertEquals(expectedEmailPreview, command.getEmailPreviewToString());
+    }
+
+    @Test
+    public void execute_emailSendPreviewCompanyRecipientTwoCandidate() {
+        model.getEmailUtil().setAreRecipientsCandidates(false);
+        model.getEmailUtil().addCandidate(testCandidate);
+        model.getEmailUtil().addJobOffer(testJobOffer);
+        Candidate secondCandidate = new CandidateBuilder(GEORGE).build();
+        model.getEmailUtil().addCandidate(GEORGE);
+        command.execute(model, commandHistory, userPrefs);
+
+        String expectedEmailPreview = "To: "
+                + testCompany.getCompanyName().toString()
+                + '(' + testCompany.getEmail().toString() + ')'
+                + " regarding job offers: "
+                + '[' + testJobOffer.getJob().toString() + "]\n"
+                + "Subject: " + model.getEmailUtil().getEmailSettings().getSubjectCompanyAsRecipient()
+                + "\nContents of the email:\n\n"
+                + model.getEmailUtil().getEmailSettings().getBodyTextCompanyAsRecipient() + '\n'
+                + "\nName: " + testCandidate.getName().toString()
+                + "\nAge: " + testCandidate.getAge().toString()
+                + "\nEducation: " + testCandidate.getEducation().toString()
+                + "\nEmail: " + testCandidate.getEmail().toString()
+                + "\nPhone Number: " + testCandidate.getPhone().toString()
+                + '\n'
+                + "\nName: " + secondCandidate.getName().toString()
+                + "\nAge: " + secondCandidate.getAge().toString()
+                + "\nEducation: " + secondCandidate.getEducation().toString()
+                + "\nEmail: " + secondCandidate.getEmail().toString()
+                + "\nPhone Number: " + secondCandidate.getPhone().toString()
                 + '\n';
 
         assertEquals(expectedEmailPreview, command.getEmailPreviewToString());
