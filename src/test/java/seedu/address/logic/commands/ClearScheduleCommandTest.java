3263523442e241145a09267a7558d140572508ec
driedmelon <sourmilo@gmail.com>
2018-11-12 11:19:21 +0800

    Clear schedule command test in progress

--- src/test/java/seedu/address/logic/commands/ClearScheduleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearScheduleCommandTest.java
@@ -1,4 +1,39 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
+
 public class ClearScheduleCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void executeNotExistingPerson() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+        expectedModel.commitAddressBook();
+
+        thrown.expectMessage(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+
+        assertCommandSuccess(new ClearScheduleCommand(INDEX_FIRST_PERSON), model,
+                commandHistory, ClearScheduleCommand.MESSAGE_USAGE, expectedModel);
+    }
 }
