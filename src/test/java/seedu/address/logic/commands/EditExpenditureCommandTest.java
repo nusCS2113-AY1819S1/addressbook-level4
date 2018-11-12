44b8623e19da9e1dba8e8c3b557f987de951a3a2
XiaoYunhan <2028427786@qq.com>
2018-11-12 21:19:57 +0800

    fix travis

--- src/test/java/seedu/address/logic/commands/EditExpenditureCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditExpenditureCommandTest.java
@@ -18,10 +18,10 @@ import org.junit.Test;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
-import seedu.address.model.expenditureinfo.Expenditure;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.expenditureinfo.Expenditure;
 import seedu.address.testutil.EditExpenditureDescriptorBuilder;
 
 /**
