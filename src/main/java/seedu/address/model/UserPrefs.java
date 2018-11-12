32584812fe1ada1e00c65884b43b213b55fa5b51
jitwei98 <limjitwei@gmail.com>
2018-11-12 10:53:22 +0800

    UserPrefs.java: Rename addressbook.xml to jithub.xml Remove exportFilePath

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -12,9 +12,8 @@ import seedu.address.commons.core.GuiSettings;
 public class UserPrefs {
 
     private GuiSettings guiSettings;
-    private Path addressBookFilePath = Paths.get("data" , "addressbook.xml");
-    private Path exportCsvFilePath = Paths.get("data", "addressbook.csv");
-    private Path exportFilePath = Paths.get("data", "export.xml");
+    private Path addressBookFilePath = Paths.get("data" , "jithub.xml");
+    private Path exportCsvFilePath = Paths.get("data", "jithub.csv");
 
     public UserPrefs() {
         setGuiSettings(500, 500, 0, 0);
@@ -42,10 +41,6 @@ public class UserPrefs {
 
     public Path getExportCsvFilePath() {
         return exportCsvFilePath;
-    }
-
-    public Path getExportFilePath() {
-        return exportFilePath;
     }
 
     @Override
